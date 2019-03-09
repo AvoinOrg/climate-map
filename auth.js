4c59652ef9466aaf33b58289760ef78c49115ae4
Elias Kunnas <elias@kunnas.com>
2019-03-09 16:14:21 +0200

    Add support for private datasets with Auth0 login

---
+++ auth.js
@@ -0,0 +1,150 @@
+const claimHashes = {
+    valio: '75e3e7c68bffb0efc8f893345bfe161f77175b8f9ce31840db93ace7fa46f3db',
+}
+
+function hexString(buffer) {
+    const byteArray = new Uint8Array(buffer);
+
+    const hexCodes = [...byteArray].map(value => {
+      const hexCode = value.toString(16);
+      const paddedHexCode = hexCode.padStart(2, '0');
+      return paddedHexCode;
+    });
+
+    return hexCodes.join('');
+  }
+
+const sha256hex = (text, callback) =>
+    crypto.subtle.digest('SHA-256', new TextEncoder().encode(text)).then(
+        digest => callback(hexString(digest))
+    )
+
+const verifyClaimHash = (text, callback) => {
+    const key = text.split('-')[0]
+    sha256hex(text, x => callback(x === claimHashes[key]))
+};
+
+const verifyClaims = (claims, callback) => {
+    // Super hacky. Too lazy to load a proper async/promise library etc.
+    let numLeft = claims.length;
+    let numSuccess = 0;
+    claims.forEach(claim => {
+        verifyClaimHash(claim, (result) => {
+            numLeft--;
+            numSuccess += result
+            if (numLeft === 0) return callback(numSuccess === claims.length);
+        })
+    })
+};
+
+window.initAuthWithClaims = function() {
+    const hashParams = location.hash.replace(/^[#?]*/, '').split('&').reduce((prev, item) => (
+        Object.assign({ [item.split('=')[0]]: item.split('=')[1] }, prev)
+    ), {});
+
+    if (!hashParams || !hashParams.codes) return window.initAuth();
+
+    const claims = hashParams.codes.split(',')
+    verifyClaims(claims, result => {
+        if (result) {
+            window.location.hash = '';
+            window.initAuth(claims);
+        }
+        else alert(`Invalid codes for private datasets: ${claims.join(", ")}`);
+    })
+}
+
+window.initAuth = function(claims) {
+    let idToken;
+    let accessToken;
+    let expiresAt;
+
+    const webAuth = new auth0.WebAuth({
+        domain: AUTH0_DOMAIN,
+        clientID: AUTH0_CLIENT_ID,
+        responseType: 'token id_token',
+        scope: 'openid',
+        redirectUri: window.location.href
+    });
+
+    const loginBtn = document.getElementById('btn-login');
+    const logoutBtn = document.getElementById('btn-logout');
+
+    loginBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        webAuth.authorize({roleclaims: claims});
+    });
+
+    logoutBtn.addEventListener('click', logout);
+
+    function handleAuthentication() {
+        webAuth.parseHash(function(err, authResult) {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                window.location.hash = '';
+                localLogin(authResult);
+            } else if (err) {
+                console.log('ERROR in webAuth.parseHash:', err);
+            }
+            displayButtons();
+        });
+    }
+
+    function localLogin(authResult) {
+        const payload = authResult.idTokenPayload
+
+        const roles = payload['https://map.buttonprogram.org/roles']
+        window.enablePrivateDatasets(roles);
+
+        // Set isLoggedIn flag in localStorage
+        localStorage.setItem('isLoggedIn', 'true');
+        // Set the time that the access token will expire at
+        expiresAt = JSON.stringify(
+            authResult.expiresIn * 1000 + new Date().getTime()
+        );
+        accessToken = authResult.accessToken;
+        idToken = authResult.idToken;
+    }
+
+    function renewTokens() {
+        webAuth.checkSession({}, (err, authResult) => {
+            if (authResult && authResult.accessToken && authResult.idToken) {
+                localLogin(authResult);
+            } else if (err) {
+                console.log('webAuth.checkSession: Could not get a new token', err.error, err.error_description);
+                logout();
+            }
+            displayButtons();
+        });
+    }
+
+    function logout() {
+        // Remove isLoggedIn flag from localStorage
+        localStorage.removeItem('isLoggedIn');
+        // Remove tokens and expiry time
+        accessToken = '';
+        idToken = '';
+        expiresAt = 0;
+        displayButtons();
+    }
+
+    function isAuthenticated() {
+        // Check whether the current time is past the
+        // Access Token's expiry time
+        const expiration = parseInt(expiresAt) || 0;
+        return localStorage.getItem('isLoggedIn') === 'true' && new Date().getTime() < expiration;
+    }
+
+    function displayButtons() {
+        const auth = isAuthenticated();
+        if (loginBtn.hidden !== auth) {
+            loginBtn.toggleAttribute('hidden');
+            logoutBtn.toggleAttribute('hidden');
+        }
+    }
+
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+        renewTokens();
+    } else {
+        handleAuthentication();
+    }
+}
---
+++ auth0_rule_add_secret_roles.js
@@ -0,0 +1,47 @@
+// This is the rule used in Auth0 Rules to add secrets/roles
+// to user app metadata based on corresponding claims.
+function(user, context, callback){
+
+    // Roles should only be set to verified users.
+    if (!user.email || !user.email_verified) {
+        return callback(null, user, context);
+    }
+
+    const claimToSecret = {
+        'valio-REDACTED' : 'valio-REDACTED',
+    };
+
+    user.user_metadata = user.user_metadata || {};
+    user.app_metadata = user.app_metadata || {};
+    user.app_metadata.roles = user.app_metadata.roles || [];
+
+    // NB: This is the only reliable way I found to add "proper" roles/secrets.
+    const roleClaims = context.request.query.roleclaims || [];
+
+    context.idToken['https://map.buttonprogram.org/roles'] = user.app_metadata.roles || [];
+
+    const secrets = [];
+    for (const claim in claimToSecret) {
+        if (roleClaims.indexOf(claim) !== -1)
+            secrets.push(claimToSecret[claim]);
+    }
+    if (secrets.length === 0) {
+        return callback(null, user, context);
+    }
+
+    // update the app_metadata that will be part of the response
+    for (const idx in secrets) {
+        if (user.app_metadata.roles.indexOf(secrets[idx]) === -1)
+            user.app_metadata.roles.push(secrets[idx]);
+    }
+
+    // persist the app_metadata update
+    auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
+    .then(function(){
+        context.idToken['https://map.buttonprogram.org/roles'] = user.app_metadata.roles;
+        callback(null, user, context);
+    })
+    .catch(function(err){
+        callback(err);
+    });
+}
