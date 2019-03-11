33f4a3f71bc1a695645d4e7a6c0043005dafc8a7
Elias Kunnas <elias@kunnas.com>
2019-03-11 18:19:48 +0200

    Add layer: Finnish Fields (Finnish Food Authority)

--- auth.js
+++ auth.js
@@ -63,7 +63,7 @@ window.initAuth = function(claims) {
         domain: AUTH0_DOMAIN,
         clientID: AUTH0_CLIENT_ID,
         responseType: 'token id_token',
-        scope: 'openid',
+        scope: 'openid email',
         redirectUri: window.location.href
     });
 
