7536f3fa202b14d7ad7542e6567caf7342521b38
xbexbex <petja.valkama@gmail.com>
2021-05-30 16:26:29 +0300

    Add profile and funnels state hooks

--- src/components/State.tsx
+++ src/components/State.tsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { observable } from "micro-observables";
 import { useObservable } from "micro-observables";
 
+import { ProfileState } from "./Utils/types";
+
 export const StateContext = React.createContext({});
 
 export const StateProvider = (props) => {
@@ -11,14 +13,18 @@ export const StateProvider = (props) => {
   const [isSidebarDisabled, setIsSidebarDisabled] = useState(false);
   const [isSignupOpen, setIsSignupOpen] = useState(false);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
-  const [profileState, setProfileState] = useState(0);
+  const [profileState, setProfileState] = useState<ProfileState>("none");
   const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [signupFunnelStep, setSignupFunnelStep] = useState(0);
 
   useEffect(() => {
     if (isSignupOpen || isLoginOpen || isProfileOpen) {
       setIsSidebarDisabled(true);
     } else {
       setIsSidebarDisabled(false);
+    }
+    if (isSignupOpen || isLoginOpen) {
+      setIsProfileOpen(false)
     }
   }, [isSignupOpen, isProfileOpen, isLoginOpen]);
 
@@ -35,6 +41,8 @@ export const StateProvider = (props) => {
     setIsProfileOpen,
     profileState,
     setProfileState,
+    signupFunnelStep,
+    setSignupFunnelStep,
   };
 
   return (
