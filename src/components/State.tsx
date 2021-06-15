c646b74f8bd3089ed79c4fbe9a1026187c51d4d1
xbexbex <petja.valkama@gmail.com>
2021-06-15 21:35:28 +0300

    Add notifications

--- src/components/State.tsx
+++ src/components/State.tsx
@@ -1,12 +1,57 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { observable } from "micro-observables";
 import { useObservable } from "micro-observables";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
+import { Snackbar } from "@material-ui/core";
+import MuiAlert from "@material-ui/lab/Alert";
 
 import { ProfileState } from "./Utils/types";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "100%",
+      zIndex: 3000,
+      "& > * + *": {
+        marginTop: theme.spacing(2),
+      },
+    },
+  })
+);
+
+const Notification = (props) => {
+  const [open, setOpen] = useState(true);
+
+  const handleClose = (event?: React.SyntheticEvent, reason?: string) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={props.duration}
+      onClose={handleClose}
+    >
+      <MuiAlert
+        elevation={6}
+        variant="filled"
+        onClose={handleClose}
+        severity={props.severity}
+      >
+        {props.message}
+      </MuiAlert>
+    </Snackbar>
+  );
+};
 
 export const StateContext = React.createContext({});
 
 export const StateProvider = (props) => {
+  const classes = useStyles({});
+
   // for use in non-react components
   const isSidebarOpen = useObservable(isOpenObservable);
 
@@ -14,8 +59,13 @@ export const StateProvider = (props) => {
   const [isSignupOpen, setIsSignupOpen] = useState(false);
   const [isProfileOpen, setIsProfileOpen] = useState(false);
   const [profileState, setProfileState] = useState<ProfileState>("none");
+  // const [profileMessage, setProfileMessage] = useState(null);
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [signupFunnelStep, setSignupFunnelStep] = useState(0);
+  const [notifications, setNotifications] = useState({});
+
+  const notificationsRef = useRef(notifications);
+  notificationsRef.current = notifications;
 
   useEffect(() => {
     if (isSignupOpen || isLoginOpen || isProfileOpen) {
@@ -24,9 +74,46 @@ export const StateProvider = (props) => {
       setIsSidebarDisabled(false);
     }
     if (isSignupOpen || isLoginOpen) {
-      setIsProfileOpen(false)
+      setIsProfileOpen(false);
     }
   }, [isSignupOpen, isProfileOpen, isLoginOpen]);
+
+  useEffect(() => {
+    if (isSignupOpen || isLoginOpen || isProfileOpen) {
+      setIsSidebarDisabled(true);
+    } else {
+      setIsSidebarDisabled(false);
+    }
+    if (isSignupOpen || isLoginOpen) {
+      setIsProfileOpen(false);
+    }
+  }, [isSignupOpen, isProfileOpen, isLoginOpen]);
+
+  const notify = (message, severity, duration = 99999) => {
+    const newNotification = {};
+    const index = new Date().getTime()
+
+    newNotification[index] = {
+      message,
+      severity,
+      duration,
+    };
+
+    setNotificationTimeout(index, duration + 1000);
+
+    setNotifications((not) => {
+      console.log({ ...not, ...newNotification })
+      return { ...notifications, ...newNotification };
+    });
+  };
+
+  const setNotificationTimeout = (index, timeout) => {
+    setTimeout(async () => {
+      const newNotifications = { ...notificationsRef.current };
+      delete newNotifications[index];
+      setNotifications(newNotifications);
+    }, timeout);
+  };
 
   const values = {
     isSidebarOpen,
@@ -43,11 +130,28 @@ export const StateProvider = (props) => {
     setProfileState,
     signupFunnelStep,
     setSignupFunnelStep,
+    notifications,
+    notify,
+    // profileMessage,
+    // setProfileMessage,
   };
 
   return (
     <StateContext.Provider value={values}>
       {props.children}
+      <div className={classes.root}>
+        {Object.keys(notifications).map((key) => {
+          console.log(key)
+          return (
+            <Notification
+              key={key}
+              message={notifications[key].message}
+              severity={notifications[key].severity}
+              duration={notifications[key].duration}
+            />
+          );
+        })}
+      </div>
     </StateContext.Provider>
   );
 };
