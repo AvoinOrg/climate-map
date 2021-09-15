989cb35c0d901801503670420926f23478b9521c
xbexbex <petja.valkama@gmail.com>
2021-09-15 16:54:07 +0300

    Refactor forest content

--- src/components/Accordion/ForestContent.tsx
+++ src/components/Accordion/ForestContent.tsx
@@ -6,40 +6,62 @@ import { AOExpansionPanelLink, AOExpansionPanel } from './AOExpansionPanel';
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
-      width: '100%',
+      width: "100%",
     },
-  }),
+    legendBox: {
+      backgroundColor: (props: any) => props.color,
+      border: "1px solid black",
+      width: "1rem",
+      height: "1rem",
+      padding: 5,
+      margin: "0 5px -2px 0",
+      display: "inline-block",
+    },
+  })
 );
 
+const LegendBox = (props) => {
+  const classes = useStyles({ color: props.color });
+  return (
+    <span>
+      <span className={classes.legendBox}></span>
+      {props.title}
+    </span>
+  );
+};
 
-const MatureForestContent = () =>
+const MatureForestContent = () => (
   <div>
     <p>
-      This layer shows forests that have reached the approximate threshold for regeneration felling.
+      This layer shows forests that have reached the approximate threshold for
+      regeneration felling.
     </p>
     Legend:
-    <legend id='legend-mature-forests' style={{display: 'flex', flexDirection: 'column', padding: '6px 0 0 0'}}>
-      <span><span style={{backgroundColor: 'rgba(73, 25, 2320, 0.65', border: '1px solid black', width: '1rem', height: '1rem', padding: 5, margin: "0 5px -2px 0", display: 'inline-block'}}></span>Mature forest</span>
-      <span><span style={{backgroundColor: 'rgba(206, 244, 66, 0.35', border: '1px solid black', width: '1rem', height: '1rem', padding: 5, margin: "4px 5px -2px 0", display: 'inline-block'}}></span>Other forest</span>
+    <legend
+      id="legend-mature-forests"
+      style={{ display: "flex", flexDirection: "column", padding: "6px 0 0 0" }}
+    >
+      <LegendBox color="rgba(73, 25, 2320, 0.65)" title="Mature forest" />
+      <LegendBox color="rgba(206, 244, 66, 0.35)" title="Other forest" />
     </legend>
   </div>
+);
 
-
-
-
-const MangroveForestContent = () =>
+const MangroveForestContent = () => (
   <div>
     <p>
       This layer shows mangrove forests monitored by
-      {} <a href="https://www.globalmangrovewatch.org/about/">the Global Mangrove Watch</a>.
+      {}{" "}
+      <a href="https://www.globalmangrovewatch.org/about/">
+        the Global Mangrove Watch
+      </a>
+      .
     </p>
-    <p>
-      The data shown here is from 2010.
-    </p>
+    <p>The data shown here is from 2010.</p>
   </div>
+);
 
-
-const TropicalForestContent = () =>
+const TropicalForestContent = () => (
   <div>
     <p>
       <a href="https://www.globalforestwatch.org/">the Global Forest Watch</a>
@@ -47,26 +69,43 @@ const TropicalForestContent = () =>
       {} <a href="https://www.cifor.org/">CIFOR data</a> of global wetlands.
     </p>
     <p>
-      Green areas area forest plantations that are on mineral soil
-      and brown areas those in peatlands.
+      Green areas area forest plantations that are on mineral soil and brown
+      areas those in peatlands.
     </p>
     <p>
-      Click on a forest plantation to view more information and estimated emission
-      reduction potentials of peatland forest plantations when the groundwater level
-      is lifted by 40 cm.
+      Click on a forest plantation to view more information and estimated
+      emission reduction potentials of peatland forest plantations when the
+      groundwater level is lifted by 40 cm.
     </p>
   </div>
+);
 
 
 const ForestContent = () => {
   const classes = useStyles({});
-  return <div className={classes.root}>
-    <AOExpansionPanelLink href='/layers/fi-forest' label={"Finland's Forests"} />
-    <AOExpansionPanel groupName={'mature-forests'} label={"Mature Forests"} content={<MatureForestContent />} />
-    <AOExpansionPanel groupName={'mangrove-forests'} label={"Mangrove forests"} content={<MangroveForestContent />} />
-    <AOExpansionPanel groupName={'gfw_tree_plantations'} label={"Tree plantations"} content={<TropicalForestContent />} />
+  return (
+    <div className={classes.root}>
+      <AOExpansionPanelLink
+        href="/layers/fi-forest"
+        label={"Finland's Forests"}
+      />
+      <AOExpansionPanel
+        groupName={"mature-forests"}
+        label={"Mature Forests"}
+        content={<MatureForestContent />}
+      />
+      <AOExpansionPanel
+        groupName={"mangrove-forests"}
+        label={"Mangrove forests"}
+        content={<MangroveForestContent />}
+      />
+      <AOExpansionPanel
+        groupName={"gfw_tree_plantations"}
+        label={"Tree plantations"}
+        content={<TropicalForestContent />}
+      />
   </div>
-}
+  );
+};
 
-
-export default ForestContent
+export default ForestContent;
