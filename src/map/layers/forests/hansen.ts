1043bd4f1efdb4d91ac9451d1037d2be3ba39e7f
xbexbex <petja.valkama@gmail.com>
2021-10-04 15:43:26 +0300

    Update hansen layer

--- src/map/layers/forests/hansen.ts
+++ src/map/layers/forests/hansen.ts
@@ -6,8 +6,7 @@ const URL_PREFIX = `https://server.avoin.org/data/map/hansen/`;
 addSource("hansen-treecover-tiles", {
   type: "raster",
   tiles: [URL_PREFIX + "treecover/tiles/{z}/{x}/{y}.png"],
-  maxzoom: 5,
-  tileSize: 256,
+  maxzoom: 7,
   attribution:
     '<a href="https://developers.google.com/earth-engine/datasets/catalog/UMD_hansen_global_forest_change_2020_v1_8">Hansen/UMD/Google/USGS/NASAESA</a>',
 });
@@ -15,7 +14,7 @@ addSource("hansen-treecover-tiles", {
 addSource("hansen-gainloss-tiles", {
   type: "raster",
   tiles: [URL_PREFIX + "gainloss/tiles/{z}/{x}/{y}.png"],
-  maxzoom: 5,
+  maxzoom: 7,
   attribution:
     '<a href="https://developers.google.com/earth-engine/datasets/catalog/UMD_hansen_global_forest_change_2020_v1_8">Hansen/UMD/Google/USGS/NASAESA</a>',
 });
