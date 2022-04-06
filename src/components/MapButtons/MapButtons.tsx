fb7087d04334de64acc5a4ce7689aa11c8e7e473
xbexbex <petja.valkama@gmail.com>
2022-04-07 00:46:01 +0300

    Update map buttons

--- src/components/MapButtons/MapButtons.tsx
+++ src/components/MapButtons/MapButtons.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles';
+import React, { useContext } from 'react'
+import { makeStyles, useTheme, Theme, createStyles } from '@material-ui/core/styles'
 
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 
-import SatelliteIcon from '@material-ui/icons/Satellite';
+import SatelliteIcon from '@material-ui/icons/Satellite'
 
-import ExploreIcon from '@material-ui/icons/Explore';
-import AddIcon from '@material-ui/icons/Add';
-import RemoveIcon from '@material-ui/icons/Remove';
+import ExploreIcon from '@material-ui/icons/Explore'
+import AddIcon from '@material-ui/icons/Add'
+import RemoveIcon from '@material-ui/icons/Remove'
 
-import GpsFixedIcon from '@material-ui/icons/GpsFixed';
+import GpsFixedIcon from '@material-ui/icons/GpsFixed'
 
-import * as Map from '../../map/map'
-import { Button } from '@material-ui/core';
+import MapContext from 'Components/Map/Map'
+import { Button } from '@material-ui/core'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -20,38 +20,45 @@ const useStyles = makeStyles((theme: Theme) =>
       position: 'absolute',
       top: theme.spacing(12),
       right: theme.spacing(2),
-      zIndex: theme.zIndex.mobileStepper, /* force this to be on top of the map */
+      zIndex: theme.zIndex.mobileStepper /* force this to be on top of the map */,
     },
     btn: {
       color: '#333333',
-      backgroundColor: "white",
+      backgroundColor: 'white',
       '&:hover': {
-        backgroundColor: "#eeeeee"
-      }
+        backgroundColor: '#eeeeee',
+      },
     },
     divider: {
       marginTop: theme.spacing(1),
       marginBottom: theme.spacing(1),
-    }
-  }),
-);
+    },
+  })
+)
 export default function GroupOrientation() {
-  const theme = useTheme();
-  const classes = useStyles(theme);
+  const theme = useTheme()
+  const classes = useStyles(theme)
+  const { mapToggleTerrain, mapResetNorth, mapZoomIn, mapZoomOut, mapRelocate } = React.useContext(MapContext)
 
   return (
     <div className={classes.iconGroup}>
-      <ButtonGroup
-        orientation="vertical"
-      >
-        <Button onClick={Map.mapToggleTerrain} size="small" className={classes.btn}><SatelliteIcon fontSize="small" /></Button>
-        {/* <Divider className={classes.divider}/> */}
-        <Button onClick={Map.mapResetNorth} size="small" className={classes.btn}><ExploreIcon fontSize="small" /></Button>
-        <Button onClick={Map.mapZoomIn} size="small" className={classes.btn}><AddIcon fontSize="small" /></Button>
-        <Button onClick={Map.mapZoomOut} size="small" className={classes.btn}><RemoveIcon fontSize="small" /></Button>
-        {/* <Divider className={classes.divider}/> */}
-        <Button onClick={Map.mapRelocate} size="small" className={classes.btn}><GpsFixedIcon fontSize="small" /></Button>
+      <ButtonGroup orientation="vertical">
+        <Button onClick={mapToggleTerrain} size="small" className={classes.btn}>
+          <SatelliteIcon fontSize="small" />
+        </Button>
+        <Button onClick={mapResetNorth} size="small" className={classes.btn}>
+          <ExploreIcon fontSize="small" />
+        </Button>
+        <Button onClick={mapZoomIn} size="small" className={classes.btn}>
+          <AddIcon fontSize="small" />
+        </Button>
+        <Button onClick={mapZoomOut} size="small" className={classes.btn}>
+          <RemoveIcon fontSize="small" />
+        </Button>
+        <Button onClick={mapRelocate} size="small" className={classes.btn}>
+          <GpsFixedIcon fontSize="small" />
+        </Button>
       </ButtonGroup>
     </div>
-  );
+  )
 }
