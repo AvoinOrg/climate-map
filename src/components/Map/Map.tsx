02d22160b2ccfb2b5105f11190acd06f09d6a626
xbexbex <petja.valkama@gmail.com>
2022-04-07 00:46:13 +0300

    Add map

---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,79 @@
+import 'ol/ol.css'
+import React, { useContext, createContext, useState, useRef, useEffect } from 'react'
+import { Map, View } from 'ol'
+import olms from 'ol-mapbox-style'
+
+interface Props {
+  children: any
+  zoom: number
+  center: [number, number]
+}
+
+interface IMapContext {
+  map?: Map
+  mapToggleTerrain?: () => void
+  mapResetNorth?: () => void
+  getGeocoder?: () => any
+  mapRelocate?: () => void
+  mapZoomIn?: () => void
+  mapZoomOut?: () => void
+}
+
+const MapContext = createContext<IMapContext>({})
+
+export const MapProvider = ({ children, zoom, center }: Props) => {
+  const [map, setMap] = useState<Map>(null)
+  const mapRef = useRef()
+
+  useEffect(() => {
+    const options = {
+      view: new View({ zoom, center }),
+      layers: [] as any[],
+      controls: [] as any[],
+      overlays: [] as any[],
+    }
+    const mapObject = new Map(options)
+
+    mapObject.setTarget(mapRef.current)
+
+    setMap(mapObject)
+    return () => mapObject.setTarget(undefined)
+  }, [])
+
+  useEffect(() => {
+    if (!map) return
+    map.getView().setZoom(zoom)
+  }, [zoom])
+
+  useEffect(() => {
+    if (!map) return
+    map.getView().setCenter(center)
+  }, [center])
+
+  // TODO ZONE
+  const getGeocoder = () => {}
+  const mapRelocate = () => {}
+  const mapResetNorth = () => {}
+  const mapToggleTerrain = () => {}
+  const mapZoomIn = () => {}
+  const mapZoomOut = () => {}
+
+  const values = {
+    map,
+    mapToggleTerrain,
+    mapResetNorth,
+    getGeocoder,
+    mapRelocate,
+    mapZoomIn,
+    mapZoomOut,
+  }
+
+  return (
+    <MapContext.Provider value={values}>
+      <div ref={mapRef} id="map" className="ol-map"></div>
+      {children}
+    </MapContext.Provider>
+  )
+}
+
+export default MapContext
