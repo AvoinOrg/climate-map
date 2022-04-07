6e43cfb9878d68a5bdf197fdd2a1ee562bdbea06
xbexbex <petja.valkama@gmail.com>
2022-04-07 15:59:19 +0300

    Disable broken functions

--- src/components/OverlayMessages.tsx
+++ src/components/OverlayMessages.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import { observable, useObservable } from "micro-observables";
-import * as LayerGroupState from 'src/map/LayerGroupState';
+import { observable, useObservable } from 'micro-observables'
+import MapContext from 'Components/Map'
 
-type Message = { message: string; layer: string; }
+type Message = { message: string; layer: string }
 const nullMessage: Message = { message: null, layer: null }
 
-const overlayMessage = observable<Message>(nullMessage);
+const overlayMessage = observable<Message>(nullMessage)
 
 export const setOverlayMessage = (condition: boolean, message: Message) => {
   overlayMessage.set(condition ? message : nullMessage)
 }
 
-const OverlayMessages = () => {
+//OL_FIX: check later what is happening in this component and remove micro-observables
+const OverlayMessages: React.FC = () => {
+  const { activeLayers } = React.useContext(MapContext)
   const { message, layer } = useObservable(overlayMessage.readOnly())
-  const activeLayers = useObservable(LayerGroupState.layerGroups)
-  const isActive = activeLayers.filter(x => x.name === layer).length > 0
+  const isActive = activeLayers.filter((x) => x.name === layer).length > 0
 
   if (!isActive) return null
 
-  return <div className='map-overlay-message-container'>
-    <div className='map-overlay-message'>
-      {message}
+  return (
+    <div className="map-overlay-message-container">
+      <div className="map-overlay-message">{message}</div>
     </div>
-  </div>
+  )
 }
 
 export default OverlayMessages
