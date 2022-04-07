55129db6e7170809738e8281bf8a26fc9b31a4e8
xbexbex <petja.valkama@gmail.com>
2022-04-07 15:58:59 +0300

    Disable broken functions

--- src/components/Accordion/AOAccordion.tsx
+++ src/components/Accordion/AOAccordion.tsx
@@ -7,63 +7,55 @@ import {
   makeStyles,
   Theme,
   Typography,
-} from "@material-ui/core";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import { useObservable } from "micro-observables";
-import React from "react";
-import { Link } from "react-router-dom";
-import * as LayerGroupState from "../../map/LayerGroupState";
+} from '@material-ui/core'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import MapContext from 'Components/Map'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     heading: {
-      fontWeight: theme.typography["regular"].fontWeight,
+      fontWeight: theme.typography['regular'].fontWeight,
     },
     content: {
-      margin: "unset",
+      margin: 'unset',
     },
   })
-);
+)
 
 export const AOAccordion = (props: any) => {
-  const { groupName, label, content, panelProps } = props;
-  const layerGroups = useObservable(LayerGroupState.layerGroups);
-  const groupEnabled =
-    layerGroups.filter((x) => x.name === groupName).length > 0;
-  const classes = useStyles({});
+  const { groupName, label, content, panelProps } = props
+  const { layerGroups } = React.useContext(MapContext)
+  // const groupEnabled = layerGroups.filter((x) => x.name === groupName).length > 0
+  const classes = useStyles({})
 
   return (
     <Accordion {...panelProps}>
-      <AccordionSummary
-        className={classes.content}
-        expandIcon={<ExpandMoreIcon />}
-      >
+      <AccordionSummary className={classes.content} expandIcon={<ExpandMoreIcon />}>
         <FormControlLabel
           onClick={(event) => {
-            event.stopPropagation();
+            event.stopPropagation()
           }}
-          onChange={(event) =>
-            LayerGroupState.setGroupState(
-              groupName,
-              (event.target as HTMLInputElement).checked
-            )
-          }
+          // OL_FIX: ENABLE LATER
+          // onChange={(event) => LayerGroupState.setGroupState(groupName, (event.target as HTMLInputElement).checked)}
           onFocus={(event) => event.stopPropagation()}
           control={<Checkbox />}
           label={label}
-          checked={groupEnabled}
+          // checked={groupEnabled}
         />
         {/* <Typography className={classes.heading}>{label}</Typography> */}
       </AccordionSummary>
 
       <AccordionDetails>{content}</AccordionDetails>
     </Accordion>
-  );
-};
+  )
+}
 
 export const AOAccordionLink = ({ href, label }: any) => {
-  const classes = useStyles({});
+  const classes = useStyles({})
 
   return (
     <Accordion expanded={false}>
@@ -71,17 +63,17 @@ export const AOAccordionLink = ({ href, label }: any) => {
         <AccordionSummary
           style={{ marginLeft: 31 }}
           className={classes.content}
-          expandIcon={<ExpandMoreIcon style={{ transform: "rotate(-90deg" }} />}
+          expandIcon={<ExpandMoreIcon style={{ transform: 'rotate(-90deg' }} />}
         >
           <Typography className={classes.heading}>{label}</Typography>
         </AccordionSummary>
       </Link>
     </Accordion>
-  );
-};
+  )
+}
 
 export const AOProfileAccordion = ({ onClick, label }: any) => {
-  const classes = useStyles({});
+  const classes = useStyles({})
 
   return (
     <Accordion onClick={onClick} expanded={false}>
@@ -89,52 +81,48 @@ export const AOProfileAccordion = ({ onClick, label }: any) => {
         <AccordionSummary
           style={{ marginLeft: 31 }}
           className={classes.content}
-          expandIcon={<ExpandMoreIcon style={{ transform: "rotate(-90deg" }} />}
+          expandIcon={<ExpandMoreIcon style={{ transform: 'rotate(-90deg' }} />}
         >
           <Typography className={classes.heading}>{label}</Typography>
         </AccordionSummary>
       </div>
     </Accordion>
-  );
-};
+  )
+}
 
-export const LayerToggleControl = ({ groupName, label }) => {
-  const layerGroups = useObservable(LayerGroupState.layerGroups);
-  const groupEnabled =
-    layerGroups.filter((x) => x.name === groupName).length > 0;
+export const LayerToggleControl = ({ groupName, label }: any) => {
+  const { layerGroups } = React.useContext(MapContext)
+  // OL_FIX: ENABLE LATER
+  // const groupEnabled = layerGroups.filter((x) => x.name === groupName).length > 0
 
   return (
     <FormControlLabel
       onClick={(event) => {
-        event.stopPropagation();
+        event.stopPropagation()
       }}
-      onChange={(event) =>
-        LayerGroupState.setGroupState(
-          groupName,
-          (event.target as HTMLInputElement).checked
-        )
-      }
+      // OL_FIX: ENABLE LATER
+      // onChange={(event) => LayerGroupState.setGroupState(groupName, (event.target as HTMLInputElement).checked)}
       onFocus={(event) => event.stopPropagation()}
       control={<Checkbox />}
       label={label}
-      checked={groupEnabled}
+      // checked={groupEnabled}
     />
-  );
-};
+  )
+}
 
 export const AOAccordionHeader = ({ href, label }: any) => {
-  const classes = useStyles({});
+  const classes = useStyles({})
 
   return (
     <Accordion expanded={false}>
       <Link to={href} className="neutral-link">
         <AccordionSummary
           className={classes.content}
-          expandIcon={<ExpandMoreIcon style={{ transform: "rotate(90deg" }} />}
+          expandIcon={<ExpandMoreIcon style={{ transform: 'rotate(90deg' }} />}
         >
           <Typography className={classes.heading}>{label}</Typography>
         </AccordionSummary>
       </Link>
     </Accordion>
-  );
-};
+  )
+}
