c2177f8796fb3a3e863b8280ea80a275fcf36181
xbexbex <petja.valkama@gmail.com>
2022-04-07 21:39:10 +0300

    Update map

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,14 @@
 import 'ol/ol.css'
 import React, { createContext, useState, useRef, useEffect } from 'react'
 import Box from '@mui/material/Box'
+import { fromLonLat } from 'ol/proj'
 import { Map, View } from 'ol'
+import TileLayer from 'ol/layer/Tile'
+import OSM, { ATTRIBUTION } from 'ol/source/OSM'
 import olms from 'ol-mapbox-style'
 
 interface Props {
   children: any
-  zoom: number
-  center: [number, number]
 }
 
 interface IMapContext {
@@ -22,19 +23,27 @@ interface IMapContext {
   layerGroups?: any[]
 }
 
+const osmLayer = new TileLayer({
+  source: new OSM(),
+})
+
 const MapContext = createContext<IMapContext>({})
 
-export const MapProvider = ({ children, zoom, center }: Props) => {
+export const MapProvider = ({ children }: Props) => {
   const [map, setMap] = useState<Map>(null)
+  const [baseLayer, setBaseLayer] = useState(osmLayer)
   const [activeLayers, setActiveLayers] = useState<any[]>([])
   const [layerGroups, setLayerGroups] = useState<any[]>([])
   const mapRef = useRef()
 
   useEffect(() => {
     const options = {
-      view: new View({ zoom, center }),
-      layers: [] as any[],
-      controls: [] as any[],
+      view: new View({ zoom: 5, center: fromLonLat([15, 62]) }),
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
       overlays: [] as any[],
     }
     const mapObject = new Map(options)
@@ -42,18 +51,9 @@ export const MapProvider = ({ children, zoom, center }: Props) => {
     mapObject.setTarget(mapRef.current)
 
     setMap(mapObject)
+    // olms(mapObject, 'https://tiles.stadiamaps.com/styles/alidade_smooth.json')
     return () => mapObject.setTarget(undefined)
   }, [])
-
-  useEffect(() => {
-    if (!map) return
-    map.getView().setZoom(zoom)
-  }, [zoom])
-
-  useEffect(() => {
-    if (!map) return
-    map.getView().setCenter(center)
-  }, [center])
 
   // TODO ZONE
   const getGeocoder = () => {}
