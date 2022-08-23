29d638a4aa4d2a544520e77f3e1d5f80c23107af
xbexbex <petja.valkama@gmail.com>
2022-08-23 16:54:14 +0300

    Update BuildingsContent

---
+++ src/components/Accordion/BuildingsContent/BuildingsContent.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import createStyles from '@mui/styles/createStyles'
+import makeStyles from '@mui/styles/makeStyles'
+
+import { AOAccordion } from '../AOAccordion'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+  })
+)
+
+const BuildingsContent = () => {
+  const classes = useStyles({})
+
+  return (
+    <div className={classes.root}>
+      <AOAccordion layerId={'building_energy_certs'} label={'Building energy certificates (Finland)'} content={null} />
+      {/* <AOAccordion groupName={'fi-buildings'} label={'Buildings (Finland)'} content={null} />
+        <AOAccordion groupName={'helsinki-buildings'} label={'Buildings (Helsinki area)'} content={null} />
+        <AOAccordion groupName={'hsy-solar-potential'} label={'Helsinki Area Solar Power Potential'} content={null} /> */}
+    </div>
+  )
+}
+
+export default BuildingsContent
