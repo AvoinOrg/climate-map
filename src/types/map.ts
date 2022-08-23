2896806882bbe70c86527b55e4e326a35370c165
xbexbex <petja.valkama@gmail.com>
2022-08-23 16:55:25 +0300

    Update types

--- src/types/map.ts
+++ src/types/map.ts
@@ -1,3 +1,8 @@
-interface mapFunctions {}
+import { Style as MbStyle } from 'mapbox-gl'
+// interface mapFunctions {}
 
 export type SourceType = 'geojson' | 'vector' | 'raster' | 'image' | 'video' | 'canvas'
+
+export type LayerId = 'building_energy_certs' | 'no2' | 'snow_cover_loss'
+
+export type LayerConf = { id: LayerId; style: MbStyle; popupFunc: any }
---
+++ src/types/state.ts
@@ -0,0 +1,5 @@
+export type VerificationStatus = 'unverified' | 'errored' | 'verified' | 'emailErrored' | 'emailSent'
+
+export type ProfileState = 'none' | 'data' | 'dataIntegrate' | 'verification' | 'farmCarbon'
+
+export type ModalState = 'none' | 'signup' | 'login' | 'profile'
