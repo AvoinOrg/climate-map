aadd23a5f5551716da0ee77f3556508ecfd6c2bb
xbexbex <petja.valkama@gmail.com>
2022-08-24 14:13:37 +0300

    Add finland buildings

--- src/components/Map/Layers/layers.ts
+++ src/components/Map/Layers/layers.ts
@@ -1,9 +1,10 @@
 // Exports the layer configurations to the Map component.
 
-import buildingEnergyCertificates from './Buildings/builidingEnergyCertificates'
+import buildingEnergyCertificates from './Buildings/buildingEnergyCertificates'
+import finlandBuildings from './Buildings/finlandBuildings'
 import snowCoverLoss from './SnowCoverLoss/snowCoverLoss'
 import airQuality from './AirQuality/airQuality'
 
-const layerConfs = [buildingEnergyCertificates, snowCoverLoss, airQuality]
+const layerConfs = [buildingEnergyCertificates, snowCoverLoss, airQuality, finlandBuildings]
 
 export default layerConfs
