409f131e460254b9b08aa2daa7c01678bd691872
xbexbex <petja.valkama@gmail.com>
2022-08-24 13:34:59 +0300

    Update snowcoverloss

--- src/components/Map/Layers/layers.ts
+++ src/components/Map/Layers/layers.ts
@@ -1,5 +1,6 @@
 import buildingEnergyCertificates from './Buildings/builidingEnergyCertificates'
+import snowCoverLoss from './SnowCoverLoss/snowCoverLoss'
 
-const layerConfs = [buildingEnergyCertificates]
+const layerConfs = [buildingEnergyCertificates, snowCoverLoss]
 
 export default layerConfs
