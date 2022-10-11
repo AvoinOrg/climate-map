1e7521dd8646f2860f7fb06f1aec8812ca82c057
xbexbex <petja.valkama@gmail.com>
2022-10-11 13:53:16 +0300

    Add state export file

---
+++ src/components/State/index.ts
@@ -0,0 +1,4 @@
+import { UiStateContext, UiStateProvider } from './UiState'
+import { UserStateContext, UserStateProvider } from './UserState'
+
+export { UiStateContext, UiStateProvider, UserStateContext, UserStateProvider }
