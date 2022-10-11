d2b7ed075af878987295b74fa4f9b8fc8502fa6e
xbexbex <petja.valkama@gmail.com>
2022-10-11 20:27:51 +0300

    Update hansen

--- src/components/Map/Layers/Forests/hansen.ts
+++ src/components/Map/Layers/Forests/hansen.ts
@@ -3,22 +3,23 @@ import { Style as MbStyle } from 'mapbox-gl'
 import { LayerId, LayerConf } from 'Types/map'
 
 const id: LayerId = 'hansen'
-const gainlossId = 'hansen_gainloss'
 const URL_PREFIX = `https://server.avoin.org/data/map/hansen/`
 
 const getStyle = async (): Promise<MbStyle> => {
+  const sourceNames = ['hansen-treecover', 'hansen-gainloss']
+
   return {
     version: 8,
     name: id,
     sources: {
-      [id]: {
+      [sourceNames[0]]: {
         type: 'raster',
         tiles: [URL_PREFIX + 'treecover/tiles/{z}/{x}/{y}.png'],
         maxzoom: 7,
         attribution:
           '<a href="https://developers.google.com/earth-engine/datasets/catalog/UMD_hansen_global_forest_change_2020_v1_8">Hansen/UMD/Google/USGS/NASAESA</a>',
       },
-      [gainlossId]: {
+      [sourceNames[1]]: {
         type: 'raster',
         tiles: [URL_PREFIX + 'gainloss/tiles/{z}/{x}/{y}.png'],
         maxzoom: 7,
@@ -28,8 +29,8 @@ const getStyle = async (): Promise<MbStyle> => {
     },
     layers: [
       {
-        id: id + '-raster',
-        source: id,
+        id: sourceNames[0] + '-raster',
+        source: sourceNames[0],
         type: 'raster',
         minzoom: 0,
         paint: {
@@ -38,8 +39,8 @@ const getStyle = async (): Promise<MbStyle> => {
         BEFORE: 'FILL',
       },
       {
-        id: gainlossId + '-raster',
-        source: gainlossId,
+        id: sourceNames[1] + '-raster',
+        source: sourceNames[1],
         type: 'raster',
         minzoom: 0,
         paint: {
