b897b5936765e4d5d46bdf5c78be0e03b724290e
xbexbex <petja.valkama@gmail.com>
2022-10-11 15:36:13 +0300

    Rename types

--- src/utils/types.ts
+++
@@ -1,7 +0,0 @@
-export type VerificationStatus = 'unverified' | 'errored' | 'verified' | 'emailErrored' | 'emailSent'
-
-export type ProfileState = 'none' | 'data' | 'dataIntegrate' | 'verification' | 'farmCarbon'
-
-export type ModalState = 'none' | 'signup' | 'login' | 'profile'
-
-export type LayerId = 'builiding_energy_certs' | 'no2' | 'snow_cover_loss'
