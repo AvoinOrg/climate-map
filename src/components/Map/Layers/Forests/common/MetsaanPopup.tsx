67cc2c68d156c77ca72c3c847383c7341d583bd7
xbexbex <petja.valkama@gmail.com>
2022-10-13 14:07:49 +0300

    Update popup

--- src/components/Map/Layers/Forests/common/MetsaanPopup.tsx
+++ src/components/Map/Layers/Forests/common/MetsaanPopup.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import _ from 'lodash'
 import Feature from 'ol/Feature'
-import { Table, TableBody, TableCell, TableContainer, TableRow, Paper } from '@mui/material'
+import { Table, TableBody, TableCell, TableContainer, TableRow } from '@mui/material'
 
 import {
   metsaanFiTreeSpecies,
@@ -29,78 +29,72 @@ export const MetsaanPopup = (f: Feature) => {
     p.ditch_completed_at || p.ditchingyear ? `Completed at: {p.ditch_completion_date || p.ditchingyear}` : ''
 
   return (
-    <Paper>
-      <TableContainer>
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell>Main TableRowee species</TableCell>
-              <TableCell>{metsaanFiTreeSpecies[p.mainTableRoweespecies] || ''}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Average TableRowee age</TableCell>
-              <TableCell>{p.meanage} years</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Average TableRowee TableRowunk diameter</TableCell>
-              <TableCell>{p.meandiameter} cm</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Average TableRowee height</TableCell>
-              <TableCell>{p.meanheight} m</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Soil</TableCell>
-              <TableCell>{soilEn || soilFi || ''}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Area</TableCell>
-              <TableCell>{_.round(p.area, 3)} hectares</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Accessibility</TableCell>
-              <TableCell>{metsaanFiAccessibilityClassifier[p.accessibility] || ''}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Approx. TableRowee stem count</TableCell>
-              <TableCell>{_.round(p.stemcount * p.area)}</TableCell>
-            </TableRow>
-            {/* <TableRow><TableCell>TODO(Probably/Not/Yes/No): Mature enough for regeneration felling?</TableCell><TableCell>{
+    <Table sx={{ width: '500px' }}>
+      <TableBody>
+        <TableRow>
+          <TableCell>Main Tree species</TableCell>
+          <TableCell>{metsaanFiTreeSpecies[p.maintreespecies] || ''}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Average metsaanFiTreeSpecies age</TableCell>
+          <TableCell>{p.meanage} years</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Average tree trunk diameter</TableCell>
+          <TableCell>{p.meandiameter} cm</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Average tree height</TableCell>
+          <TableCell>{p.meanheight} m</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Soil</TableCell>
+          <TableCell>{soilEn || soilFi || ''}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Area</TableCell>
+          <TableCell>{_.round(p.area, 3)} hectares</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Accessibility</TableCell>
+          <TableCell>{metsaanFiAccessibilityClassifier[p.accessibility] || ''}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Approx. tree stem count</TableCell>
+          <TableCell>{_.round(p.stemcount * p.area)}</TableCell>
+        </TableRow>
+        {/* <TableRow><TableCell>TODO(Probably/Not/Yes/No): Mature enough for regeneration felling?</TableCell><TableCell>{
               p.regeneration_felling_prediction
             }</TableCell></TableRow>  */}
-            <TableRow>
-              <TableCell>Development class</TableCell>
-              <TableCell>{metsaanFiDevelopmentClass[p.developmentclass] || ''}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Fertility classifier</TableCell>
-              <TableCell>{metsaanFiFertilityClass[p.fertilityclass] || ''}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Main group</TableCell>
-              <TableCell>{metsaanFiMainGroups[p.maingroup] || ''}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Subgroup</TableCell>
-              <TableCell>{metsaanFiSubgroups[p.subgroup] || ''}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Ditching</TableCell>
-              <TableCell>{ditching}</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Data source</TableCell>
-              <TableCell>
-                {metsaanFiDatasources.filter((x: any) => x.id === p.datasource)[0].description || ''}
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Identifier</TableCell>
-              <TableCell>StandID={p.standid}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
+        <TableRow>
+          <TableCell>Development class</TableCell>
+          <TableCell>{metsaanFiDevelopmentClass[p.developmentclass] || ''}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Fertility classifier</TableCell>
+          <TableCell>{metsaanFiFertilityClass[p.fertilityclass] || ''}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Main group</TableCell>
+          <TableCell>{metsaanFiMainGroups[p.maingroup] || ''}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Subgroup</TableCell>
+          <TableCell>{metsaanFiSubgroups[p.subgroup] || ''}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Ditching</TableCell>
+          <TableCell>{ditching}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Data source</TableCell>
+          <TableCell>{metsaanFiDatasources.filter((x: any) => x.id === p.datasource)[0].description || ''}</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Identifier</TableCell>
+          <TableCell>StandID={p.standid}</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
   )
 }
