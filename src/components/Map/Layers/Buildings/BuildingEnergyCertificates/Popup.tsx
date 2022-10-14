f888dec2d3bf3996385f957e822babc1343dc79f
xbexbex <petja.valkama@gmail.com>
2022-10-14 13:53:21 +0300

    Add react popup for BuildingEnergyCertificates

---
+++ src/components/Map/Layers/Buildings/BuildingEnergyCertificates/Popup.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import Feature from 'ol/Feature'
+import { Table, TableBody, TableCell, TableRow } from '@mui/material'
+import _ from 'lodash'
+
+interface Props {
+  f: Feature
+}
+
+const popup = ({ f }: Props) => {
+  const p = f.getProperties()
+
+  const energyUse = p.e_luku * p.lämmitetty_nettoala
+  const energyPerVolume = p.i_raktilav
+    ? `<br/>Energy use per m³: ${_.round(energyUse / p.i_raktilav)} kWh per year`
+    : ''
+
+  const url = `https://www.energiatodistusrekisteri.fi/public_html?energiatodistus-id=${p.todistustunnus}&command=access&t=energiatodistus&p=energiatodistukset`
+
+  return (
+    <Table sx={{ width: '500px' }} size={'small'}>
+      <TableBody>
+        <TableRow>
+          <TableCell>Certificate ID</TableCell>
+          <TableCell>
+            <a href={url}>{p.todistustunnus}</a>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Total energy consumption</TableCell>
+          <TableCell>{_.round(energyUse)} years</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>Energy use per m²</TableCell>
+          <TableCell>
+            {p.e_luku} kWh per year {energyPerVolume}
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  )
+}
+
+export default popup
