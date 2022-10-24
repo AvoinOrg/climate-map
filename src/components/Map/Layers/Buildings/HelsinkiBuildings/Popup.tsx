8a645c3238c629c5dc67b18aaa8599adcc7060d5
Tomi Heikkinen <tomi@avoin.org>
2022-10-24 14:52:26 +0300

    add floor area, building data & heating type

--- src/components/Map/Layers/Buildings/HelsinkiBuildings/Popup.tsx
+++ src/components/Map/Layers/Buildings/HelsinkiBuildings/Popup.tsx
@@ -9,10 +9,12 @@ interface Props {
 
 const Popup = ({ features }: Props) => {
   let buildingIds: any = []
-  const tableValues: any = {}
+  const tableValues: any = {}  
 
   features.forEach((feature) => {
     const p = feature.getProperties()
+    // const buildingdate = p.c_valmpvm
+    //const resultdate = buildingdate?.toString().substr(0, 9) || '0000-0-0'
 
     buildingIds.push({ vtj_prt: p.vtj_prt, ratu: p.ratu })
 
@@ -22,6 +24,47 @@ const Popup = ({ features }: Props) => {
 
     if (p.postinumero != null) {
       tableValues['Zip Code'] = <address>{p.postinumero}</address>
+    }
+
+    if (p.i_kerrosala != null) {
+      tableValues['Floor area (m2)'] = <address>{p.i_kerrosala}</address>
+    }
+
+    if (p.c_valmpvm != null) {
+      var doccdate = p.c_valmpvm.toString().substr(0, 9)
+      var [yyyy, mm, dd] = doccdate.split("-");
+      var revdoccdate = dd + '-' + mm + '-' + yyyy;
+      tableValues['Date of completion of construction'] = <address>{revdoccdate}</address>
+    }
+
+    if (p.c_lammtapa != null) {
+      var bhsystem = p.c_lammtapa
+      switch (bhsystem) {
+        case '1': 
+        bhsystem = 'Water central heating';
+        break;
+
+        case '2': 
+        bhsystem = 'Central air heating';
+        break;
+
+        case '3': 
+        bhsystem = 'Direct electrical heating';
+        break;
+
+        case '4': 
+        bhsystem = 'Oven heating';
+        break;
+
+        case '5': 
+        bhsystem = 'No fixed heating device';
+        break;
+
+        default: 
+        bhsystem = "Unknown";
+
+      }
+      tableValues['Building Heating System'] = <address>{bhsystem}</address>
     }
 
     if (p.hakija != null || p.hakija_osoite != null || p.hakija_postinumero != null) {
