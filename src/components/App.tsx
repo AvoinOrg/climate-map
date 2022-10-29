8bfe1e9bc77ae72706183f529b4b9d3854cb6ee0
xbexbex <petja.valkama@gmail.com>
2022-10-29 18:23:12 +0300

    Add App.tsx

---
+++ src/components/App.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
+import { BrowserRouter } from 'react-router-dom'
+
+import { Sidebar } from 'Components/Sidebar'
+import { NavBar } from 'Components/NavBar'
+import { OverlayMessages } from 'Components/OverlayMessages'
+import { UserModal } from 'Components/Profile'
+import { Routes } from 'Components/Routing'
+import theme from 'Style/theme'
+import { UiStateProvider, UserStateProvider } from 'Components/State'
+import { MapProvider, GroupOrientation } from 'Components/Map'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <UiStateProvider>
+            <MapProvider>
+              <UserStateProvider>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline>
+                  <GroupOrientation />
+                  <NavBar />
+                  <OverlayMessages />
+                  <Sidebar>
+                    <Routes />
+                  </Sidebar>
+                  <UserModal />
+                </CssBaseline>
+              </UserStateProvider>
+            </MapProvider>
+          </UiStateProvider>
+        </ThemeProvider>
+      </StyledEngineProvider>
+    </BrowserRouter>
+  )
+}
+
+export default App
