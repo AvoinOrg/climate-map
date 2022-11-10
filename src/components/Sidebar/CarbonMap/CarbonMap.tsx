c7a358b67e95ed80da270866c3327981bef2f0f6
xbexbex <petja.valkama@gmail.com>
2022-11-10 14:40:56 +0200

    Add basic carbon calculation

--- src/components/Sidebar/CarbonMap/CarbonMap.tsx
+++ src/components/Sidebar/CarbonMap/CarbonMap.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import axios from 'axios'
+import { Button, Box } from '@mui/material'
+import shp from 'shpjs'
+
+// import { useFileUploadMutation } from 'Queries/carbon'
 
 const CarbonMap = () => {
-  return <div></div>
+  const [uploadFile, setUploadFile] = useState(null)
+  const [res, setRes] = useState(null)
 }
 
 export default CarbonMap
