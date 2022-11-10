fc1cef0c79f147472bf395738a6e2b55f330c69a
xbexbex <petja.valkama@gmail.com>
2022-11-10 14:42:45 +0200

    Add basic carbon calculation

--- src/components/Sidebar/CarbonMap/CarbonMap.tsx
+++ src/components/Sidebar/CarbonMap/CarbonMap.tsx
@@ -8,6 +8,35 @@ import shp from 'shpjs'
 const CarbonMap = () => {
   const [uploadFile, setUploadFile] = useState(null)
   const [res, setRes] = useState(null)
+  const handleSubmit = async () => {
+    const formData = new FormData()
+    formData.append('file', uploadFile)
+
+    // const response = await useFileUploadMutation("http://localhost:8000/calculate", uploadFile)
+
+    const response = await axios.post('http://localhost:8000/calculate', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+
+    setRes(response.data)
+  }
+  const handleFileInput = async (e: any) => {
+    setUploadFile(f)
+  }
+  return (
+    <Box sx={{ margin: '100px', display: 'flex', flexDirection: 'column' }}>
+      <Button variant="contained" component="label">
+        Select shapefile
+        <input hidden accept=".zip" multiple type="file" onChange={handleFileInput} />
+      </Button>
+      <Button onClick={handleSubmit} sx={{ margin: '10px 0 0 0' }}>
+        Calculate carbon
+      </Button>
+      {res && <p>{JSON.stringify(res)}</p>}
+    </Box>
+  )
 }
 
 export default CarbonMap
