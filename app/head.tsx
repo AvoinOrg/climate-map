ce1144a7f859ffb5a2f89d2c8d30a85577000b5e
xbexbex <petja.valkama@gmail.com>
2022-11-14 19:13:19 +0200

    Add basic app structure

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default async function Head() {
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </>
+  )
+}
