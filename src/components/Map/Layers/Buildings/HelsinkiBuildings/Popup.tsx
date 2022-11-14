6378f1b9f0471a38c851e444821e5c7865c6be11
Tomi Heikkinen <tomiheikkinen2@kamk.fi>
2022-11-14 15:24:30 +0200

    Helsinki-Testbed

--- src/components/Map/Layers/Buildings/HelsinkiBuildings/Popup.tsx
+++ src/components/Map/Layers/Buildings/HelsinkiBuildings/Popup.tsx
@@ -2,26 +2,20 @@ import React from 'react'
 import Feature from 'ol/Feature'
 import { Table, TableBody, TableCell, TableRow } from '@mui/material'
 import _ from 'lodash'
-// import energy_c from './json/energy_c.json'
-// console.log(energy_c[0][0])
+import { cloneDeep } from 'lodash'
+import './HBenergy.css'
+import consuption from "./json/energy_c.json"
 
-export const energyJson = {  
-  "0_1975": {"Oil": 70.917, "dis_heating": 66.272, "direct_heating":62.625, "awhpump":45.955, "ghpump":35.928},  
-  "1976_77": {"Oil": 64.053, "dis_heating": 59.901, "direct_heating": 56.309, "awhpump": 41.838, "ghpump": 32.857},   
-  "1978_84": {"Oil":55.573, "dis_heating":52.022, "direct_heating":48.554, "awhpump":36.507, "ghpump":28.958},
-  "1985_02": {"Oil":53.097, "dis_heating":49.738, "direct_heating":46.352, "awhpump":35.047, "ghpump":27.861},  
-  "2003_07": {"Oil":48.289, "dis_heating":45.339, "direct_heating":43.156, "awhpump":32.839, "ghpump":26.574},  
-  "2008_09": {"Oil":47.917, "dis_heating":44.999, "direct_heating":42.832, "awhpump":32.612, "ghpump":26.411},  
-  "2010_11": {"Oil":40.39, "dis_heating":38.03, "direct_heating":36.216, "awhpump":28.204, "ghpump":23.408},  
-  "2012_17": {"Oil":37.074, "dis_heating":34.92, "direct_heating":33.122, "awhpump":26.018, "ghpump":21.708},  
-  "2018_0": {"Oil":34.911, "dis_heating":32.903, "direct_heating":31.101, "awhpump":24.632, "ghpump":20.722}
-};
+const EnergyData = () => cloneDeep(consuption);
+// const loadData = [...jsonData];
 
-console.log(energyJson["0_1975"]["Oil"])
+console.log(EnergyData)
 
 fetch('./json/energy_c.json')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  .then((res) => res.json())
+  .then((data) => {
+    console.log('data:', data);
+  })
 
 interface Props {
   features: Feature[]
@@ -58,55 +52,60 @@ const Popup = ({ features }: Props) => {
     } 
     if (p.c_poltaine != null) {               
       tableValues['Fuel'] = <address>{p.c_poltaine}</address>
-    } 
+    }     
     // district heating
-    // if (p.c_kayttotark == 32 && p.c_kayttotark == 39 && p.c_poltaine == 1 && p.c_valmpvm !=null) {
-    if (p.c_kayttark == '032' && p.c_kayttotark == '039' && p.c_poltaine == 1 && p.c_valmpvm !=null) {  
+    const kktark = String(p.c_kayttark)
+    if (kktark == "032" || kktark == "039" && p.c_poltaine == 1 && p.c_valmpvm !=null) {    
       var tecdate = p.c_valmpvm.toString().substr(0, 4) 
+      // console.log(JSON.stringify(p.c_kayttark))
       var docctilav = p.i_raktilav 
+      console.log(docctilav)
       var tecons = 0
       switch (tecdate) {
         case tecdate <= 1975 && tecdate > 0: 
-        tecons = docctilav * energyJson["0_1975"]["dis_heating"]
+        tecons = docctilav * consuption["0_1975"]["dis_heating"]
+        console.log(consuption["0_1975"]["dis_heating"])
         break;
 
         case tecdate <= 1977 && tecdate >= 1976:  
-        tecons = docctilav * energyJson["1976_77"]["dis_heating"]
+        tecons = docctilav * consuption["1976_77"]["dis_heating"]
         break;
 
         case tecdate <= 1984 && tecdate >= 1978:  
-        tecons = docctilav * energyJson["1978_84"]["dis_heating"]
+        tecons = docctilav * consuption["1978_84"]["dis_heating"]
         break;
 
         case tecdate <= 2002 && tecdate >= 1985: 
-        tecons = docctilav * energyJson["1985_02"]["dis_heating"]
+        tecons = docctilav * consuption["1985_02"]["dis_heating"]
         break;
 
         case tecdate <= 2007 && tecdate >= 2003:  
-        tecons = docctilav * energyJson["2003_07"]["dis_heating"]
+        tecons = docctilav * consuption["2003_07"]["dis_heating"]
         break;
 
         case tecdate <= 2009 && tecdate >= 2008:  
-        tecons = docctilav * energyJson["2008_09"]["dis_heating"]
+        tecons = docctilav * consuption["2008_09"]["dis_heating"]
         break;
 
         case tecdate <= 2011 && tecdate >= 2010:  
-        tecons = docctilav * energyJson["2010_11"]["dis_heating"]
+        tecons = docctilav * consuption["2010_11"]["dis_heating"]
         break;
 
         case tecdate <= 2017 && tecdate >= 2012:  
-        tecons = docctilav * energyJson["2012_17"]["dis_heating"]
+        tecons = docctilav * consuption["2012_17"]["dis_heating"]
         break;
 
         case tecdate >= 2018:  
-        tecons = docctilav * energyJson["2018_0"]["dis_heating"]
+        tecons = docctilav * consuption["2018_0"]["dis_heating"]
         break;
 
         default: 
         tecons = 0
 
       }
-      tableValues['Estimated Annual Energy Consumption [kWh/m3,a]'] = <address>{tecons.toFixed(2)}</address>
+
+
+      tableValues['Estimated Annual Energy Consumption [kWh/m3,a]'] = <address id="color_energy">{tecons.toFixed(2)}</address>
     }    
 
     if (p.hakija != null || p.hakija_osoite != null || p.hakija_postinumero != null) {
