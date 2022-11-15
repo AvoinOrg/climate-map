beacd96773fd58ec40bad7707f2fe850e88d9a3a
xbexbex <petja.valkama@gmail.com>
2022-11-15 15:02:34 +0200

    Add page

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import React from 'react'
+import { MainMenu } from '#/components/Sidebar'
+
+const Page = ({
+  // Layouts must accept a children prop.
+  // This will be populated with nested layouts or pages
+  children,
+}: {
+  children: React.ReactNode
+}) => {
+  return <MainMenu />
+}
+
+export default Page
