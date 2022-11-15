2ff88d44047aa47823adc6e31149c40a458ac95b
xbexbex <petja.valkama@gmail.com>
2022-11-15 21:50:47 +0200

    Add next fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,12 @@
 import React from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider, StyledEngineProvider } from '@mui/material/styles'
+import { Raleway } from '@next/font/google'
+
+const raleway = Raleway({
+  weight: ['400', '500'],
+  subsets: ['latin'],
+})
 
 import { Sidebar } from '#/components/Sidebar'
 import { NavBar } from '#/components/NavBar'
@@ -22,24 +28,26 @@ const RootLayout = ({
   return (
     <html lang="en">
       <body>
-        <StyledEngineProvider injectFirst>
-          <ThemeProvider theme={theme}>
-            <UiStateProvider>
-              <MapProvider>
-                <UserStateProvider>
-                  {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                  <CssBaseline>
-                    <GroupOrientation />
-                    <NavBar />
-                    <OverlayMessages />
-                    <Sidebar>{children}</Sidebar>
-                    <UserModal />
-                  </CssBaseline>
-                </UserStateProvider>
-              </MapProvider>
-            </UiStateProvider>
-          </ThemeProvider>
-        </StyledEngineProvider>
+        <main className={raleway.className}>
+          <StyledEngineProvider injectFirst>
+            <ThemeProvider theme={theme}>
+              <UiStateProvider>
+                <MapProvider>
+                  <UserStateProvider>
+                    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                    <CssBaseline>
+                      <GroupOrientation />
+                      <NavBar />
+                      <OverlayMessages />
+                      <Sidebar>{children}</Sidebar>
+                      <UserModal />
+                    </CssBaseline>
+                  </UserStateProvider>
+                </MapProvider>
+              </UiStateProvider>
+            </ThemeProvider>
+          </StyledEngineProvider>
+        </main>
       </body>
     </html>
   )
