6a563f4283bc1c84a2d13728019a536587d950d1
xbexbex <petja.valkama@gmail.com>
2022-11-16 22:22:19 +0200

    Remove children props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,8 @@
 import React from 'react'
 import { MainMenu } from '#/components/Sidebar'
 
-const Page = ({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
-}: {
-  children: React.ReactNode
-}) => {
-  return <MainMenu>{children}</MainMenu>
+const Page = () => {
+  return <MainMenu />
 }
 
 export default Page
