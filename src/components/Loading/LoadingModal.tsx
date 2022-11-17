d3157505e57f1a221d9aae54fd6ea07e131b3c53
xbexbex <petja.valkama@gmail.com>
2022-11-17 16:01:21 +0200

    Refactor styles

--- src/components/Loading/LoadingModal.tsx
+++ src/components/Loading/LoadingModal.tsx
@@ -1,35 +1,26 @@
 import React from 'react'
-import { Theme } from '@mui/material/styles'
-import createStyles from '@mui/styles/createStyles'
-import makeStyles from '@mui/styles/makeStyles'
+import Box from '@mui/material/Box'
 import { CircularProgress } from '@mui/material'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      padding: '64px 10px 200px 10px',
-      zIndex: theme.zIndex.modal + 1,
-      backgroundColor: 'white',
-      overflowY: 'auto',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    pane: {},
-  })
-)
-
 export const LoadingModal = () => {
-  const classes = useStyles({})
-
   return (
-    <div className={classes.root}>
+    <Box
+      sx={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        padding: '64px 10px 200px 10px',
+        zIndex: 'zIndex.modal' + 1,
+        backgroundColor: 'white',
+        overflowY: 'auto',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
       <CircularProgress color="secondary" size={200} />
-    </div>
+    </Box>
   )
 }
