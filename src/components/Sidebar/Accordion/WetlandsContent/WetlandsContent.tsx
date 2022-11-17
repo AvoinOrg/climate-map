9a4bb56653865fdf5baef5307e3a0bf582b951e1
xbexbex <petja.valkama@gmail.com>
2022-11-17 20:51:38 +0200

    Update styling

--- src/components/Sidebar/Accordion/WetlandsContent/WetlandsContent.tsx
+++ src/components/Sidebar/Accordion/WetlandsContent/WetlandsContent.tsx
@@ -1,24 +1,14 @@
 import React from 'react'
-import createStyles from '@mui/styles/createStyles'
-import makeStyles from '@mui/styles/makeStyles'
+import { Box } from '@mui/material'
 import { AOAccordion } from '#/components/Sidebar/Accordion'
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    root: {
-      width: '100%',
-    },
-  })
-)
-
 const WetlandsContent = () => {
-  const classes = useStyles({})
   return (
-    <div className={classes.root}>
+    <Box sx={{ width: '100%' }}>
       <AOAccordion layerId={'fi_bogs'} label={'Bogs and swamps (Finland)'} content={null} />
       <AOAccordion layerId={'cifor_peatdepth'} label={'Tropical Peatlands'} content={null} />
       <AOAccordion layerId={'cifor_wetlands'} label={'Tropical Wetlands'} content={null} />
-    </div>
+    </Box>
   )
 }
 
