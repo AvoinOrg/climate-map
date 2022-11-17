a461ebe92de95bfbe599b31e8866759a7770beab
xbexbex <petja.valkama@gmail.com>
2022-11-17 16:01:30 +0200

    Refactor styles

--- src/components/NavBar/ActionButtons.tsx
+++ src/components/NavBar/ActionButtons.tsx
@@ -1,28 +1,13 @@
+'use client'
+
 import React from 'react'
 import Button from '@mui/material/Button'
-import { Theme } from '@mui/material/styles'
-import createStyles from '@mui/styles/createStyles'
-import makeStyles from '@mui/styles/makeStyles'
+import { styled } from '@mui/material/styles'
+import { Box } from '@mui/material'
+
 import { UiStateContext } from '../State'
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    buttonContainer: {
-      display: 'flex',
-      overflow: 'visible',
-    },
-    button: {
-      height: 40,
-      display: 'inline',
-      width: 90,
-      margin: '0 0 0 10px',
-      fontSize: '0.9rem',
-    },
-  })
-)
-
 const ActionButtons = () => {
-  const classes = useStyles({})
   const { modalState, setModalState }: any = React.useContext(UiStateContext)
 
   const handleLoginClick = () => {
@@ -42,26 +27,33 @@ const ActionButtons = () => {
   }
 
   return (
-    <div className={classes.buttonContainer}>
-      <Button
+    <Box sx={{ display: 'flex', overflow: 'visible' }}>
+      <StyledButton
         color="secondary"
-        className={classes.button}
         onClick={handleSignupClick}
         variant={modalState === 'signup' ? 'contained' : 'outlined'}
         disableElevation
       >
         Sign up
-      </Button>
-      <Button
+      </StyledButton>
+      <StyledButton
         color="secondary"
-        className={classes.button}
         onClick={handleLoginClick}
         variant={modalState === 'login' ? 'contained' : 'outlined'}
         disableElevation
       >
         Log in
-      </Button>
-    </div>
+      </StyledButton>
+    </Box>
   )
 }
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  height: 40,
+  display: 'inline',
+  width: 90,
+  margin: '0 0 0 10px',
+  fontSize: '0.9rem',
+}))
+
 export default ActionButtons
