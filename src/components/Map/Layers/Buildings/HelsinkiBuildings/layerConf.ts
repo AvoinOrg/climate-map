a666cb29cd5890f5ee648fc7aed50138a259c899
Tomi Heikkinen <tomiheikkinen2@kamk.fi>
2022-11-22 13:16:47 +0200

    create sourceNames[2]

--- src/components/Map/Layers/Buildings/HelsinkiBuildings/layerConf.ts
+++ src/components/Map/Layers/Buildings/HelsinkiBuildings/layerConf.ts
@@ -34,6 +34,16 @@ const getStyle = async (): Promise<MbStyle> => {
         bounds: [24, 59, 26, 61],
         attribution: '<a href="https://www.hel.fi">© City of Helsinki</a>',
       },
+      [sourceNames[2]]: {
+        type: 'vector',
+        tiles: ['https://server.avoin.org/data/map/helsinki-buildings/{z}/{x}/{y}.pbf'],
+        maxzoom: 14,
+        // Bounds source: https://koordinates.com/layer/4257-finland-11000000-administrative-regions/
+        // select ST_Extent(ST_Transform(ST_SetSRID(geom,3067), 4326))
+        // from "finland-11000000-administrative-regions" where kunta_ni1='Helsinki';
+        bounds: [32, 39],
+        attribution: '<a href="https://www.hel.fi">© City of Helsinki</a>',
+      },
     },
     layers: [
       {
@@ -124,6 +134,43 @@ const getStyle = async (): Promise<MbStyle> => {
         },
         BEFORE: 'LABEL',
       },
+      /* {
+        id: sourceNames[2] + '-fill',
+        source: sourceNames[2],
+        'source-layer': 'default',
+        type: 'fill',
+        paint: {
+          'fill-color': 'green',
+          'fill-opacity': fillOpacity,
+        },
+        BEFORE: 'FILL',
+      },
+      {
+        id: sourceNames[2] + '-outline',
+        source: sourceNames[2],
+        'source-layer': 'default',
+        type: 'line',
+        minzoom: 11,
+        paint: {
+          'line-opacity': 0.75,
+        },
+        BEFORE: 'OUTLINE',
+      },
+      {
+        id: sourceNames[2] + '-sym',
+        source: sourceNames[2],
+        'source-layer': 'default',
+        type: 'symbol',
+        minzoom: 16,
+        paint: {},
+        layout: {
+          'symbol-placement': 'point',
+          'text-font': ['Open Sans Regular'],
+          'text-size': 20,
+          'text-field': '',
+        },
+        BEFORE: 'LABEL',
+      }, */
     ],
   }
 }
