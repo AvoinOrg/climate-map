9389796823bdc6c391a3a9aec7c9a759a3144b7f
Tomi Heikkinen <tomiheikkinen2@kamk.fi>
2022-12-12 16:19:56 +0200

    add heating texts

---
+++ src/components/Map/Layers/Buildings/HelsinkiBuildings/constants.ts
@@ -0,0 +1,18 @@
+export const buildingHelBhsysClass: any = {
+    1: 'District heating',
+    2: 'Light fuel oil',
+    3: 'Heavy fuel oil',
+    4: 'Direct electric heating',
+    9: 'Air-to-water heat pumpu, Ground source heat pump etc',
+  }
+
+/*export const buildingHelEstimatedClass: any = {
+    1: 'Estimated yearly heating related CO2-emissions: [tCO2/a]',
+    2: 'Estimated yearly heating related CO2-emissions: [kgCO2/(m3,a)]',
+    3: 'Estimated yearly heating related CO2-emissions: [kgCO2/kWh]',
+    4: 'Energy consumption reduction potential by switching to GSHP: MWh/a',
+    5: 'CO2-emission reduction potential by switching to GSHP: tCO2/a', 
+    6: 'Energy consumption reduction potential by switching to AWHP: MWh/a', 
+    7: 'CO2-emission reduction potential by switching to AWHP: tCO2/a',    
+  }
+*/
