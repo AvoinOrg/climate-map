7f42c321874074466e9bff00be8a06e9fda787f5
xbexbex <petja.valkama@gmail.com>
2023-01-23 17:57:06 +0200

    Remove duplicate functions

--- src/utils/mapUtils.ts
+++ src/utils/mapUtils.ts
@@ -7,22 +7,6 @@ import { packColor } from 'ol/renderer/webgl/shaders'
 
 export const fillOpacity = 0.65
 
-export const roundToSignificantDigitsPos = (n: number, expr: Expression) => [
-  // Multiply back by true scale
-  '/',
-  // Round to two significant digits:
-  ['round', ['/', expr, ['^', 10, ['+', -n + 1, ['floor', ['log10', expr]]]]]],
-  ['^', 10, ['-', n - 1, ['floor', ['log10', expr]]]],
-]
-export const roundToSignificantDigits = (n: number, expr: Expression) => [
-  'case',
-  ['==', 0, expr],
-  0,
-  ['>', 0, expr],
-  ['*', -1, roundToSignificantDigitsPos(n, ['*', -1, expr])],
-  roundToSignificantDigitsPos(n, expr),
-]
-
 const defaultVectorStyles: any = {
   LineString: new Style({
     stroke: new Stroke({
