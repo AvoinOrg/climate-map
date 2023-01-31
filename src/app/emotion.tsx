b63c81cd64a416be8423eaa6f0e55b39c4bb6e09
xbexbex <petja.valkama@gmail.com>
2023-01-31 18:34:23 +0200

    Update emotion.tsx

--- src/app/emotion.tsx
+++ src/app/emotion.tsx
@@ -5,33 +5,46 @@ import { CacheProvider } from '@emotion/react'
 import createCache from '@emotion/cache'
 import { useServerInsertedHTML } from 'next/navigation'
 import { useState } from 'react'
-import { ThemeProvider } from '@mui/material/styles'
-
-import theme from '#/common/style/theme'
 
 const RootStyleRegistry = ({ children }: { children: JSX.Element }) => {
-  const [cache] = useState(() => {
-    const cache = createCache({ key: 'css' })
+  const [{ cache, flush }] = useState(() => {
+    const cache = createCache({ key: 'css', prepend: true })
     cache.compat = true
-    return cache
+    const prevInsert = cache.insert
+    let inserted: string[] = []
+    cache.insert = (...args) => {
+      const serialized = args[1]
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name)
+      }
+      return prevInsert(...args)
+    }
+    const flush = () => {
+      const prevInserted = inserted
+      inserted = []
+      return prevInserted
+    }
+    return { cache, flush }
   })
 
   useServerInsertedHTML(() => {
+    const names = flush()
+    if (names.length === 0) return null
+    let styles = ''
+    for (const name of names) {
+      styles += cache.inserted[name]
+    }
     return (
       <style
-        data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+        data-emotion={`${cache.key} ${names.join(' ')}`}
         dangerouslySetInnerHTML={{
-          __html: Object.values(cache.inserted).join(' '),
+          __html: styles,
         }}
       />
     )
   })
 
-  return (
-    <CacheProvider value={cache}>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </CacheProvider>
-  )
+  return <CacheProvider value={cache}>{children}</CacheProvider>
 }
 
 export default RootStyleRegistry
