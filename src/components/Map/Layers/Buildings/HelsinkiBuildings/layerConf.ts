04ca4f72461cfbde51ec8307d8b8a4a3e515c66e
Tomi Heikkinen <tomiheikkinen2@kamk.fi>
2023-02-02 14:58:41 +0200

    added mapboxgl.map template

--- src/components/Map/Layers/Buildings/HelsinkiBuildings/layerConf.ts
+++ src/components/Map/Layers/Buildings/HelsinkiBuildings/layerConf.ts
@@ -127,7 +127,28 @@ const getStyle = async (): Promise<MbStyle> => {
     ],
   }
 }
+/* 
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
 
+map.on('load', function() {
+  map.addSource('hel-buildings', {
+    'type': 'geojson',
+    'data': 'src/components/Map/Layers/Buildings/HelsinkiBuildings/geojson/Helsinki_rakennukset_039_022023.geojson'
+  });
+  map.addLayer({
+    'id': 'hel-layer',
+    'type': 'fill',
+    'source': 'hel-buildings',
+    'paint': {
+      'fill-color': '#088',
+      'fill-opacity': 0.5
+    }
+  });
+});
+*/
 const layerConf: LayerConf = { id: id, style: getStyle, popup: Popup }
 
 export default layerConf
