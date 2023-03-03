fd0147d6988631b8b87b45c2868130ef90f1620c
Tomi Heikkinen <tomiheikkinen2@kamk.fi>
2023-03-03 15:53:00 +0200

    Geoserver conf

---
+++ src/components/Map/Layers/Buildings/HelsinkiBuildings/layerConf_new.ts
@@ -0,0 +1,89 @@
+import { Style as MbStyle } from 'mapbox-gl'
+
+import _ from 'lodash'
+
+import { fillOpacity, roundToSignificantDigits } from '#/utils/mapUtils'
+import { LayerId, LayerConf } from '#/types/map'
+import { layerOptions } from './constants'
+import Popup from './Popup'
+
+const SERVER_URL = process.env.NEXT_PUBLIC_GEOSERVER_URL
+
+const id: LayerId = 'helsinki_buildings'
+
+const getStyle = async (): Promise<MbStyle> => {
+  const sources: any = {}
+  let layers: any = []
+
+  for (const layerId in layerOptions) {
+    const options = layerOptions[layerId]
+    sources[layerId] = {
+      type: 'vector',
+      scheme: 'tms',
+      tiles: [`${SERVER_URL}/gwc/service/tms/1.0.0/misc:${options.serverId}@EPSG:900913@pbf/{z}/{x}/{y}.pbf`],
+      minzoom: options.minzoom,
+      maxzoom: options.maxzoom,
+      bounds: [19, 59, 32, 71], // Finland
+      attribution: '<a href="https://www.hel.fi">© City of Helsinki</a>',
+      promoteId: 'id',
+    }
+
+    layers = [
+      ...layers,
+      {
+        id: `${layerId}-fill`,
+        source: layerId,
+        'source-layer': options.serverId,
+        type: 'fill',
+        paint: {
+          // 'fill-color': fiForestsAreaCO2FillColor(fiForestsCumulativeCO2eValueExpr),
+          'fill-opacity': layerId === 'parcel' ? 1 : fillOpacity,
+        },
+        ...(options.layerMinzoom != null && { minzoom: options.layerMinzoom }),
+        ...(options.layerMaxzoom != null && { maxzoom: options.layerMaxzoom }),
+        selectable: true,
+        multiSelectable: true,
+        BEFORE: 'FILL',
+      },
+      {
+        id: `${layerId}-outline`,
+        source: layerId,
+        'source-layer': options.serverId,
+        type: 'line',
+        paint: {
+          'line-opacity': 0.5,
+        },
+        ...(options.layerMinzoom != null && { minzoom: options.layerMinzoom }),
+        ...(options.layerMaxzoom != null && { maxzoom: options.layerMaxzoom }),
+        BEFORE: 'OUTLINE',
+      },
+      {
+        id: `${layerId}-highlighted`,
+        source: layerId,
+        'source-layer': options.serverId,
+        type: 'fill',
+        paint: {
+          'fill-outline-color': '#484896',
+          'fill-color': '#6e599f',
+          'fill-opacity': 0.4,
+        },
+        filter: ['in', 'id', ''],
+        BEFORE: 'OUTLINE',
+        ...(options.layerMinzoom != null && { minzoom: options.layerMinzoom }),
+        ...(options.layerMaxzoom != null && { maxzoom: options.layerMaxzoom }),
+      },   
+    ]
+  }
+  return {
+    version: 8,
+    name: id,
+    sources: sources,
+    layers: layers,
+  }
+}
+
+const layerConf: LayerConf = { id: id, style: getStyle, popup: Popup, useMb: true, }
+
+export default layerConf
+
+
