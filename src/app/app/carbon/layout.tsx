0b607fe1cfa32284ff3f99a1f9c76b28b7707d6a
xbexbex <petja.valkama@gmail.com>
2023-03-10 14:21:41 +0200

    Refactor layout and page

--- src/app/app/carbon/layout.tsx
+++ src/app/app/carbon/layout.tsx
@@ -1,11 +1,20 @@
 'use client'
 
 import React from 'react'
+import UploadButton from './components/UploadButton'
+import {  Box } from '@mui/material'
 
 import { AppStateProvider } from './state/AppState'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <AppStateProvider>{children}</AppStateProvider>
+  return (
+    <AppStateProvider>
+      <Box sx={{ padding: '150px 30px 100px 30px', minWidth: '300px', display: 'flex', flexDirection: 'column' }}>
+        <UploadButton></UploadButton>
+        {children}
+      </Box>
+    </AppStateProvider>
+  )
 }
 
 export default Layout
