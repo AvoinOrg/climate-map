4233a9e457b8b83c7fec0659b1063a8c5ffc9407
xbexbex <petja.valkama@gmail.com>
2023-03-10 14:09:19 +0200

    Move MainMenu to page

--- src/app/app/carbon/page.tsx
+++ src/app/app/carbon/page.tsx
@@ -1,16 +1,132 @@
 'use client'
 
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import axios from 'axios'
+import { Button, Box } from '@mui/material'
+import { styled } from '@mui/material/styles'
 
-import MainMenu from './components/MainMenu'
-import { AppStateProvider } from './state/AppState'
+// import { useFileUploadMutation } from 'Queries/carbon'
+import { MapContext } from '#/components/Map'
+import { getGeoJsonArea } from '#/common/utils/gis'
 
-const CarbonMap = () => {
+import { AppStateContext } from './state/AppState'
+import PlanListItem from './components/PlanListItem'
+
+const MainMenu = () => {
+  const [uploadFile, setUploadFile] = useState<string | Blob>('')
+  const [res, setRes] = useState(null)
+  const { addJSONLayer, setMapLibraryMode } = useContext(MapContext)
+  const { planConfs, updatePlanConf, addPlanConf } = useContext(AppStateContext)
+
+  useEffect(() => {
+    setMapLibraryMode('mapbox')
+  }, [])
+
+  const handleSubmit = async () => {
+    const formData = new FormData()
+    formData.append('file', uploadFile)
+
+    // const response = await useFileUploadMutation("http://localhost:8000/calculate", uploadFile)
+
+    const response = await axios.post('http://localhost:8000/calculate', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+
+    setRes(response.data)
+  }
+
+  const handleFileInput = async (e: any) => {
+    const f = e.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(f)
+
+    const initializePlan = (json: any) => {
+      addJSONLayer('userCarbonJson', '1', json, 'kaytto_tark', 'EPSG:3857')
+      const areaHa = getGeoJsonArea(json) / 10000
+      addPlanConf({ json: json, name: f.name, id: '1', areaHa: areaHa })
+    }
+
+    reader.onloadend = async () => {
+      if (reader.result != null) {
+        if (f.name.split('.').pop() === 'gpkg') {
+          if (typeof reader.result !== 'string') {
+            const { GeoPackageAPI, setSqljsWasmLocateFile } = await import('@ngageoint/geopackage')
+            setSqljsWasmLocateFile((file) => '/dyn/' + file)
+
+            const geopackage = await GeoPackageAPI.open(new Uint8Array(reader.result))
+            const featureTables = geopackage.getFeatureTables()
+            const featureDao = geopackage.getFeatureDao(featureTables[0])
+            // console.log(featureDao.columns)
+
+            // // get the info for the table
+            // const tableInfo = geopackage.getInfoForTable(featureDao)
+
+            // const srs = geopackage.getSrs(tableInfo.srs.id)
+            // const srsName = srs.organization.toUpperCase() + ':' + srs.organization_coordsys_id
+
+            const extract = async (geopackage: any, tableName: string): Promise<any> => {
+              const geoJson: any = {
+                type: 'FeatureCollection',
+                features: [],
+              }
+              const iterator = geopackage.iterateGeoJSONFeatures(tableName)
+              for (const feature of iterator) {
+                geoJson.features.push(feature)
+              }
+              return Promise.resolve(geoJson)
+            }
+
+            const json = await extract(geopackage, featureTables[0])
+            initializePlan(json)
+          } else {
+            console.error('reader.result is a string, not an ArrayBuffer')
+          }
+        } else {
+          const shp = (await import('shpjs')).default
+          const json = await shp(reader.result)
+          initializePlan(json)
+        }
+      }
+    }
+
+    setUploadFile(f)
+  }
+
   return (
-    <AppStateProvider>
-      <MainMenu />
-    </AppStateProvider>
+    <Box sx={{ padding: '150px 30px 100px 30px', minWidth: '300px', display: 'flex', flexDirection: 'column' }}>
+      <BigMenuButton variant="contained" component="label">
+        Tuo kaava
+        <input hidden accept=".zip, .gpkg" multiple type="file" onChange={handleFileInput} />
+      </BigMenuButton>
+
+      {planConfs != null && (
+        <>
+          <Box component="p">Omat kaavat</Box>
+          <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+            {planConfs.map((planConf) => (
+              <PlanListItem key={planConf.id} planConf={planConf} />
+            ))}
+          </Box>
+        </>
+      )}
+      {/* <BigMenuButton variant="contained">
+        Uusi kaava
+        <input hidden accept=".zip, .gpkg" multiple type="file" onChange={handleFileInput} />
+      </BigMenuButton> */}
+      {/* <BigMenuButton variant="outlined" onClick={handleSubmit}>
+        Laske hiilivarasto
+      </BigMenuButton> */}
+      {res && <p>{JSON.stringify(res)}</p>}
+    </Box>
   )
 }
 
-export default CarbonMap
+const BigMenuButton = styled(Button)<{ component?: string }>({
+  width: '300px',
+  height: '50px',
+  margin: '0 0 15px 0',
+})
+
+export default MainMenu
