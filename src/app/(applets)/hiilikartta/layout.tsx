01af2e5252691738cf3b57d93260578d5c04ba44
xbexbex <petja.valkama@gmail.com>
2023-03-10 17:40:31 +0200

    Update padding

--- src/app/(applets)/hiilikartta/layout.tsx
+++ src/app/(applets)/hiilikartta/layout.tsx
@@ -2,14 +2,14 @@
 
 import React from 'react'
 import UploadButton from './components/UploadButton'
-import {  Box } from '@mui/material'
+import { Box } from '@mui/material'
 
 import { AppStateProvider } from './state/AppState'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <AppStateProvider>
-      <Box sx={{ padding: '150px 30px 100px 30px', minWidth: '300px', display: 'flex', flexDirection: 'column' }}>
+      <Box sx={{ padding: '120px 30px 100px 50px', minWidth: '300px', display: 'flex', flexDirection: 'column' }}>
         <UploadButton></UploadButton>
         {children}
       </Box>
