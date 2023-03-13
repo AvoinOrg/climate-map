f71ca8d2860721c6af3079d8a5e82386d90efb1e
xbexbex <petja.valkama@gmail.com>
2023-03-13 18:00:04 +0200

    Add NavigationHeader

--- src/app/(applets)/hiilikartta/components/NavigationHeader.tsx
+++ src/app/(applets)/hiilikartta/components/NavigationHeader.tsx
@@ -3,21 +3,26 @@ import { Box } from '@mui/material'
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew'
 import Link from 'next/link'
 import { Link as MuiLink } from '@mui/material'
+import { useRouter, usePathname } from 'next/navigation'
 
-interface Props {
-  text: string
-  route: string
-}
+import { getRoute } from '#/common/utils/routing'
+import { routeTree } from '../routes'
 
-const NavigationHeader = ({ text, route }: Props) => {
-  const Title = () => <Box sx={(theme) => ({ typography: theme.typography.subtitle1 })}>{text}</Box>
+// interface Props {
+//   basePath: string
+// }
+
+const NavigationHeader = () => {
+  const paths = usePathname()
+  const router = useRouter()
+  const Title = () => <Box sx={(theme) => ({ typography: theme.typography.subtitle1 })}>{paths}</Box>
 
   return (
     <Box sx={(theme) => ({ minHeight: '25px', display: 'flex', flexDirection: 'row' })}>
-      {route ? (
-        <MuiLink href={route} sx={{ display: 'flex', color: 'inherit', textDecoration: 'none' }} component={Link}>
-          <Title />
+      {'asdf' ? (
+        <MuiLink href={'asdf'} sx={{ display: 'flex', color: 'inherit', textDecoration: 'none' }} component={Link}>
           <ArrowBackIosNewIcon sx={(theme) => ({ float: 'left', cursor: 'pointer' })}></ArrowBackIosNewIcon>
+          <Title />
         </MuiLink>
       ) : (
         <Title />
