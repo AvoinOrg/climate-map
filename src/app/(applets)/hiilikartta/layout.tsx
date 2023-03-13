aeb40a0c9d44aa11316de42747d95161837cdaa1
xbexbex <petja.valkama@gmail.com>
2023-03-13 18:04:32 +0200

    Import navigationHeader

--- src/app/(applets)/hiilikartta/layout.tsx
+++ src/app/(applets)/hiilikartta/layout.tsx
@@ -5,12 +5,14 @@ import UploadButton from './components/UploadButton'
 import { Box } from '@mui/material'
 
 import { AppStateProvider } from './state/AppState'
+import NavigationHeader from './components/NavigationHeader'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <AppStateProvider>
       <Box sx={{ padding: '120px 30px 100px 50px', minWidth: '300px', display: 'flex', flexDirection: 'column' }}>
         <UploadButton></UploadButton>
+        {/* <NavigationHeader></NavigationHeader> */}
         {children}
       </Box>
     </AppStateProvider>
