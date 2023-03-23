cd0835b7f52bfd27301833205d9079e7248d60a9
xbexbex <petja.valkama@gmail.com>
2023-03-23 15:46:01 +0200

    Move buttons to layout

--- src/app/(applets)/hiilikartta/layout.tsx
+++ src/app/(applets)/hiilikartta/layout.tsx
@@ -2,7 +2,8 @@
 
 import React from 'react'
 import UploadButton from './components/UploadButton'
-import { Box } from '@mui/material'
+import { Box, Button } from '@mui/material'
+import { styled } from '@mui/material/styles'
 
 import { AppStateProvider } from './state/AppState'
 import NavigationHeader from './components/NavigationHeader'
@@ -11,12 +12,19 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <AppStateProvider>
       <Box sx={{ padding: '120px 30px 100px 50px', minWidth: '300px', display: 'flex', flexDirection: 'column' }}>
-        <UploadButton></UploadButton>
+        <BigMenuButton variant="contained" component="label"></BigMenuButton>
+        <BigMenuButton variant="contained">Uusi kaava</BigMenuButton>
         {/* <NavigationHeader></NavigationHeader> */}
         {children}
       </Box>
     </AppStateProvider>
   )
 }
+
+const BigMenuButton = styled(Button)<{ component?: string }>({
+  width: '300px',
+  height: '60px',
+  margin: '0 0 15px 0',
+})
 
 export default Layout
