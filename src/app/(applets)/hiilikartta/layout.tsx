dc6d01cd5b241de00fe6693f9be9d675da5bff73
xbexbex <petja.valkama@gmail.com>
2023-03-23 15:46:34 +0200

    Add routing to import page

--- src/app/(applets)/hiilikartta/layout.tsx
+++ src/app/(applets)/hiilikartta/layout.tsx
@@ -5,14 +5,25 @@ import UploadButton from './components/UploadButton'
 import { Box, Button } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
+import { getRoute } from '#/common/utils/routing'
+
 import { AppStateProvider } from './state/AppState'
 import NavigationHeader from './components/NavigationHeader'
+import Link from 'next/link'
+import { routeTree } from './routes'
+import MuiLink from '@mui/material/Link'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <AppStateProvider>
       <Box sx={{ padding: '120px 30px 100px 50px', minWidth: '300px', display: 'flex', flexDirection: 'column' }}>
-        <BigMenuButton variant="contained" component="label"></BigMenuButton>
+        <MuiLink
+          href={getRoute(routeTree.base.import, routeTree)}
+          sx={{ display: 'flex', color: 'inherit', textDecoration: 'none' }}
+          component={Link}
+        >
+          <BigMenuButton variant="contained" component="label"></BigMenuButton>
+        </MuiLink>
         <BigMenuButton variant="contained">Uusi kaava</BigMenuButton>
         {/* <NavigationHeader></NavigationHeader> */}
         {children}
