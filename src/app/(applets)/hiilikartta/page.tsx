dc0d55e414452d74f49db78ccc5aef97d61eddf2
xbexbex <petja.valkama@gmail.com>
2023-03-27 17:48:32 +0300

    Add map mode selection

--- src/app/(applets)/hiilikartta/page.tsx
+++ src/app/(applets)/hiilikartta/page.tsx
@@ -1,14 +1,20 @@
 'use client'
 
-import React, { useContext, useState } from 'react'
-
+import React, { useContext, useEffect, useState } from 'react'
 import { Box } from '@mui/material'
+
+import { MapContext } from '#/components/Map'
 
 import { AppStateContext } from './state/AppState'
 import PlanListItem from './components/PlanListItem'
 
 const Page = () => {
   const { planConfs } = useContext(AppStateContext)
+  const { setMapLibraryMode } = useContext(MapContext)
+
+  useEffect(() => {
+    setMapLibraryMode('mapbox')
+  }, [])
 
   return (
     <>
