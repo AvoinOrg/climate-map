bd21402943d16b71d2e4197133aa3b72ccfccff2
xbexbex <petja.valkama@gmail.com>
2023-04-05 16:32:21 +0300

    Fix popup

--- src/components/Map/Layers/Forests/TreePlantations/Popup.tsx
+++ src/components/Map/Layers/Forests/TreePlantations/Popup.tsx
@@ -8,84 +8,82 @@ interface Props {
 }
 
 const Popup = ({ features }: Props) => {
-  if (!features.length) {
-    const p = features[0].properties || features[0]
-    const { image, spec_simp, type_text, area_ha, peat_ratio, avg_peatdepth } = p
-    const isPeat = peat_ratio >= 0.4
+  const p = features[0].properties || features[0]
+  const { image, spec_simp, type_text, area_ha, peat_ratio, avg_peatdepth } = p
+  const isPeat = peat_ratio >= 0.4
 
-    const imageObjs = []
-    const images: string[] = image
-      .replace(/\.(tif|img|_)/g, '')
-      .toUpperCase()
-      .split(/[,; ]+/)
+  const imageObjs = []
+  const images: string[] = image
+    .replace(/\.(tif|img|_)/g, '')
+    .toUpperCase()
+    .split(/[,; ]+/)
 
-    for (const x of images) {
-      if (!/LGN\d/.test(x)) {
-        continue
-      }
-      const base = x.replace(/LGN.*/, 'LGN0')
-      // Most of the source images seem to fall in these categories.
-
-      // Candidate URLs:
-      for (const z of [0, 1, 2]) {
-        imageObjs.push({ url: `https://earthexplorer.usgs.gov/metadata/12864/${base + z}/`, title: base + z })
-      }
+  for (const x of images) {
+    if (!/LGN\d/.test(x)) {
+      continue
     }
+    const base = x.replace(/LGN.*/, 'LGN0')
+    // Most of the source images seem to fall in these categories.
 
-    return (
-      <Box sx={{ lineHeight: 1 }}>
-        <p>
-          <b>Tree plantation (Global Forest Watch)</b>
-        </p>
-        <p>{spec_simp}</p>
-        <p>{type_text}</p>
-        {isPeat && <p>Tropical peatland</p>}
+    // Candidate URLs:
+    for (const z of [0, 1, 2]) {
+      imageObjs.push({ url: `https://earthexplorer.usgs.gov/metadata/12864/${base + z}/`, title: base + z })
+    }
+  }
 
-        <Table size={'small'}>
-          <TableBody>
-            {isPeat && (
-              <>
-                <TableRow>
-                  <TableCell>Average peat depth</TableCell>
-                  <TableCell>{avg_peatdepth.toFixed(1)} metres</TableCell>
-                </TableRow>
-              </>
-            )}
-            <TableRow>
-              <TableCell>Area</TableCell>
-              <TableCell>{pp(area_ha, 3)}</TableCell>
-            </TableRow>
-            {isPeat && (
+  return (
+    <Box sx={{ lineHeight: 1 }}>
+      <p>
+        <b>Tree plantation (Global Forest Watch)</b>
+      </p>
+      <p>{spec_simp}</p>
+      <p>{type_text}</p>
+      {isPeat && <p>Tropical peatland</p>}
+
+      <Table size={'small'}>
+        <TableBody>
+          {isPeat && (
+            <>
               <TableRow>
-                <TableCell>Emission reduction potential when ground water level is raised by 40 cm</TableCell>
-                <TableCell>{pp(19.4 * area_ha)} tons CO2e/year</TableCell>
+                <TableCell>Average peat depth</TableCell>
+                <TableCell>{avg_peatdepth.toFixed(1)} metres</TableCell>
               </TableRow>
-            )}
+            </>
+          )}
+          <TableRow>
+            <TableCell>Area</TableCell>
+            <TableCell>{pp(area_ha, 3)}</TableCell>
+          </TableRow>
+          {isPeat && (
             <TableRow>
-              <TableCell>Landsat source ID</TableCell>
-              <TableCell>
-                <code>{image}</code>
+              <TableCell>Emission reduction potential when ground water level is raised by 40 cm</TableCell>
+              <TableCell>{pp(19.4 * area_ha)} tons CO2e/year</TableCell>
+            </TableRow>
+          )}
+          <TableRow>
+            <TableCell>Landsat source ID</TableCell>
+            <TableCell>
+              <code>{image}</code>
+            </TableCell>
+          </TableRow>
+          {imageObjs.length > 0 && (
+            <TableRow>
+              <TableCell>Potential Landsat source images</TableCell>
+              <TableCell sx={{ lineHeight: 0.5 }}>
+                {imageObjs.map((imageObj) => (
+                  <p>
+                    <a target="_blank" href={imageObj.url}>
+                      {imageObj.title}
+                    </a>
+                  </p>
+                ))}
               </TableCell>
             </TableRow>
-            {imageObjs.length > 0 && (
-              <TableRow>
-                <TableCell>Potential Landsat source images</TableCell>
-                <TableCell sx={{ lineHeight: 0.5 }}>
-                  {imageObjs.map((imageObj) => (
-                    <p>
-                      <a target="_blank" href={imageObj.url}>
-                        {imageObj.title}
-                      </a>
-                    </p>
-                  ))}
-                </TableCell>
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </Box>
-    )
-  }
+          )}
+        </TableBody>
+      </Table>
+    </Box>
+  )
 }
 
 export default Popup
