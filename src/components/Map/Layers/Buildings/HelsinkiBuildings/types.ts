a983dbf754f4d7a0f65c43575f8f7afab1e2e3e7
Tomi Heikkinen <tomiheikkinen2@kamk.fi>
2023-04-06 11:33:51 +0300

    inc. cost savings

---
+++ src/components/Map/Layers/Buildings/HelsinkiBuildings/types.ts
@@ -0,0 +1,18 @@
+import { StringValueOf } from './general'
+
+export enum LayerLevel {
+    Helsinki_buildings = 'fi_miscs_helsinki_buildings',
+}
+export type LayerLevelStrings = StringValueOf<LayerLevel>
+
+export interface ILayerOption {    
+    serverId: string    
+    minzoom: number
+    maxzoom?: number
+    layerMinzoom?: number | null
+    layerMaxzoom?: number | null
+}
+  
+export type ILayerOptions = {
+    [key in LayerLevelStrings]: ILayerOption
+}
