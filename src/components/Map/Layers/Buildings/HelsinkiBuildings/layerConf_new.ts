198b217e07cc897ad83455219a1cb30a54f0d1d6
Tomi Heikkinen <tomiheikkinen2@kamk.fi>
2023-04-11 10:40:10 +0300

    new layerConf

---
+++ src/components/Map/Layers/Buildings/HelsinkiBuildings/layerConf_new.ts
@@ -0,0 +1,96 @@
+// import { Style as MbStyle } from 'mapbox-gl'
+
+import { Style as MbStyle } from 'mapbox-gl'
+
+import { fillOpacity, roundToSignificantDigitsExpr } from '#/common/utils/map'
+import { LayerId, LayerConf, ExtendedMbStyle } from '#/common/types/map'
+
+import _ from 'lodash'
+
+import { layerOptions } from './constants'
+import Popup from './Popup'
+
+const SERVER_URL = process.env.NEXT_PUBLIC_GEOSERVER_URL
+
+const id: LayerId = 'helsinki_buildings'
+
+const getStyle = async (): Promise<ExtendedMbStyle> => {
+  const sourceNames = ['helsinki_buildings']
+  const sources: any = {}
+  let layers: any = []
+
+for (const layerId in layerOptions) {
+  const options = layerOptions[layerId]
+  sources[layerId] = {
+      type: 'vector',
+      scheme: 'tms',
+      tiles: [`${SERVER_URL}/gwc/service/tms/1.0.0/misc:${options.serverId}@EPSG:900913@pbf/{z}/{x}/{y}.pbf`],
+      minzoom: options.minzoom,
+      maxzoom: options.maxzoom,
+      bounds: [19, 59, 32, 71], // Finland
+      attribution: '<a href="https://www.hel.fi">© City of Helsinki</a>',
+      promoteId: 'id',
+    }
+
+  layers = [
+    {
+        id: sourceNames[0] + '-fill',
+        source: sourceNames[0],
+        'source-layer': 'geometry',
+        type: 'fill',
+        paint: {
+          'fill-color': 'cyan',
+          'fill-opacity': fillOpacity,
+        },
+    },
+    {
+      id: sourceNames[0] + '-outline',
+      source: sourceNames[0],
+      'source-layer': 'geometry',
+      type: 'line',
+      minzoom: 11,
+      paint: {
+        'line-opacity': 0.75,
+      },
+    },
+    {
+      id: sourceNames[0] + '-symbol',
+      source: sourceNames[0],
+      'source-layer': 'geometry',
+      type: 'symbol',
+      minzoom: 16,
+      paint: {},
+      layout: {
+        'symbol-placement': 'point',
+        'text-font': ['Open Sans Regular'],
+        'text-size': 20,
+        'text-field': [
+          'case',
+          ['has', 'i_raktilav'],
+          [
+            'let',
+            'co2',
+            ['/', ['*', 15, ['to-number', ['get', 'i_raktilav'], 0]], 1000],
+            [
+              'concat',
+              roundToSignificantDigitsExpr(2, ['var', 'co2']), // kg -> tons
+              ' t CO2e/y',
+            ],
+          ],
+          '',
+        ],
+      },
+    },    
+  ]
+  }
+  return {
+    version: 8,
+    name: id,
+    sources: sources,
+    layers: layers,
+  }
+}   
+
+const layerConf: LayerConf = { id: id, style: getStyle, popup: Popup, useMb: true, }
+
+export default layerConf
