7418ebd3d34c823327cdca4b0804b84822275225
xbexbex <petja.valkama@gmail.com>
2023-04-19 15:43:41 +0300

    Add PopupDrawer

---
+++ src/components/Sidebar/PopupDrawer.tsx
@@ -0,0 +1,30 @@
+'use client'
+
+import React from 'react'
+import { Box, Collapse } from '@mui/material'
+
+interface Props {
+  open: boolean
+  children: React.ReactNode
+}
+
+const PopupDrawer = ({ open, children }: Props) => {
+  return (
+    <Collapse collapsedSize={0} orientation={'horizontal'} in={open}>
+      <Box
+        sx={{
+          // width: open ? 'auto' : 100,
+          overflowX: 'hidden',
+          // position: 'absolute',
+          // transition: `all ${TRANSITION_DURATION}ms cubic-bezier(0, 0, 0.2, 1) 0ms`,
+          transition: 'width 1s linear !important',
+          zIndex: 99999,
+        }}
+      >
+        {children}
+      </Box>
+    </Collapse>
+  )
+}
+
+export default PopupDrawer
