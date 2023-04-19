028c239af71699d59317f285776a681631ae81ff
xbexbex <petja.valkama@gmail.com>
2023-04-19 16:29:20 +0300

    Add SidebarHeader

---
+++ src/components/Sidebar/SidebarHeader.tsx
@@ -0,0 +1,24 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+import SidebarToggleButton from './SidebarToggleButton'
+
+interface Props {
+  children?: React.ReactNode
+  title: string
+}
+
+const SidebarHeader = ({ children, title }: Props) => {
+  return (
+    <Box sx={{ padding: '3px 24px 24px 24px', backgroundColor: 'tertiary.main' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+        <SidebarToggleButton sx={{ margin: '24px 0 0 0' }}></SidebarToggleButton>
+        <Typography sx={{ margin: '20px 0 0 0', width: '100%', textAlign: 'end' }} variant="h2">
+          {title}
+        </Typography>
+        {children}
+      </Box>
+    </Box>
+  )
+}
+
+export default SidebarHeader
--- src/components/Sidebar/index.ts
+++ src/components/Sidebar/index.ts
@@ -1,4 +1,7 @@
 import { Sidebar } from './Sidebar'
 import { MainMenu } from './MainMenu'
+import SidebarToggleButton from './SidebarToggleButton'
+import SidebarNavigation from './SidebarNavigation'
+import SidebarHeader from './SidebarHeader'
 
-export { MainMenu, Sidebar }
+export { MainMenu, Sidebar, SidebarToggleButton, SidebarHeader, SidebarNavigation }
