922c7f425523556436149ec5d215452b500df526
xbexbex <petja.valkama@gmail.com>
2023-04-24 18:07:27 +0300

    Remove redundant elements

--- src/app/(applets)/hiilikartta/layout.tsx
+++ src/app/(applets)/hiilikartta/layout.tsx
@@ -4,11 +4,7 @@ import React from 'react'
 import { Box, Button } from '@mui/material'
 import { styled } from '@mui/material/styles'
 
-import { getRoute } from '#/common/utils/routing'
-
-import Link from 'next/link'
 import { routeTree } from './common/routes'
-import MuiLink from '@mui/material/Link'
 import { SidebarHeader } from '#/components/Sidebar'
 import { BreadcrumbNav } from '../../../components/Sidebar'
 
@@ -18,28 +14,11 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
       <SidebarHeader title={'Hiilikartta'}>
         <BreadcrumbNav routeTree={routeTree}></BreadcrumbNav>
       </SidebarHeader>
-      <Box sx={{ padding: '120px 30px 100px 50px', minWidth: '400px', display: 'flex', flexDirection: 'column' }}>
-        <MuiLink
-          href={getRoute(routeTree.import, routeTree)}
-          sx={{ display: 'flex', color: 'inherit', textDecoration: 'none' }}
-          component={Link}
-        >
-          <BigMenuButton variant="contained" component="label">
-            Tuo kaava
-          </BigMenuButton>
-        </MuiLink>
-        <BigMenuButton variant="contained">Uusi kaava</BigMenuButton>
-        {/* <NavigationHeader></NavigationHeader> */}
+      <Box sx={{ padding: '120px 30px 100px 30px', minWidth: '400px', display: 'flex', flexDirection: 'column' }}>
         {children}
       </Box>
     </>
   )
 }
-
-const BigMenuButton = styled(Button)<{ component?: string }>({
-  width: '100%',
-  height: '60px',
-  margin: '0 0 15px 0',
-})
 
 export default Layout
