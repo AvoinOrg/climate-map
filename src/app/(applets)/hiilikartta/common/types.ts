bef40efa031a8ba72087a1eadf3e7a08d92443d4
xbexbex <petja.valkama@gmail.com>
2023-04-26 16:45:23 +0300

    Add NewPlanConf type

--- src/app/(applets)/hiilikartta/common/types.ts
+++ src/app/(applets)/hiilikartta/common/types.ts
@@ -1,5 +1,9 @@
-export type PlanConf = {
+export interface PlanConf extends NewPlanConf {
   id: string
+  created: number
+}
+
+export type NewPlanConf = {
   name: string
   areaHa: number
   json: any
