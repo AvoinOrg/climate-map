5d56a8c60d0f4bd6e7b2a6ee823552a17f3199c3
xbexbex <petja.valkama@gmail.com>
2023-04-26 16:45:42 +0300

    Update store

--- src/app/(applets)/hiilikartta/state/appStore.ts
+++ src/app/(applets)/hiilikartta/state/appStore.ts
@@ -1,17 +1,38 @@
-import { useState, useEffect } from 'react';
+import { generateShortId } from '#/common/utils/general'
+import { useState, useEffect } from 'react'
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
 
-import { PlanConf } from '../common/types'
+import { NewPlanConf, PlanConf } from '../common/types'
 
 interface AppState {
-  planConfs: PlanConf[]
+  planConfs: { [key: string]: PlanConf }
+  deletePlanConf: (planId: string) => void
+  addPlanConf: (newPlanConf: NewPlanConf) => Promise<PlanConf>
 }
 
 export const useAppStore = create<AppState>()(
   persist(
     (set, get) => ({
-      planConfs: [],
+      planConfs: {},
+      deletePlanConf: async (planId: string) => {
+        set((state) => {
+          const planConfs = { ...state.planConfs }
+          delete planConfs[planId]
+          return { planConfs }
+        })
+      },
+      addPlanConf: async (newPlanConf: NewPlanConf) => {
+        const id = generateShortId()
+        const created = new Date().getTime()
+        const planConf = { id, created, ...newPlanConf }
+        await set((state) => {
+          const planConfs = { ...state.planConfs }
+          planConfs[id] = planConf
+          return { planConfs }
+        })
+        return planConf
+      },
     }),
     {
       name: 'food-storage', // name of item in the storage (must be unique)
