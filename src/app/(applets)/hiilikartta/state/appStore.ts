041138d2759e267b676a3a0eaf7fe0a538096041
xbexbex <petja.valkama@gmail.com>
2023-04-27 17:21:29 +0300

    Add immer

--- src/app/(applets)/hiilikartta/state/appStore.ts
+++ src/app/(applets)/hiilikartta/state/appStore.ts
@@ -2,40 +2,40 @@ import { generateShortId } from '#/common/utils/general'
 import { useState, useEffect } from 'react'
 import { create } from 'zustand'
 import { persist, createJSONStorage } from 'zustand/middleware'
+import { immer } from 'zustand/middleware/immer'
 
 import { NewPlanConf, PlanConf } from '../common/types'
 
-interface AppState {
+type State = {
   planConfs: { [key: string]: PlanConf }
+}
+
+type Actions = {
   deletePlanConf: (planId: string) => void
   addPlanConf: (newPlanConf: NewPlanConf) => Promise<PlanConf>
 }
 
-export const useAppStore = create<AppState>()(
+export const useAppStore = create<State & Actions>()(
   persist(
-    (set, get) => ({
+    immer((set, get) => ({
       planConfs: {},
       deletePlanConf: async (planId: string) => {
         set((state) => {
-          const planConfs = { ...state.planConfs }
-          delete planConfs[planId]
-          return { planConfs }
+          delete state.planConfs[planId]
         })
       },
       addPlanConf: async (newPlanConf: NewPlanConf) => {
         const id = generateShortId()
         const created = new Date().getTime()
-        const planConf = { id, created, ...newPlanConf }
+        const planConf = { id, created, reports: {}, ...newPlanConf }
         await set((state) => {
-          const planConfs = { ...state.planConfs }
-          planConfs[id] = planConf
-          return { planConfs }
+          state.planConfs[id] = planConf
         })
         return planConf
       },
-    }),
+    })),
     {
-      name: 'food-storage', // name of item in the storage (must be unique)
+      name: 'hiilikarttaStore', // name of item in the storage (must be unique)
       storage: createJSONStorage(() => sessionStorage), // (optional) by default the 'localStorage' is used
     }
   )
