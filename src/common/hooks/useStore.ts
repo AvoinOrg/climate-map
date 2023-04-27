a2c334dc0737e98b12d0709a74c2dba3c738f10f
xbexbex <petja.valkama@gmail.com>
2023-04-27 18:27:35 +0300

    Add null if not yet initialized

--- src/common/hooks/useStore.ts
+++ src/common/hooks/useStore.ts
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 
 const useStore = <T, F>(store: (callback: (state: T) => unknown) => unknown, callback: (state: T) => F) => {
   const result = store(callback) as F
-  const [data, setData] = useState<F>()
+  const [data, setData] = useState<F | null>(null)
 
   useEffect(() => {
     setData(result)
