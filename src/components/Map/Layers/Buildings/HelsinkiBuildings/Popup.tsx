dfb679a9eceeeb36d00401822f771f1bf6c320f6
xbexbex <petja.valkama@gmail.com>
2023-06-17 13:49:25 +0300

    Fix formating

--- src/components/Map/Layers/Buildings/HelsinkiBuildings/Popup.tsx
+++ src/components/Map/Layers/Buildings/HelsinkiBuildings/Popup.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import { Table, TableBody, TableCell, TableRow } from '@mui/material'
 import { PopupProps } from '#/common/types/map'
-import _ from 'lodash'
+import { uniqWith, isEqual } from 'lodash-es'
 
 import { buildingHelBhsysClass, buildingHePaybackClass, energyConsumption } from './constants'
 
 // Variables
-let heatings, heatingsL, heatingsS, heatingsmS, tecons, teconss, teconsL, teconsS, teconsmS, tecdate, tecdate_st, emisfactord, emisfactords
+let heatings,
+  heatingsL,
+  heatingsS,
+  heatingsmS,
+  tecons,
+  teconss,
+  teconsL,
+  teconsS,
+  teconsmS,
+  tecdate,
+  tecdate_st,
+  emisfactord,
+  emisfactords
 const nulls = '000'
 // Energy prices
 const districtprice = 81
-const powerprice  = 100
+const powerprice = 100
 const interest_rate = 0.03
 // convert to float
 const convertToFloat = (a: string) => {
@@ -20,14 +32,14 @@ const convertToFloat = (a: string) => {
   return floatValue
 }
 // integer numbers:
-function numberWithISpaces(xa : any) {
-  return xa.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+function numberWithISpaces(xa: any) {
+  return xa.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
 }
 // floating numbers
 function numberWithFSpaces(xa: any) {
-  var parts = xa.toString().split(".");
-  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ");
-  return parts.join(".");
+  var parts = xa.toString().split('.')
+  parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+  return parts.join('.')
 }
 // Emission factors [kgCO2/kWh]
 const empdp = [0.255, 0.195, 0.104, 0.104, 0.104]
@@ -179,22 +191,22 @@ const Popup = ({ features }: PopupProps) => {
         teconsmS = 0
       }
 
-      let districthconsmp = teconsL/1000
-      let electenergyconspt = teconsS/1000
-      let electenergyconsptm = teconsmS/1000
+      let districthconsmp = teconsL / 1000
+      let electenergyconspt = teconsS / 1000
+      let electenergyconsptm = teconsmS / 1000
 
-      let yearcostbi = (districthconsmp*districtprice)+(electenergyconspt*powerprice)
-      let yearcostai = (electenergyconsptm*powerprice)
-      let savingsum = (Number(yearcostbi) - Number(yearcostai))
+      let yearcostbi = districthconsmp * districtprice + electenergyconspt * powerprice
+      let yearcostai = electenergyconsptm * powerprice
+      let savingsum = Number(yearcostbi) - Number(yearcostai)
 
       const n = 15
       let present_value, cumulative_yield, discretes
       cumulative_yield = 0
       // calculates the cumulative sum
       for (let i = 1; i <= n; i++) {
-          discretes = 1/(1+interest_rate)**i
-          present_value = discretes*Number(savingsum)
-          cumulative_yield += present_value
+        discretes = 1 / (1 + interest_rate) ** i
+        present_value = discretes * Number(savingsum)
+        cumulative_yield += present_value
       }
 
       tableValues['Estimated yearly heating related CO2-emissions: [tCO2/a]'] = (
@@ -202,24 +214,43 @@ const Popup = ({ features }: PopupProps) => {
       )
       tableValues['Estimated yearly heating related CO2-emissions: [kgCO2/(m3/a)]'] = <address>{heatings}</address>
       tableValues['Estimated yearly heating related CO2-emissions: [kgCO2/kWh]'] = (
-        <address>{emissionFactor(tecons, empdp[1]) + ' ' + nulls}</address>)
+        <address>{emissionFactor(tecons, empdp[1]) + ' ' + nulls}</address>
+      )
       // payback time
-      tableValues['From District Heat to Geothermal Heat (Cost Savings):'] = (<address></address>)
+      tableValues['From District Heat to Geothermal Heat (Cost Savings):'] = <address></address>
       tableValues['Before Investing: [Mwh/a, €/a]'] = (
-      <address>
-        <div>{buildingHePaybackClass[1]}: {districthconsmp.toFixed(3)}</div>
-        <div>{buildingHePaybackClass[2]}: {electenergyconspt.toFixed(3)}</div>
-        <div>{buildingHePaybackClass[3]}: {numberWithISpaces(yearcostbi.toFixed(0).toLocaleString())}</div>
-      </address>)
+        <address>
+          <div>
+            {buildingHePaybackClass[1]}: {districthconsmp.toFixed(3)}
+          </div>
+          <div>
+            {buildingHePaybackClass[2]}: {electenergyconspt.toFixed(3)}
+          </div>
+          <div>
+            {buildingHePaybackClass[3]}: {numberWithISpaces(yearcostbi.toFixed(0).toLocaleString())}
+          </div>
+        </address>
+      )
       tableValues['After Investing: [Mwh/a, €/a]'] = (
-      <address>
-        <div>{buildingHePaybackClass[1]}: 0</div>
-        <div>{buildingHePaybackClass[2]}: {electenergyconsptm.toFixed(3)}</div>
-        <div>{buildingHePaybackClass[3]}: {numberWithISpaces(Number(yearcostai).toFixed(0).toLocaleString())}</div>
-      </address>)
-      tableValues['Saving: [€]'] = (<address>{numberWithISpaces(savingsum.toFixed(0))}</address>)
-      tableValues['Price of district heating & Electricity: [€/MWh, snt/kWh]'] = (<address>{districtprice} & {powerprice/10}</address>)
-      tableValues['Cumulative Savings (15a / Rate 3%): [€]'] = (<address>{numberWithISpaces(cumulative_yield.toFixed(0).toLocaleString())}</address>)
+        <address>
+          <div>{buildingHePaybackClass[1]}: 0</div>
+          <div>
+            {buildingHePaybackClass[2]}: {electenergyconsptm.toFixed(3)}
+          </div>
+          <div>
+            {buildingHePaybackClass[3]}: {numberWithISpaces(Number(yearcostai).toFixed(0).toLocaleString())}
+          </div>
+        </address>
+      )
+      tableValues['Saving: [€]'] = <address>{numberWithISpaces(savingsum.toFixed(0))}</address>
+      tableValues['Price of district heating & Electricity: [€/MWh, snt/kWh]'] = (
+        <address>
+          {districtprice} & {powerprice / 10}
+        </address>
+      )
+      tableValues['Cumulative Savings (15a / Rate 3%): [€]'] = (
+        <address>{numberWithISpaces(cumulative_yield.toFixed(0).toLocaleString())}</address>
+      )
     }
     // Oil
     if (kktark == '032' || (kktark == '039' && p.c_poltaine == 2) || (p.c_poltaine == 3 && p.c_valmpvm != null)) {
@@ -348,7 +379,6 @@ const Popup = ({ features }: PopupProps) => {
       tableValues['CO2-emission reduction potential by switching to GSHP: tCO2/a'] = <address>{''}</address>
       tableValues['Energy consumption reduction potential by switching to AWHP: MWh/a'] = <address>{'AWHP = Air-to-water heat pump'}</address>
       tableValues['CO2-emission reduction potential by switching to AWHP: tCO2/a'] = <address>{''}</address>*/
-
     }
     if (p.hakija != null || p.hakija_osoite != null || p.hakija_postinumero != null) {
       tableValues['Demolition requested by'] = (
@@ -363,7 +393,7 @@ const Popup = ({ features }: PopupProps) => {
   })
 
   // remove duplicate building ids by comparing the objects
-  buildingIds = _.uniqWith(buildingIds, _.isEqual)
+  buildingIds = uniqWith(buildingIds, isEqual)
 
   const buildingIdString = Object.keys(buildingIds).reduce((prev: any, index: any) => {
     const curr = buildingIds[index]
@@ -382,8 +412,7 @@ const Popup = ({ features }: PopupProps) => {
 
   return (
     <Table sx={{ width: '500px' }} size={'small'}>
-      <TableBody
-      >
+      <TableBody>
         <TableRow>
           <TableCell>Building ID:</TableCell>
           <TableCell>{buildingIdString.slice(0, 10)}</TableCell>
