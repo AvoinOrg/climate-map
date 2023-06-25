257b0ae82c495a245e35cf1a1ab1a18a5ceafa7e
xbexbex <petja.valkama@gmail.com>
2023-06-25 13:36:06 +0300

    Remove context

--- src/components/NavBar/NavBarSearch.tsx
+++ src/components/NavBar/NavBarSearch.tsx
@@ -2,7 +2,7 @@
 
 import { observable, useObservable } from 'micro-observables'
 import React, { useRef, useEffect } from 'react'
-import { MapContext } from '#/components/Map'
+import { useMapStore } from '#/common/store'
 
 const DEFAULT_PLACEHOLDER = 'Look up location'
 
@@ -13,22 +13,26 @@ const searchPlaceholder = observable<Placeholder>(nullPlaceholder)
 
 export const setSearchPlaceholder = (x: Placeholder) => searchPlaceholder.set(x)
 
+// TODO: Fix search
+
 export const NavBarSearch: React.FC = () => {
-  const { map, getGeocoder, activeLayerGroupIds } = React.useContext(MapContext)
-  const { placeholder, layer } = useObservable(searchPlaceholder.readOnly())
-  const isActive = activeLayerGroupIds.filter((x) => x === layer).length > 0
+  // const { map, getGeocoder, activeLayerGroupIds } = React.useContext(MapContext)
+  // const activeLayerGroupIds = useMapStore((state) => state.activeLayerGroupIds)
+  // const { placeholder, layer } = useObservable(searchPlaceholder.readOnly())
+  // const isActive = activeLayerGroupIds.filter((x) => x === layer).length > 0
 
-  const geocoderSearchRef = useRef(null)
-  useEffect(() => {
-    const geocoder = getGeocoder()
-    if (geocoder) geocoderSearchRef.current.appendChild(geocoder.onAdd(map))
-  }, [geocoderSearchRef])
+  // const geocoderSearchRef = useRef(null)
+  // useEffect(() => {
+  //   const geocoder = getGeocoder()
+  //   if (geocoder) geocoderSearchRef.current.appendChild(geocoder.onAdd(map))
+  // }, [geocoderSearchRef])
 
-  useEffect(() => {
-    const geocoder = getGeocoder()
-    if (!geocoder) return
-    geocoder._inputEl.placeholder = isActive ? placeholder : DEFAULT_PLACEHOLDER
-  }, [geocoderSearchRef, isActive, placeholder, layer])
+  // useEffect(() => {
+  //   const geocoder = getGeocoder()
+  //   if (!geocoder) return
+  //   geocoder._inputEl.placeholder = isActive ? placeholder : DEFAULT_PLACEHOLDER
+  // }, [geocoderSearchRef, isActive, placeholder, layer])
 
-  return <div ref={geocoderSearchRef} />
+  // return <div ref={geocoderSearchRef} />
+  return null
 }
