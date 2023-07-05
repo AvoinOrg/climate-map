5dc912c15ce9cee999acd239a98855631d6a0515
xbexbex <petja.valkama@gmail.com>
2023-07-05 13:25:22 +0300

    Remove SidebarHeader from page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,10 @@
 'use client'
 
 import { MainMenu } from '#/components/Sidebar'
-import { SidebarToggleButton } from '#/components/Sidebar'
 
 const Page = () => {
   return (
     <>
-      <SidebarToggleButton></SidebarToggleButton>
       <MainMenu />
     </>
   )
