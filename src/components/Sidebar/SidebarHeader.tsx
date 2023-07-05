7ded9c9eae52ff4bb2914c551a297422e2e40289
xbexbex <petja.valkama@gmail.com>
2023-07-05 13:47:10 +0300

    Update styling

--- src/components/Sidebar/SidebarHeader.tsx
+++ src/components/Sidebar/SidebarHeader.tsx
@@ -9,9 +9,9 @@ interface Props {
 
 const SidebarHeader = ({ children, title }: Props) => {
   return (
-    <Box sx={{ padding: '3px 24px 24px 24px', backgroundColor: 'tertiary.main' }}>
+    <Box sx={{ p: 0, pr: 3, pb: 3, backgroundColor: 'tertiary.main' }}>
       <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-        <SidebarToggleButton sx={{ margin: '24px 0 0 0' }}></SidebarToggleButton>
+        <SidebarToggleButton />
         <Typography sx={{ margin: '20px 0 0 0', width: '100%', textAlign: 'end' }} variant="h2">
           {title}
         </Typography>
