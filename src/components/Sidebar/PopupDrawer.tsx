ff7ae148ffc7648e9846e81e793e411baf510a3a
xbexbex <petja.valkama@gmail.com>
2023-07-06 11:30:46 +0300

    Replace MUI collapse with css transition

--- src/components/Sidebar/PopupDrawer.tsx
+++ src/components/Sidebar/PopupDrawer.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React from 'react'
-import { Box, Collapse } from '@mui/material'
+import { Box } from '@mui/material'
 
 interface Props {
   open: boolean
@@ -10,20 +10,17 @@ interface Props {
 
 const PopupDrawer = ({ open, children }: Props) => {
   return (
-    <Collapse collapsedSize={0} orientation={'horizontal'} in={open}>
-      <Box
-        sx={(theme) => ({
-          // width: open ? 'auto' : 100,
-          overflowX: 'hidden',
-          // position: 'absolute',
-          // transition: `all ${TRANSITION_DURATION}ms cubic-bezier(0, 0, 0.2, 1) 0ms`,
-          transition: 'width 1s linear !important',
-          zIndex: theme.zIndex.popup,
-        })}
-      >
-        {children}
-      </Box>
-    </Collapse>
+    <Box
+      className="popup-drawer-container"
+      sx={(theme) => ({
+        transition: 'width 1s linear',
+        zIndex: theme.zIndex.popup,
+        width: open ? 'auto' : '0px',
+        display: 'flex',
+      })}
+    >
+      {open && children}
+    </Box>
   )
 }
 
