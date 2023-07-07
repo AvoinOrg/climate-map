61d6e9b507de6a39aa662a4aa9afcba4668aeee7
xbexbex <petja.valkama@gmail.com>
2023-07-07 14:25:30 +0300

    Add new icons and update styling

--- src/components/Map/MapButtons.tsx
+++ src/components/Map/MapButtons.tsx
@@ -3,15 +3,11 @@
 import React from 'react'
 import { styled } from '@mui/material/styles'
 import ButtonGroup from '@mui/material/ButtonGroup'
-import SatelliteIcon from '@mui/icons-material/Satellite'
-import ExploreIcon from '@mui/icons-material/Explore'
-import AddIcon from '@mui/icons-material/Add'
-import RemoveIcon from '@mui/icons-material/Remove'
-import GpsFixedIcon from '@mui/icons-material/GpsFixed'
-import EditIcon from '@mui/icons-material/Edit'
+import ExploreIcon from '@mui/icons-material/ExploreOutlined'
+import { Button } from '@mui/material'
 
 import { useMapStore } from '#/common/store'
-import { Button } from '@mui/material'
+import { Terrain, Bullseye, Minus, Plus } from '#/components/icons'
 
 export const MapButtons = () => {
   const mapToggleTerrain = useMapStore((state) => state.mapToggleTerrain)
@@ -19,47 +15,51 @@ export const MapButtons = () => {
   const mapZoomIn = useMapStore((state) => state.mapZoomIn)
   const mapZoomOut = useMapStore((state) => state.mapZoomOut)
   const mapRelocate = useMapStore((state) => state.mapRelocate)
-  const setIsDrawPolygon = useMapStore((state) => state.setIsDrawPolygon)
+  // const setIsDrawPolygon = useMapStore((state) => state.setIsDrawPolygon)
 
   return (
-    <IconGroup>
-      <ButtonGroup orientation="vertical">
+    <Box
+      sx={(theme) => ({
+        position: 'absolute',
+        top: theme.spacing(1),
+        right: theme.spacing(1),
+        zIndex: theme.zIndex.mobileStepper /* force this to be on top of the map */,
+      })}
+    >
+      <ButtonGroup orientation="horizontal">
         <StyledButton onClick={mapToggleTerrain} size="small">
-          <SatelliteIcon fontSize="small" />
+          <Terrain />
         </StyledButton>
         <StyledButton onClick={mapResetNorth} size="small">
-          <ExploreIcon fontSize="small" />
-        </StyledButton>
-        <StyledButton onClick={mapZoomIn} size="small">
-          <AddIcon fontSize="small" />
-        </StyledButton>
-        <StyledButton onClick={mapZoomOut} size="small">
-          <RemoveIcon fontSize="small" />
+          <ExploreIcon sx={{ fontSize: '27px' }} />
         </StyledButton>
         <StyledButton onClick={mapRelocate} size="small">
-          <GpsFixedIcon fontSize="small" />
+          <Bullseye />
         </StyledButton>
-        <StyledButton onClick={() => setIsDrawPolygon(true)} size="small">
+        <StyledButton onClick={mapZoomIn} size="small">
+          <Plus />
+        </StyledButton>
+        <StyledButton onClick={mapZoomOut} size="small">
+          <Minus />
+        </StyledButton>
+        {/* <StyledButton onClick={() => setIsDrawPolygon(true)} size="small">
           <EditIcon fontSize="small" />
-        </StyledButton>
+        </StyledButton> */}
       </ButtonGroup>
-    </IconGroup>
+    </Box>
   )
 }
 
 // TODO: theme styling
 const StyledButton = styled(Button)(({ theme }) => ({
-  color: '#333333',
-  backgroundColor: 'white',
+  color: theme.palette.neutral.darker,
+  backgroundColor: theme.palette.neutral.light,
+  border: 'none',
   '&:hover': {
-    backgroundColor: '#eeeeee',
+    backgroundColor: theme.palette.neutral.main,
+    border: 'none',
   },
 }))
 
 // TODO: theme styling
-const IconGroup = styled('div')(({ theme }) => ({
-  position: 'absolute',
-  top: theme.spacing(2),
-  right: theme.spacing(2),
-  zIndex: theme.zIndex.mobileStepper /* force this to be on top of the map */,
-}))
+const Box = styled('div')(({ theme }) => ({}))
