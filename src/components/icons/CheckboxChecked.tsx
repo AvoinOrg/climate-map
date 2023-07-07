2a96f566a291157b6ab0b82465806140fbf66a65
xbexbex <petja.valkama@gmail.com>
2023-07-07 14:18:08 +0300

    Add props to include fillColor

--- src/components/icons/CheckboxChecked.tsx
+++ src/components/icons/CheckboxChecked.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react'
 import type { SVGProps } from 'react'
-const SvgCheckboxChecked = (props: SVGProps<SVGSVGElement>) => (
+interface Props extends SVGProps<SVGSVGElement> {
+  fillColor?: string
+}
+
+const SvgCheckboxChecked = ({ fillColor = 'gray', ...props }): Props => (
   <svg xmlns="http://www.w3.org/2000/svg" width={23} height={23} fill="none" {...props}>
-    <path fill="#274AFF" fillOpacity={0.35} stroke="currentColor" strokeWidth={2} d="M1 1h21v21H1z" />
+    <path fill={fillColor} fillOpacity={0.35} stroke="currentColor" strokeWidth={2} d="M1 1h21v21H1z" />
     <path stroke="currentColor" strokeWidth={2} d="m5 10.5 5 5.5 8-9" />
   </svg>
 )
--- src/components/icons/CheckcircleChecked.tsx
+++ src/components/icons/CheckcircleChecked.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
 import type { SVGProps } from 'react'
-const SvgCheckcircleChecked = (props: SVGProps<SVGSVGElement>) => (
+
+interface Props extends SVGProps<SVGSVGElement> {
+  fillColor?: string
+}
+
+const SvgCheckcircleChecked = ({ fillColor = 'gray', ...props }): Props => (
   <svg xmlns="http://www.w3.org/2000/svg" width={23} height={23} fill="none" {...props}>
-    <circle cx={11.5} cy={11.5} r={10.5} fill="#274AFF" fillOpacity={0.35} stroke="currentColor" strokeWidth={2} />
+    <circle cx={11.5} cy={11.5} r={10.5} fill={fillColor} fillOpacity={0.35} stroke="currentColor" strokeWidth={2} />
     <path stroke="currentColor" strokeWidth={2} d="m5 10.5 5 5.5 8-9" />
   </svg>
 )
