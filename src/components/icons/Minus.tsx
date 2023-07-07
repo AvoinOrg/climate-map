84acd0be6c1417af61d9043f47e13bb00b7e326f
xbexbex <petja.valkama@gmail.com>
2023-07-07 14:10:31 +0300

    Update width and height to be visually consistent

--- src/components/icons/Minus.tsx
+++ src/components/icons/Minus.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
 import type { SVGProps } from 'react'
 const SvgMinus = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={21} height={2} fill="none" {...props}>
-    <path stroke="#000" strokeWidth={2} d="M.5 1h20" />
+  <svg xmlns="http://www.w3.org/2000/svg" width={19} height={2.5} fill="none" {...props}>
+    <path stroke="#000" strokeWidth={3} d="M.5 1h20" />
   </svg>
 )
 export default SvgMinus
