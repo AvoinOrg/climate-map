652fd6b4d3f0136765ef302d05184866ef00bac9
xbexbex <petja.valkama@gmail.com>
2023-07-17 13:33:20 +0300

    Add new public folder copying system
    The contents are copied from any
    "public" folders to the root public folder. This allows for better
    organization of the static files for various sub-apps.

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     appDir: true,
   },
   compiler: {
-    styledComponents: true
+    styledComponents: true,
   },
   modularizeImports: {
     '@mui/material': {
@@ -44,6 +44,29 @@ const nextConfig = {
       }),
       new CopyPlugin({
         patterns: [
+          {
+            // copy main public folder to the root public folder
+            from: path.join(__dirname, '/src/public/**/*'),
+            context: path.resolve(__dirname, 'src'),
+            // removes the relative path from the public folder
+            to: ({ context, absoluteFilename }) => {
+              let relativePath = path.relative(context, absoluteFilename)
+              let publicIndex = relativePath.indexOf('public') + 6 // To exclude 'public' from path
+              return path.join(__dirname, '/public', relativePath.slice(publicIndex))
+            },
+          },
+          {
+            // copy any app public folder to the root public folder
+            from: path.join(__dirname, '/src/app/**/public/**/*'),
+            context: path.resolve(__dirname, 'src/app'),
+            to: ({ context, absoluteFilename }) => {
+              let relativePath = path.relative(context, absoluteFilename)
+              let publicIndex = relativePath.indexOf('public') + 6 // To exclude 'public' from path
+              let newRelativePath = relativePath.slice(publicIndex)
+              let parentDirectory = path.basename(path.dirname(relativePath.slice(0, publicIndex)))
+              return path.join(__dirname, '/public', parentDirectory, newRelativePath)
+            },
+          },
           {
             // copy sql-wasm for the GPKG library used in carbon app
             from: path.join(__dirname, 'node_modules/rtree-sql.js/dist/sql-wasm.wasm'),
