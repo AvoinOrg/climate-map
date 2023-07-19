30e4340a3e3de7a7d9dc52bd7ab12ac1145d004f
xbexbex <petja.valkama@gmail.com>
2023-07-19 16:00:14 +0300

    Add popup window for login

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,10 +6,36 @@ import { styled } from '@mui/material/styles'
 
 import { useUIStore } from '#/common/store'
 
+const openWindow = () => {
+  const width = 500
+  const height = 600
+  const left = window.screen.width / 2 - width / 2
+  const top = window.screen.height / 2 - height / 2
+
+  const url = '/login' // replace with the actual Zitadel login URL
+  const options = `
+  toolbar=no,
+  location=no,
+  directories=no,
+  status=no,
+  menubar=no,
+  scrollbars=no,
+  resizable=no,
+  width=${width},
+  height=${height},
+  top=${top},
+  left=${left}
+`
+
+  window.open(url, '_blank', options)
+
+  window.open(url, '', options)
+}
+
 export const NavBar = () => {
   // const { isLoggedIn }: any = React.useContext(UserStateContext)
   const isSidebarOpen = useUIStore((state) => state.isSidebarOpen)
-  const setIsLoginModalOpen = useUIStore((state) => state.setIsLoginModalOpen)
+  // const setIsLoginModalOpen = useUIStore((state) => state.setIsLoginModalOpen)
   return (
     <>
       {isSidebarOpen && (
@@ -31,7 +57,7 @@ export const NavBar = () => {
           })}
         >
           <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-            <Button sx={{ color: 'neutral.lighter', typography: 'h3' }} onClick={() => setIsLoginModalOpen(true)}>
+            <Button sx={{ color: 'neutral.lighter', typography: 'h3' }} onClick={openWindow}>
               Kirjaudu
             </Button>
           </Box>
