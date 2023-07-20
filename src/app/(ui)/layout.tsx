871d150f686331d69320fc04d89a4a6ec89864cd
xbexbex <petja.valkama@gmail.com>
2023-07-20 16:45:32 +0300

    Add tolgee

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
 import { Box } from '@mui/material'
+import { Tolgee, DevTools, TolgeeProvider, FormatSimple } from '@tolgee/react'
 
 import theme from '#/common/style/theme'
 import { Sidebar } from '#/components/Sidebar'
@@ -15,6 +16,12 @@ import { LoginModal } from '#/components/Modal'
 // import { UserModal } from '#/components/Profile'
 // import { UiStateProvider, UserStateProvider } from '#/components/State'
 // import RootStyleRegistry from './emotion'
+
+const tolgee = Tolgee().use(DevTools()).use(FormatSimple()).init({
+  language: 'fi',
+  apiUrl: process.env.NEXT_PUBLIC_TOLGEE_API_URL,
+  apiKey: process.env.NEXT_PUBLIC_TOLGEE_API_KEY,
+})
 
 const Layout = ({
   // Layouts must accept a children prop.
@@ -27,23 +34,28 @@ const Layout = ({
     <ThemeProvider theme={theme}>
       {/* <UserStateProvider> */}
       <CssBaseline>
-        <Map>
-          {/* <UserModal /> */}
-          <Box
-            className="layout-container"
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              height: '100vh',
-              width: '100vw',
-              zIndex: 'drawer',
-            }}
-          >
-            <Sidebar>{children}</Sidebar>
-            <NavBar />
-          </Box>
-          <LoginModal></LoginModal>
-        </Map>
+        <TolgeeProvider
+          tolgee={tolgee}
+          fallback="Loading..." // loading fallback
+        >
+          <Map>
+            {/* <UserModal /> */}
+            <Box
+              className="layout-container"
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100vh',
+                width: '100vw',
+                zIndex: 'drawer',
+              }}
+            >
+              <Sidebar>{children}</Sidebar>
+              <NavBar />
+            </Box>
+            <LoginModal></LoginModal>
+          </Map>
+        </TolgeeProvider>
       </CssBaseline>
       {/* </UserStateProvider> */}
     </ThemeProvider>
