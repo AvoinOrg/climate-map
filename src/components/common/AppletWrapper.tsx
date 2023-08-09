d1e14030efcc56a32001050bd12e83c8341accc1
xbexbex <petja.valkama@gmail.com>
2023-08-09 15:57:34 +0300

    Add support for tolgee and SidebarHeader

--- src/components/common/AppletWrapper.tsx
+++ src/components/common/AppletWrapper.tsx
@@ -4,25 +4,67 @@
 
 'use client'
 
-import React, { useEffect } from 'react'
-import { useMapStore } from '#/common/store'
+import React, { useEffect, useLayoutEffect } from 'react'
+import { useMapStore, useUIStore } from '#/common/store'
 import { MapContext } from '#/common/types/map'
+import { useTolgee } from '@tolgee/react'
 
 const AppletWrapper = ({
   children,
   mapContext,
+  localizationNamespace,
+  defaultLanguage,
+  SidebarHeaderElement,
 }: {
   children: React.ReactNode
   mapContext: MapContext
+  localizationNamespace?: string
+  defaultLanguage?: string
+  SidebarHeaderElement?: React.JSX.Element
 }) => {
+  const tolgee = useTolgee(['update'])
+
   const setMapContext = useMapStore((state) => state.setMapContext)
   const stateMapContext = useMapStore((state) => state.mapContext)
+  const setSidebarHeaderElement = useUIStore(
+    (state) => state.setSidebarHeaderElement
+  )
+
+  useEffect(() => {
+    if (tolgee.isLoaded()) {
+      localizationNamespace != null && tolgee.addActiveNs(localizationNamespace)
+      defaultLanguage != null && tolgee.changeLanguage(defaultLanguage)
+    }
+  }, [tolgee.isLoaded()])
+
+  useLayoutEffect(() => {
+    if (SidebarHeaderElement != null && setSidebarHeaderElement != null) {
+      setSidebarHeaderElement(SidebarHeaderElement)
+    }
+  }, [setSidebarHeaderElement, SidebarHeaderElement])
 
   useEffect(() => {
     setMapContext(mapContext)
+
+    return () => {
+      tolgee.removeActiveNs(localizationNamespace)
+    }
   }, [])
 
-  return <>{stateMapContext === mapContext && children}</>
+  const isTolgeeReady = () => {
+    if (
+      localizationNamespace != null &&
+      tolgee
+        .getAllRecords()
+        .some((item) => item.namespace === localizationNamespace)
+    ) {
+      return false
+    }
+
+    return true
+  }
+
+  return <>{stateMapContext === mapContext && isTolgeeReady() && children}</>
 }
 
 export default AppletWrapper
