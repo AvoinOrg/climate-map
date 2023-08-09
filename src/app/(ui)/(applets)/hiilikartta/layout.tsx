9d367081a3c1fd82d6b801ca694bc6b4e2f667b5
xbexbex <petja.valkama@gmail.com>
2023-08-09 16:14:18 +0300

    Add AppletWrapper to layout

--- src/app/(ui)/(applets)/hiilikartta/layout.tsx
+++ src/app/(ui)/(applets)/hiilikartta/layout.tsx
@@ -1,61 +1,41 @@
 'use client'
 
-import React, { useEffect, useLayoutEffect } from 'react'
+import React from 'react'
 import { Box } from '@mui/material'
 
 import { routeTree } from './common/routes'
 import { SidebarHeader } from '#/components/Sidebar'
 import { BreadcrumbNav } from '#/components/Sidebar'
-import { useUIStore } from '#/common/store'
-import { useTolgee } from '@tolgee/react'
+import AppletWrapper from '#/components/common/AppletWrapper'
 
-const tolgeeNs = 'hiilikartta'
-const tolgeeLang = 'fi'
+const localizationNamespace = 'hiilikartta'
+const defaultLanguage = 'fi'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  const tolgee = useTolgee(["update"])
-  const setSidebarHeaderElement = useUIStore(
-    (state) => state.setSidebarHeaderElement
-  )
-
   const SidebarHeaderElement = (
     <SidebarHeader title={'Hiilikartta'}>
       <BreadcrumbNav routeTree={routeTree}></BreadcrumbNav>
     </SidebarHeader>
   )
 
-  useEffect(() => {
-    if (tolgee.isLoaded()) {
-      tolgee.addActiveNs(tolgeeNs)
-      tolgee.changeLanguage(tolgeeLang)
-    }
-  }, [tolgee.isLoaded()])
-
-  useLayoutEffect(() => {
-    if (setSidebarHeaderElement != null) {
-      setSidebarHeaderElement(SidebarHeaderElement)
-    }
-  }, [setSidebarHeaderElement])
-
   return (
-    <>
-      {tolgee
-        .getAllRecords()
-        .some(
-          (item) => item.namespace === tolgeeNs && item.language === tolgeeLang
-        ) && (
-        <Box
-          sx={{
-            padding: '35px 30px 100px 30px',
-            width: '400px',
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          {children}
-        </Box>
-      )}
-    </>
+    <AppletWrapper
+      mapContext={'hiilikartta'}
+      localizationNamespace={localizationNamespace}
+      defaultLanguage={defaultLanguage}
+      SidebarHeaderElement={SidebarHeaderElement}
+    >
+      <Box
+        sx={{
+          padding: '35px 30px 100px 30px',
+          width: '400px',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        {children}
+      </Box>
+    </AppletWrapper>
   )
 }
 
