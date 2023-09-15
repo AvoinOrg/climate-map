c1f9dc04aa33502bb662ab95c5bfc386333c5b86
xbexbex <petja.valkama@gmail.com>
2023-09-15 14:44:57 +0300

    Add sx to SidebarHeader

--- src/components/Sidebar/SidebarHeader.tsx
+++ src/components/Sidebar/SidebarHeader.tsx
@@ -5,9 +5,10 @@ import SidebarToggleButton from './SidebarToggleButton'
 interface Props {
   children?: React.ReactNode
   title: string
+  sx?: any
 }
 
-const SidebarHeader = ({ children, title }: Props) => {
+const SidebarHeader = ({ children, title, sx }: Props) => {
   return (
     <Box
       sx={{
@@ -19,6 +20,7 @@ const SidebarHeader = ({ children, title }: Props) => {
         border: 1,
         borderColor: 'primary.dark',
         flexDirection: 'column',
+        ...sx
       }}
     >
       <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', flexGrow: 1 }}>
