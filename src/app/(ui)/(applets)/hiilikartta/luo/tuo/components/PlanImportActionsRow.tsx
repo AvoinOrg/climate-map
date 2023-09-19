e2e3c27940792a24a32e2f16e908b95276158e06
xbexbex <petja.valkama@gmail.com>
2023-09-19 15:08:09 +0300

    Add PlanImportActionsRow
    As both Gpkg and Shp init components use the same
    buttons for accept and cancel, those buttons are now in a new component.

---
+++ src/app/(ui)/(applets)/hiilikartta/luo/tuo/components/PlanImportActionsRow.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Box, IconButton } from '@mui/material'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
+
+const InitActionsRow = ({ onClickNext }: { onClickNext: () => void }) => {
+  return (
+    <Box
+      sx={(theme) => ({
+        minHeight: '25px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        margin: '40px 0 60px 0',
+      })}
+    >
+      <IconButton onClick={onClickNext}>
+        <ArrowForwardIosIcon
+          sx={(theme) => ({ float: 'right', cursor: 'pointer' })}
+        ></ArrowForwardIosIcon>
+      </IconButton>
+    </Box>
+  )
+}
+
+export default InitActionsRow
