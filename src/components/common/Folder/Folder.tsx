d9e3d461affc4f49e20cd0ba1fd01e06781cdd60
xbexbex <petja.valkama@gmail.com>
2023-09-25 13:41:51 +0300

    Remove redundant imports

--- src/components/common/Folder/Folder.tsx
+++ src/components/common/Folder/Folder.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Box } from '@mui/material'
-import { styled } from '@mui/material/styles'
 import type { SxProps, Theme } from '@mui/system'
 import SvgFolder from './SvgFolder'
 import { useTheme } from '@mui/material/styles'
