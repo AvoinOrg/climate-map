311514daf95a332f9d3b4de1bd5e1aed0db514e3
xbexbex <petja.valkama@gmail.com>
2023-09-25 13:41:03 +0300

    Update styling

--- src/components/common/Folder/Folder.tsx
+++ src/components/common/Folder/Folder.tsx
@@ -4,24 +4,7 @@ import { styled } from '@mui/material/styles'
 import type { SxProps, Theme } from '@mui/system'
 import SvgFolder from './SvgFolder'
 import { useTheme } from '@mui/material/styles'
-
-const StyledBox = styled(Box)(({ theme }) => ({
-  position: 'relative',
-  width: '100%',
-  display: 'inline-block',
-  '& svg': {
-    width: '100%',
-    height: 'auto',
-    display: 'block',
-  },
-  '& > div.content': {
-    position: 'absolute',
-    top: 0, // Adjust accordingly to position content inside folder SVG
-    right: 0,
-    left: 0,
-    overflow: 'auto',
-  },
-}))
+import { resolveColor } from '#/common/utils/styling'
 
 type Props = {
   children?: React.ReactNode
@@ -29,23 +12,57 @@ type Props = {
 }
 
 const Folder = ({ children, sx }: Props) => {
-  let color = useTheme().palette.neutral.lighter
-  let borderColor = useTheme().palette.neutral.main
+  const theme = useTheme()
+  let color = theme.palette.neutral.darker
+  let borderColor = theme.palette.neutral.main
+  let backgroundColor = theme.palette.neutral.lighter
 
   if (sx && typeof sx === 'object') {
     if ('color' in sx && typeof sx.color === 'string') {
-      color = sx.color
+      color = resolveColor(sx.color, theme)
     }
     if ('borderColor' in sx && typeof sx.borderColor === 'string') {
-      borderColor = sx.borderColor
+      borderColor = resolveColor(sx.borderColor, theme)
+    }
+    if ('backgroundColor' in sx && typeof sx.backgroundColor === 'string') {
+      backgroundColor = resolveColor(sx.backgroundColor, theme)
     }
   }
 
+  if (sx && 'backgroundColor' in sx) {
+    delete sx.backgroundColor
+  }
+
   return (
-    <StyledBox sx={sx}>
-      <SvgFolder color={color} borderColor={borderColor} />
-      <div className="content">{children}</div>
-    </StyledBox>
+    <Box
+      sx={{
+        position: 'relative',
+        width: '100%',
+        display: 'inline-block',
+        ...mainStyles,
+      }}
+    >
+      <SvgFolder
+        sx={{ width: '100%', height: 'auto', display: 'block' }}
+        color={backgroundColor}
+        borderColor={borderColor}
+      />
+      <Box
+        sx={{
+          position: 'absolute',
+          top: 0, // Adjust accordingly to position content inside folder SVG
+          right: 0,
+          left: 0,
+          overflow: 'auto',
+          p: 1,
+          pt: 3,
+          ...paddingStyles,
+        }}
+        className="folder-content"
+      >
+        {children}
+      </Box>
+    </Box>
   )
 }
 
