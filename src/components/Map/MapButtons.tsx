6801534d83a9a1bd34015814489aeeb3024691bf
xbexbex <petja.valkama@gmail.com>
2023-10-05 14:03:40 +0300

    Add DrawButtons

--- src/components/Map/MapButtons.tsx
+++ src/components/Map/MapButtons.tsx
@@ -4,10 +4,21 @@ import React from 'react'
 import { styled } from '@mui/material/styles'
 import ButtonGroup from '@mui/material/ButtonGroup'
 import ExploreIcon from '@mui/icons-material/ExploreOutlined'
+import DoneIcon from '@mui/icons-material/Done'
 import { Box, Button } from '@mui/material'
 
 import { useMapStore } from '#/common/store'
-import { Terrain, Bullseye, Minus, Plus } from '#/components/icons'
+import { useDrawMode } from '#/common/hooks/map/useDrawMode'
+import {
+  Terrain,
+  Bullseye,
+  Minus,
+  Plus,
+  Polygon,
+  EditDocument,
+} from '#/components/icons'
+import { useIsDrawEnabled } from '#/common/hooks/map/useIsDrawEnabled'
+import { useAllowedDrawModes } from '#/common/hooks/map/useAllowedDrawModes'
 
 export const MapButtons = () => {
   const mapToggleTerrain = useMapStore((state) => state.mapToggleTerrain)
@@ -15,6 +26,11 @@ export const MapButtons = () => {
   const mapZoomIn = useMapStore((state) => state.mapZoomIn)
   const mapZoomOut = useMapStore((state) => state.mapZoomOut)
   const mapRelocate = useMapStore((state) => state.mapRelocate)
+  const setDrawMode = useMapStore((state) => state.setDrawMode)
+  const disableDraw = useMapStore((state) => state.disableDraw)
+  const drawMode = useDrawMode()
+  const isDrawEnabled = useIsDrawEnabled()
+  const allowedDrawModes = useAllowedDrawModes()
   // const setIsDrawPolygon = useMapStore((state) => state.setIsDrawPolygon)
 
   return (
@@ -23,10 +39,39 @@ export const MapButtons = () => {
         position: 'absolute',
         top: theme.spacing(1),
         right: theme.spacing(1),
-        zIndex: theme.zIndex.mobileStepper /* force this to be on top of the map */,
+        display: 'flex',
+        flexDirection: 'row',
+        zIndex:
+          theme.zIndex.mobileStepper /* force this to be on top of the map */,
       })}
     >
-      <ButtonGroup orientation="horizontal">
+      {isDrawEnabled && (
+        <StyledButtonGroup
+          orientation="horizontal"
+          sx={{ height: '100%', mr: 1 }}
+        >
+          {drawMode != null && (
+            <StyledButton onClick={() => disableDraw()} size="small">
+              <DoneIcon />
+            </StyledButton>
+          )}
+          {allowedDrawModes.includes('edit') && (
+            <StyledButton
+              onClick={() => setDrawMode('edit')}
+              size="small"
+              disabled={drawMode === 'edit'}
+            >
+              <EditDocument />
+            </StyledButton>
+          )}
+          {allowedDrawModes.includes('polygon') && (
+            <StyledButton onClick={() => setDrawMode('polygon')} size="small">
+              <Polygon />
+            </StyledButton>
+          )}
+        </StyledButtonGroup>
+      )}
+      <StyledButtonGroup orientation="horizontal" sx={{ height: '100%' }}>
         <StyledButton onClick={mapToggleTerrain} size="small">
           <Terrain />
         </StyledButton>
@@ -42,10 +87,7 @@ export const MapButtons = () => {
         <StyledButton onClick={mapZoomOut} size="small">
           <Minus />
         </StyledButton>
-        {/* <StyledButton onClick={() => setIsDrawPolygon(true)} size="small">
-          <EditIcon fontSize="small" />
-        </StyledButton> */}
-      </ButtonGroup>
+      </StyledButtonGroup>
     </Box>
   )
 }
