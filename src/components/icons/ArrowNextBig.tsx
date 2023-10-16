f773f1656bce8343284fe57b20d322106588aa78
xbexbex <petja.valkama@gmail.com>
2023-10-16 13:17:32 +0300

    Reformat

--- src/components/icons/ArrowNextBig.tsx
+++ src/components/icons/ArrowNextBig.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react'
 import type { SVGProps } from 'react'
+
 const SvgArrowNextBig = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={33} height={25} fill="none" {...props}>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={33}
+    height={25}
+    fill="none"
+    {...props}
+  >
     <g stroke="currentColor" strokeWidth={2}>
       <path d="M1 0v25M6.425 12.5H32M21.15.893 31.225 12.5 21.15 24.107" />
     </g>
