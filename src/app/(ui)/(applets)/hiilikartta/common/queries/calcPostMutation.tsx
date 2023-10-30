ca35851ac4c438372d26c5ba76a0b1be27dbec3e
xbexbex <petja.valkama@gmail.com>
2023-10-30 18:55:20 +0200

    Add calcPostMutation

---
+++ src/app/(ui)/(applets)/hiilikartta/common/queries/calcPostMutation.tsx
@@ -0,0 +1,59 @@
+import { UseMutationOptions } from '@tanstack/react-query'
+import axios from 'axios'
+import JSZip from 'jszip'
+import { PlanConf } from '../types'
+import { useAppletStore } from 'applets/hiilikartta/state/appletStore'
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+type ResponseData = {
+  status: string
+  id: string
+}
+
+export const calcPostMutation = (): UseMutationOptions<
+  ResponseData,
+  Error,
+  PlanConf
+> => {
+  const updatePlanConf = useAppletStore((state) => state.updatePlanConf)
+
+  return {
+    mutationFn: async (planConf: PlanConf) => {
+      const zip = new JSZip()
+      zip.file('file', JSON.stringify(planConf.data))
+      const zipBlob = await zip.generateAsync({ type: 'blob' })
+
+      const formData = new FormData()
+      formData.append('file', zipBlob, 'file.zip')
+      formData.append('zoning_col', planConf.fileSettings.zoningColumn)
+
+      const postRes = await axios.post(
+        `${API_URL}/hiilikartta/data`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          params: { id: planConf.serverId },
+        }
+      )
+
+      if (postRes.status !== 200) {
+        throw new Error('Failed to start calculation.')
+      }
+
+      updatePlanConf(planConf.id, { isCalculating: true })
+      return postRes.data
+    },
+    onError: (error, planConf, context) => {
+      console.error(error)
+      updatePlanConf(planConf.id, {
+        isCalculating: false,
+        reportData: undefined,
+      })
+    },
+    retry: 3,
+    retryDelay: (attemptIndex) => Math.min(attemptIndex * 1000, 3000),
+  }
+}
