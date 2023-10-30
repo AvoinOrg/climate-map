ff7939e1ad2bde3db5d4e1ea3a7119a875beb83c
xbexbex <petja.valkama@gmail.com>
2023-10-30 18:57:43 +0200

    Add a subscription to query calc state

--- src/app/(ui)/(applets)/hiilikartta/state/appletStore.ts
+++ src/app/(ui)/(applets)/hiilikartta/state/appletStore.ts
@@ -1,10 +1,17 @@
 import { create } from 'zustand'
-import { persist, createJSONStorage } from 'zustand/middleware'
+import {
+  persist,
+  createJSONStorage,
+  subscribeWithSelector,
+} from 'zustand/middleware'
 import { immer } from 'zustand/middleware/immer'
+import { pickBy } from 'lodash-es'
 
 import { generateShortId, generateUUID } from '#/common/utils/general'
+import { queryClient } from '#/common/queries/queryClient'
 
 import { NewPlanConf, PlanConf } from '../common/types'
+import { calcPollQuery } from '../common/queries/calcPollQuery'
 
 type State = {
   planConfs: { [key: string]: PlanConf }
@@ -21,41 +28,49 @@ type Actions = {
 
 export const useAppletStore = create<State & Actions>()(
   persist(
-    immer((set, get) => ({
-      planConfs: {},
-      deletePlanConf: async (planId: string) => {
-        set((state) => {
-          delete state.planConfs[planId]
-        })
-      },
-      addPlanConf: async (newPlanConf: NewPlanConf) => {
-        const id = generateShortId()
-        const serverId = generateUUID()
-        const created = new Date().getTime()
-        const planConf = {
-          id,
-          serverId,
-          created,
-          reportData: undefined,
-          isCalculating: false,
-          ...newPlanConf,
-        }
-        await set((state) => {
-          state.planConfs[id] = planConf
-        })
-        return planConf
-      },
-      updatePlanConf: async (planId: string, planConf: Partial<PlanConf>) => {
-        const updatedPlanConf = { ...get().planConfs[planId], ...planConf }
-        await set((state) => {
-          state.planConfs[planId] = updatedPlanConf
-        })
-        return updatedPlanConf
-      },
-    })),
+    subscribeWithSelector(
+      immer((set, get) => ({
+        planConfs: {},
+        deletePlanConf: async (planId: string) => {
+          set((state) => {
+            delete state.planConfs[planId]
+          })
+        },
+        addPlanConf: async (newPlanConf: NewPlanConf) => {
+          const id = generateShortId()
+          const serverId = generateUUID()
+          const created = new Date().getTime()
+          const planConf = {
+            id,
+            serverId,
+            created,
+            reportData: undefined,
+            isCalculating: false,
+            ...newPlanConf,
+          }
+          await set((state) => {
+            state.planConfs[id] = planConf
+          })
+          return planConf
+        },
+        updatePlanConf: async (planId: string, planConf: Partial<PlanConf>) => {
+          const updatedPlanConf = { ...get().planConfs[planId], ...planConf }
+          await set((state) => {
+            state.planConfs[planId] = updatedPlanConf
+          })
+          return updatedPlanConf
+        },
+      }))
+    ),
     {
       name: 'hiilikarttaStore', // name of item in the storage (must be unique)
       storage: createJSONStorage(() => sessionStorage), // (optional) by default the 'localStorage' is used
-    }
-  )
+
+useAppletStore.subscribe(
+  (state) => pickBy(state.planConfs, (planConf) => planConf.isCalculating),
+  (planConfs, _previousPlanConfs) => {
+    Object.keys(planConfs).forEach((planId: string) => {
+      queryClient.fetchQuery(calcPollQuery(planConfs[planId]))
+    })
+  }
 )
