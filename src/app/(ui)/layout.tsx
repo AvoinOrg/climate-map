a4622dcb467d19a46b89e5e027e1a3dac92d333b
xbexbex <petja.valkama@gmail.com>
2023-11-02 14:02:55 +0200

    Add staticData locale loading to Tolgee

--- src/app/(ui)/layout.tsx
+++ src/app/(ui)/layout.tsx
@@ -6,7 +6,13 @@ import React, { useEffect, useState } from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
 import { Box } from '@mui/material'
-import { Tolgee, DevTools, TolgeeProvider, FormatSimple } from '@tolgee/react'
+import {
+  Tolgee,
+  DevTools,
+  TolgeeProvider,
+  FormatSimple,
+  TreeTranslationsData,
+} from '@tolgee/react'
 import { QueryClientProvider } from '@tanstack/react-query'
 
 import theme from '#/common/style/theme'
@@ -19,12 +25,33 @@ import { LoginModal } from '#/components/Modal'
 // import { UiStateProvider, UserStateProvider } from '#/components/State'
 // import RootStyleRegistry from './emotion'
 
-const tolgee = Tolgee().use(DevTools()).use(FormatSimple()).init({
-  language: 'en',
-  defaultNs: 'avoin-map',
-  apiUrl: process.env.NEXT_PUBLIC_TOLGEE_API_URL,
-  apiKey: process.env.NEXT_PUBLIC_TOLGEE_API_KEY,
-})
+const loadTranslation = async (
+  ns: string,
+  lang: string
+): Promise<TreeTranslationsData> => {
+  try {
+    const module = await import(`#/i18n/${ns}/${lang}.json`)
+    return module.default as TreeTranslationsData
+  } catch (error) {
+    console.error(`Failed to load ${lang}:${ns} translations`, error)
+    return {} // Return a fallback or an empty object as needed
+  }
+}
+
+const tolgee = Tolgee()
+  .use(DevTools())
+  .use(FormatSimple())
+  .init({
+    language: 'en',
+    defaultNs: 'avoin-map',
+    apiUrl: process.env.NEXT_PUBLIC_TOLGEE_API_URL,
+    apiKey: process.env.NEXT_PUBLIC_TOLGEE_API_KEY,
+    staticData: {
+      'en:avoin-map': () => loadTranslation('avoin-map', 'en'),
+      'fi:hiilikartta': () => loadTranslation('hiilikartta', 'fi'),
+      // Add more translations as needed
+    },
+  })
 
 const Layout = ({
   // Layouts must accept a children prop.
