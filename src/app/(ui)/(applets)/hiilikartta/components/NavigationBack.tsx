35d889165fd444568d962e9f09634001b5751628
xbexbex <petja.valkama@gmail.com>
2023-11-02 16:10:13 +0200

    Reformat

--- src/app/(ui)/(applets)/hiilikartta/components/NavigationBack.tsx
+++ src/app/(ui)/(applets)/hiilikartta/components/NavigationBack.tsx
@@ -7,14 +7,37 @@ import Link from '#/components/common/Link'
 //   basePath: string
 // }
 
-const NavigationBack = ({ route, label = 'Takaisin' }: { route: string; label?: string }) => {
+const NavigationBack = ({
+  route,
+  label = 'Takaisin',
+}: {
+  route: string
+  label?: string
+}) => {
   // const router = useRouter()
-  const Title = () => <Box sx={(theme) => ({ typography: theme.typography.subtitle1 })}>{label}</Box>
+  const Title = () => (
+    <Box sx={(theme) => ({ typography: theme.typography.subtitle1 })}>
+      {label}
+    </Box>
+  )
 
   return (
-    <Box sx={(theme) => ({ minHeight: '25px', display: 'flex', flexDirection: 'row', margin: '20px 0 60px 0' })}>
-      <Link href={route} sx={{ display: 'flex', color: 'inherit', textDecoration: 'none' }} component={Link}>
-        <ArrowBackIosNewIcon sx={(theme) => ({ float: 'left', cursor: 'pointer' })}></ArrowBackIosNewIcon>
+    <Box
+      sx={(theme) => ({
+        minHeight: '25px',
+        display: 'flex',
+        flexDirection: 'row',
+        margin: '20px 0 60px 0',
+      })}
+    >
+      <Link
+        href={route}
+        sx={{ display: 'flex', color: 'inherit', textDecoration: 'none' }}
+        component={Link}
+      >
+        <ArrowBackIosNewIcon
+          sx={(theme) => ({ float: 'left', cursor: 'pointer' })}
+        ></ArrowBackIosNewIcon>
         <Title />
       </Link>
     </Box>
