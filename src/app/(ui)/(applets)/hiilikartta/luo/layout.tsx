28b4a568e9c41df3c6ca737ea3b2dec9e1e3502f
xbexbex <petja.valkama@gmail.com>
2023-11-04 19:42:34 +0200

    Add SidebarContentBox as wrapper

--- src/app/(ui)/(applets)/hiilikartta/luo/layout.tsx
+++ src/app/(ui)/(applets)/hiilikartta/luo/layout.tsx
@@ -3,12 +3,23 @@
 import React from 'react'
 import { Typography } from '@mui/material'
 
+import { SidebarContentBox } from '#/components/Sidebar'
+
+import { SIDEBAR_WIDTH_REM } from '../common/constants'
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <Typography sx={(theme) => ({ typography: theme.typography.h2, margin: '0 0 35px 0' })}>Luo kaava</Typography>
+    <SidebarContentBox sx={{ width: SIDEBAR_WIDTH_REM + 'rem' }}>
+      <Typography
+        sx={(theme) => ({
+          typography: theme.typography.h2,
+          margin: '0 0 35px 0',
+        })}
+      >
+        Luo kaava
+      </Typography>
       {children}
-    </>
+    </SidebarContentBox>
   )
 }
 
