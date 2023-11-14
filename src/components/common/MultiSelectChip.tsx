d6cd15d3c7ae4d8253bc1e9bfdb0082adfe20bc3
xbexbex <petja.valkama@gmail.com>
2023-11-14 15:22:10 +0200

    Replace multiselect with autocomplete

--- src/components/common/MultiSelectChip.tsx
+++ src/components/common/MultiSelectChip.tsx
@@ -1,45 +1,17 @@
 import * as React from 'react'
-import { Theme, useTheme } from '@mui/material/styles'
 import Box from '@mui/material/Box'
-import OutlinedInput from '@mui/material/OutlinedInput'
-import InputLabel from '@mui/material/InputLabel'
-import MenuItem from '@mui/material/MenuItem'
-import FormControl from '@mui/material/FormControl'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
 import Chip from '@mui/material/Chip'
-import { find } from 'lodash-es'
+import { Autocomplete, TextField } from '@mui/material'
 
 import { SelectOption } from '#/common/types/general'
 
-const ITEM_HEIGHT = 48
-const ITEM_PADDING_TOP = 8
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-}
-
-function getStyles(name: string, personName: readonly string[], theme: Theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  }
-}
-
-const findLabelByValue = (valueToFind: string, data: SelectOption[]) => {
-  const item = find(data, { value: valueToFind })
-  return item ? item.label : undefined
-}
-
 interface Props {
-  value: string[]
+  value: SelectOption[]
   options: SelectOption[]
-  onChange: (event: SelectChangeEvent<string[]>) => void
+  onChange: (
+    event: React.SyntheticEvent<Element, Event>,
+    newValue: SelectOption[]
+  ) => void
   sx?: any
   optionSx?: any
   iconSx?: any
@@ -56,36 +28,46 @@ const MultiSelectChip = ({
   const theme = useTheme()
 
   return (
-    <FormControl sx={{ ...sx }}>
-      <InputLabel id="multiple-chip-label">Chip</InputLabel>
-      <Select
-        labelId="multiple-chip-label"
-        id="multiple-chip"
-        multiple
-        value={value}
-        onChange={onChange}
-        input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
-        renderValue={(selected) => (
-          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
-            {selected.map((value) => (
-              <Chip key={value} label={findLabelByValue(value, options)} />
-            ))}
-          </Box>
-        )}
-        MenuProps={MenuProps}
-      >
-        {options.map((option) => (
-          <MenuItem
+    <Autocomplete
+      multiple
+      id="fixed-tags-demo"
+      value={value}
+      sx={sx}
+      onChange={onChange}
+      options={options}
+      noOptionsText={t('components.autocomplete.no_results')}
+      getOptionLabel={(option) => option.label}
+      renderTags={(tagValue, getTagProps) =>
+        tagValue.map((option, index) => {
+          const { key, ...tagProps } = getTagProps({ index })
+          return <Chip label={option.label} key={key} {...tagProps} />
+        })
+      }
+      style={{ width: 500 }}
+      isOptionEqualToValue={(option, value) => option.value === value.value}
+      renderInput={(params) => {
+        return (
+          <TextField {...params} label="Fixed tag" placeholder="Favorites" />
+        )
+      }}
+      renderOption={(props, option) => {
+        return (
+          <Box
+            sx={
+              {
+                // margin: '5px',
+              }
+            }
+            component="li"
+            {...props}
+            {...optionSx}
             key={option.value}
-            value={option.value}
-            style={getStyles(option.value, value, theme)}
-            sx={{ ...optionSx }}
           >
             {option.label}
-          </MenuItem>
-        ))}
-      </Select>
-    </FormControl>
+          </Box>
+        )
+      }}
+    />
   )
 }
 
