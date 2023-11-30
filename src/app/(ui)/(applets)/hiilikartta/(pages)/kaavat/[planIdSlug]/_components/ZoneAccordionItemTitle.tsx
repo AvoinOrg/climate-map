0effc3e007a9cc1c685fc54e4dc77dc64e4d6234
xbexbex <petja.valkama@gmail.com>
2023-11-30 14:24:41 +0200

    Update zone title to be editable

--- src/app/(ui)/(applets)/hiilikartta/(pages)/kaavat/[planIdSlug]/_components/ZoneAccordionItemTitle.tsx
+++ src/app/(ui)/(applets)/hiilikartta/(pages)/kaavat/[planIdSlug]/_components/ZoneAccordionItemTitle.tsx
@@ -1,34 +1,47 @@
-import { Box, Typography } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { Box } from '@mui/material'
 import { useTranslate } from '@tolgee/react'
 
+import EditableText from '#/components/common/EditableText'
 import { Exclamation } from '#/components/icons'
+
 import { PlanDataFeature } from '../../../../common/types'
+import { useAppletStore } from 'applets/hiilikartta/state/appletStore'
 
 const ZoneAccordionItemTitle = ({
   name,
   zoningCode,
   isValidZoningCode,
+  onChange,
 }: {
   name: PlanDataFeature['properties']['name']
   zoningCode: PlanDataFeature['properties']['zoning_code']
   isValidZoningCode: boolean
+  onChange: (event: any) => void
 }) => {
   const { t } = useTranslate('hiilikartta')
+  const [title, setTitle] = useState('')
+  const [titleAppendix, setTitleAppendix] = useState('')
 
-  let title = ''
-  if (typeof name === 'string') {
-    if (name === '') {
-      title += t('sidebar.plan_settings.new_area')
+  useEffect(() => {
+    if (typeof name === 'string') {
+      if (name === '') {
+        setTitle(t('sidebar.plan_settings.new_area'))
+      } else {
+        setTitle(name)
+      }
     } else {
-      title += name
+      setTitle(`${t('sidebar.plan_settings.area')} ${name}`)
     }
-  } else {
-    title += `${t('sidebar.plan_settings.area')} ${name}`
-  }
+  }, [name])
 
-  if (zoningCode != null && zoningCode != '') {
-    title = `${name} (${zoningCode})`
-  }
+  useEffect(() => {
+    if (zoningCode != null && zoningCode != '') {
+      setTitleAppendix(` (${zoningCode})`)
+    } else {
+      setTitleAppendix('')
+    }
+  }, [zoningCode])
 
   return (
     <Box
@@ -37,13 +50,21 @@ const ZoneAccordionItemTitle = ({
         flexDirection: 'row',
         color: isValidZoningCode ? 'auto' : 'warning.main',
         justifyContent: 'space-between',
-        flex: '1',
         height: '100%',
         alignItems: 'center',
+        overflow: 'hidden',
         pr: 1,
+        width: 0,
+        flexGrow: 1,
       }}
     >
-      <Typography sx={{ display: 'inline' }}>{`${title}`}</Typography>
+      <EditableText
+        value={title}
+        valueAppendix={titleAppendix}
+        onChange={onChange}
+        sx={{ pr: 1 }}
+        iconSx={{ fontSize: '16px' }}
+      ></EditableText>
       {!isValidZoningCode && (
         <Exclamation sx={{ height: '1.4rem' }}></Exclamation>
       )}
