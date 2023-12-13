9f7b4decfa89153c43ee873cee4cd3881ee90c8b
xbexbex <petja.valkama@gmail.com>
2023-12-13 12:42:39 +0200

    Update line chart

--- src/app/(ui)/(applets)/hiilikartta/components/CarbonLineChart.tsx
+++ src/app/(ui)/(applets)/hiilikartta/components/CarbonLineChart.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import { LinePath } from '@visx/shape'
+import { useCallback } from 'react'
+import { Group } from '@visx/group'
 import { scaleLinear, scaleBand } from '@visx/scale'
-import { AxisBottom, AxisLeft } from '@visx/axis'
+import { AxisLeft, AxisBottom } from '@visx/axis'
+import { Line, LinePath } from '@visx/shape'
+import { extent, bisector } from 'd3-array'
+import { LinearGradient } from '@visx/gradient'
+import { GridRows, GridColumns } from '@visx/grid'
+import { useTooltip, TooltipWithBounds, defaultStyles } from '@visx/tooltip'
+import { localPoint } from '@visx/event'
+import { LegendOrdinal } from '@visx/legend'
+import { scaleOrdinal } from 'd3-scale'
 
-import { CalcFeatureCollection, featureYears } from '../common/types'
+import { CalcFeatureCollection } from '../common/types'
+import { pp } from '#/common/utils/general'
+import { Chart } from 'chart.js'
 
 const width = 600
 const height = 400
@@ -15,25 +25,66 @@ type Point = {
 }
 
 type Props = {
-  data: CalcFeatureCollection
+  data: CalcFeatureCollection[]
+  featureYears: string[]
+  planNames: string[]
 }
 
-const CarbonLineChart = ({ data }: Props) => {
-  // Extract data
+const CarbonLineChart = ({ data, featureYears, planNames }: Props) => {
+  // tooltip parameters
+  const {
+    tooltipData,
+    tooltipLeft = 0,
+    tooltipTop = 0,
+    showTooltip,
+    hideTooltip,
+  } = useTooltip()
+
+  // defining inner measurements
+  const innerWidth = width - margin.left - margin.right
+  const innerHeight = height - margin.top - margin.bottom
+
+  // data for lines
   const nochangeData: Point[] = featureYears.map((year) => ({
     year,
     value:
-      data.features[0].properties.bio_carbon_per_area.nochange[year] +
-      data.features[0].properties.ground_carbon_per_area.nochange[year],
-  }))
-  const plannedData: Point[] = featureYears.map((year) => ({
-    year,
-    value:
-      data.features[0].properties.bio_carbon_per_area.planned[year] +
-      data.features[0].properties.ground_carbon_per_area.planned[year],
+      data[0].features[0].properties.bio_carbon_ha.nochange[year] +
+      data[0].features[0].properties.ground_carbon_ha.nochange[year],
   }))
 
-  // Scales
+  const plannedDatas: Point[][] = data.map((calc) => {
+    return featureYears.map((year) => ({
+      year,
+      value:
+        calc.features[0].properties.bio_carbon_ha.planned[year] +
+        calc.features[0].properties.ground_carbon_ha.planned[year],
+    }))
+  })
+
+  const series = [nochangeData, ...plannedDatas]
+  const flatSeries = series.flat()
+
+  //colors for lines
+  const colors = ['red', 'blue', 'orange']
+
+  // Defining selector functions
+  const getValue = (d: Point) => Math.round(d.value, 2)
+  const getYear = (d: Point) => Number(d.year)
+  const bisectDate = bisector((d: Point) => d.year).left
+
+  // get data from a year
+  const getDataForYear = (year: string) => {
+    const output = flatSeries.filter((el) => {
+      return el.year === year
+    })
+    return output
+  }
+
+  // to remove comma from date
+  const formatDate = (year: string) => year.toString()
+
+  // Defining scales
+  // horizontal, x scale
   const xScale = scaleBand<string>({
     domain: featureYears,
     range: [margin.left, width - margin.right],
@@ -42,77 +93,193 @@ const CarbonLineChart = ({ data }: Props) => {
 
   const yMax = Math.max(
     ...nochangeData.map((d) => d.value),
-    ...plannedData.map((d) => d.value)
+    ...plannedDatas.flatMap((plannedData) => plannedData.map((d) => d.value))
   )
 
   // Add some padding, for example 10%
   const paddedYMax = yMax * 1.1
 
   const yScale = scaleLinear({
-    domain: [0, paddedYMax],
+    domain: [-10, paddedYMax],
     range: [height - margin.bottom, margin.top],
   })
 
-  const legendItems = [
-    { color: 'blue', label: 'ei muutosta' },
-    { color: 'red', label: 'kaava' },
-  ]
+  // defining tooltip styles
+  const tooltipStyles = {
+    ...defaultStyles,
+    minWidth: 60,
+    backgroundColor: 'white',
+    color: 'black',
+  }
 
-  const legendPadding = 10
-  const legendX = width - margin.right - legendPadding
-  const legendY =
-    height - margin.bottom - legendItems.length * 20 - legendPadding
+  const handleTooltip = useCallback(
+    (event: any) => {
+      const { x } = localPoint(event) || { x: 0 }
+      const domain = xScale.domain()
+      const step = xScale.step()
+      const xIndex = Math.floor((x - margin.left) / step)
+      const x0 = domain[Math.max(0, Math.min(xIndex, domain.length - 1))]
+
+      const index = bisectDate(series[0], x0, 1)
+      let d = series[0][index]
+
+      // if (d1 && getYear(d1)) {
+      //   d =
+      //     x0.valueOf() - getYear(d0).valueOf() >
+      //     getYear(d1).valueOf() - x0.valueOf()
+      //       ? d1
+      //       : d0
+      // }
+      showTooltip({
+        tooltipData: getDataForYear(d.year),
+        tooltipLeft: x,
+        tooltipTop: yScale(getValue(d)),
+      })
+    },
+    [data, featureYears]
+  )
+
+  const colorScale = scaleOrdinal()
+    .domain(['ilman muutosta'].concat(...planNames))
+    .range(colors)
+
+  const ChartLegend = () => {
+    // const { colorScale, theme, margin } = useContext(DataContext)
+
+    return (
+      <LegendOrdinal
+        direction="row"
+        itemMargin="8px 8px 8px 0"
+        scale={colorScale}
+        // labelFormat={(label) => label.replace('-', ' ')}
+        legendLabelProps={{ color: 'white' }}
+        style={{
+          // backgroundColor: theme.backgroundColor,
+          marginTop: 0,
+          paddingLeft: margin.left + 50,
+          color: 'black',
+          display: 'flex', // required in addition to `direction` if overriding styles
+        }}
+      />
+    )
+  }
 
   return (
-    <svg width={width} height={height}>
-      {/* Nochange Line */}
-      <LinePath
-        data={nochangeData}
-        x={(d) => xScale(d.year) + xScale.bandwidth() / 2}
-        y={(d) => yScale(d.value)}
-        stroke="blue"
-        strokeWidth={2}
-      />
-
-      <LinePath
-        data={plannedData}
-        x={(d) => xScale(d.year) + xScale.bandwidth() / 2}
-        y={(d) => yScale(d.value)}
-        stroke="red"
-        strokeWidth={2}
-      />
-
-      <AxisBottom
-        scale={xScale}
-        top={height - margin.bottom}
-        left={0}
-        tickFormat={(value) => (value === 'now' ? 'nykytila' : value)}
-      />
-
-      <AxisLeft scale={yScale} top={0} left={margin.left} />
-      <text
-        x={margin.left} // Adjust as necessary for horizontal positioning
-        y={height - margin.bottom + 20} // Adjust for vertical positioning
-        style={{ fontSize: '12px', textAnchor: 'end' }}
-      >
-        tCO2e
-      </text>
-      <g transform={`translate(${legendX}, ${legendY})`}>
-        {legendItems.map((item, index) => (
-          <g key={item.label} transform={`translate(0, ${index * 20})`}>
-            <text
-              x={-20 - 5} // Position text to left of the rectangle with 5-pixel gap
-              y={12}
-              textAnchor="end" // This ensures text aligns to its rightmost edge
-              style={{ userSelect: 'none', fontSize: '12px' }}
-            >
-              {item.label}
-            </text>
-            <rect x={-20} width={15} height={15} fill={item.color} />
-          </g>
-        ))}
-      </g>
-    </svg>
+    <div style={{ position: 'relative' }}>
+      <svg width={width} height={height}>
+        {/* <rect x={0} y={0} width={width} height={height} fill={'none'} rx={14} /> */}
+        <Group left={margin.left} top={margin.top}>
+          <GridRows
+            scale={yScale}
+            width={innerWidth}
+            height={innerHeight}
+            stroke="gray"
+            strokeOpacity={0.2}
+            left={margin.left}
+          />
+          <GridColumns
+            scale={xScale}
+            width={innerWidth}
+            height={innerHeight + 25}
+            stroke="gray"
+            strokeOpacity={0.2}
+            top={margin.top}
+          />
+          <LinearGradient
+            id="area-gradient"
+            from={'#43b284'}
+            to={'#43b284'}
+            toOpacity={0.1}
+          />
+          <AxisLeft scale={yScale} top={0} left={margin.left} />
+          <text
+            x={margin.left} // Adjust as necessary for horizontal positioning
+            y={height - margin.bottom + 20} // Adjust for vertical positioning
+            style={{ fontSize: '12px', textAnchor: 'end' }}
+          >
+            tCO2e
+          </text>
+          <AxisBottom
+            scale={xScale}
+            top={height - margin.bottom}
+            left={0}
+            tickFormat={(value) => (value === 'now' ? 'nykytila' : value)}
+          />
+          {series.map((sData, i) => (
+            <LinePath
+              key={i}
+              stroke={colors[i]}
+              strokeWidth={3}
+              data={sData}
+              x={(d) => xScale(String(getYear(d))) ?? 0}
+              y={(d) => yScale(getValue(d)) ?? 0}
+            />
+          ))}
+          {tooltipData && (
+            <g>
+              <Line
+                from={{ x: tooltipLeft - margin.left, y: margin.top }}
+                to={{
+                  x: tooltipLeft - margin.left,
+                  y: innerHeight + margin.top,
+                }}
+                stroke={'gray'}
+                strokeWidth={2}
+                pointerEvents="none"
+                strokeDasharray="4,2"
+                opacity={0.5}
+              />
+            </g>
+          )}
+          {/* {tooltipData &&
+            tooltipData.map((d, i) => (
+              <g>
+                <GlyphCircle
+                  left={tooltipLeft - margin.left}
+                  top={yScale(d.amount) + 2}
+                  size={110}
+                  fill={colors[i]}
+                  stroke={'white'}
+                  strokeWidth={2}
+                />
+              </g>
+            ))} */}
+          <rect
+            x={0}
+            y={0}
+            width={innerWidth}
+            height={innerHeight}
+            onTouchStart={handleTooltip}
+            fill={'transparent'}
+            onTouchMove={handleTooltip}
+            onMouseMove={handleTooltip}
+            onMouseLeave={() => hideTooltip()}
+          />
+        </Group>
+      </svg>
+      {/* render a tooltip */}
+      {tooltipData ? (
+        <TooltipWithBounds
+          key={Math.random()}
+          top={tooltipTop}
+          left={tooltipLeft}
+          style={tooltipStyles}
+        >
+          {tooltipData.map((d, i) => {
+            var title = ''
+            if (i === 0) {
+              title = 'ilman muutosta'
+            } else {
+              title = planNames[i - 1]
+            }
+            return (
+              <p key={i}>{`${title}: ${getValue(tooltipData[i])} ton/ha`}</p>
+            )
+          })}
+        </TooltipWithBounds>
+      ) : null}
+      <ChartLegend />
+    </div>
   )
 }
 
