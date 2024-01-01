f037832de3d216ef8d016746c0138854773b7ae2
xbexbex <petja.valkama@gmail.com>
2024-01-01 19:26:49 +0200

    Add currentSitutation to datas

--- src/app/(ui)/(applets)/hiilikartta/components/CarbonMapGraph/CarbonMapGraph.tsx
+++ src/app/(ui)/(applets)/hiilikartta/components/CarbonMapGraph/CarbonMapGraph.tsx
@@ -1,7 +1,13 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react'
 import { Box } from '@mui/material'
+import { cloneDeep } from 'lodash-es'
+import { useTranslate } from '@tolgee/react'
 
-import { PlanConfWithReportData } from 'applets/hiilikartta/common/types'
+import {
+  featureCols,
+  PlanConfWithReportData,
+  ZONING_CODE_COL,
+} from 'applets/hiilikartta/common/types'
 import CarbonMapGraphMap from './CarbonMapGraphMap'
 import CarbonChangeLegend from '../CarbonChangeLegend'
 
@@ -11,15 +17,37 @@ type Props = {
 }
 
 const CarbonMapGraph = ({ planConfs, featureYears }: Props) => {
+  const { t } = useTranslate('hiilikartta')
   const [activePlanConfId, setActivePlanConfId] = useState(planConfs[0].id)
   const [activeYear, setActiveYear] = useState(featureYears[0])
 
   const datas = useMemo(() => {
-    return planConfs.map((planConf) => ({
+    let datas = planConfs.map((planConf) => ({
       id: planConf.id,
       name: planConf.name,
       data: planConf.reportData.areas,
     }))
+
+    const currentSituation = cloneDeep(datas[0])
+    for (const feature of currentSituation.data.features) {
+      featureCols.forEach((propertyKey) => {
+        const property = feature.properties[propertyKey]
+        if (property && property.planned && property.nochange) {
+          // Update each planned year with the corresponding nochange value
+          Object.keys(property.planned).forEach((year) => {
+            property.planned[year] =
+              property.nochange[year] ?? property.planned[year]
+          })
+        }
+        feature.properties[ZONING_CODE_COL] = 'none'
+      })
+    }
+    currentSituation.id = currentSituation.id + '-current'
+    currentSituation.name = t('report.general.current_situation')
+
+    datas = [currentSituation, ...datas]
+
+    return datas
   }, [planConfs])
 
   return (
