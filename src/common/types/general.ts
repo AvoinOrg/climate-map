e3d6d6d6b31cb2c8341e31cb21c4a7139ac1105c
xbexbex <petja.valkama@gmail.com>
2024-01-03 16:12:50 +0200

    Reformat

--- src/common/types/general.ts
+++ src/common/types/general.ts
@@ -2,4 +2,7 @@ export type ValueOf<T> = T[keyof T]
 
 export type StringValueOf<T> = T[keyof T] & string
 
-export type SelectOption = { label: string; value: string }
+export type SelectOption = {
+  label: string
+  value: string
+}
