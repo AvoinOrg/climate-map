08d4d2f1d94d40790a5d65b63f950ac6df739a4e
xbexbex <petja.valkama@gmail.com>
2024-01-04 17:55:34 +0200

    Update table styling

--- src/app/(ui)/(applets)/hiilikartta/components/CarbonMapGraph/CarbonMapGraphTable.tsx
+++ src/app/(ui)/(applets)/hiilikartta/components/CarbonMapGraph/CarbonMapGraphTable.tsx
@@ -7,9 +7,11 @@ import {
   TableHead,
   TableRow,
 } from '@mui/material'
+import { styled } from '@mui/material/styles'
 import { T } from '@tolgee/react'
 
 import { MapGraphData } from 'applets/hiilikartta/common/types'
+import { round } from 'lodash-es'
 
 type Props = {
   datas: MapGraphData[]
@@ -54,40 +56,69 @@ const CarbonMapGraphTable = ({ datas, activeYear }: Props) => {
   }, [datas])
 
   return (
-    <TableContainer>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableContainer sx={{ mt: 4, overFlowX: 'scroll' }}>
+      <Table
+        sx={{ borderCollapse: 'collapse', 'th, td': { border: 0 } }}
+        aria-label="simple table"
+      >
         <TableHead>
           <TableRow>
-            <TableCell sx={{ border: 0 }}>
+            <TableCell
+              sx={{
+                typography: 'h8',
+                color: 'secondary.dark',
+                verticalAlign: 'top',
+              }}
+            >
               <T ns="hiilikartta" keyName="report.map_graph.year"></T>
-              {activeYear}
+              <b>{' ' + activeYear}</b>
             </TableCell>
             {datas.map((data, index) => (
-              <TableCell key={index} sx={{ border: 0 }}>
+              <TableCell
+                sx={{
+                  typography: 'h7',
+                  lineHeight: 'normal',
+                  verticalAlign: 'top',
+                  wordBreak: 'break-word',
+                  overflowWrap: 'break-word',
+                  minWidth: '8rem',
+                }}
+                key={index}
+              >
                 {data.name}
               </TableCell>
             ))}
           </TableRow>
         </TableHead>
         <TableBody>
-          <TableRow key={'co2_ha'} sx={{ '&:*': { border: 0 } }}>
-            <TableCell component="th" scope="row">
-              <T ns="hiilikartta" keyName="report.map_graph.unit_co2_ha"></T>
-            </TableCell>
+          <TableRow key={'co2_ha'}>
+            <FirstColumnCell component="th" scope="row">
+              <T
+                ns="hiilikartta"
+                keyName="report.map_graph.unit_co2_ha_compared"
+              ></T>
+            </FirstColumnCell>
             {co2HaRowData.map((rowData, index) => (
-              <TableCell key={index} align="right">
-                {rowData}
-              </TableCell>
+              <DataCell key={index} align="left">
+                {round(rowData, 0)}
+              </DataCell>
             ))}
           </TableRow>
-          <TableRow key={'co2_total'} sx={{ '&:*': { border: 0 } }}>
-            <TableCell component="th" scope="row">
-              <T ns="hiilikartta" keyName="report.map_graph.unit_co2_total"></T>
-            </TableCell>
+          <TableRow key={'co2_total'}>
+            <FirstColumnCell
+              sx={{ typography: 'body7' }}
+              component="th"
+              scope="row"
+            >
+              <T
+                ns="hiilikartta"
+                keyName="report.map_graph.unit_co2_total_compared"
+              ></T>
+            </FirstColumnCell>
             {co2TotalRowData.map((rowData, index) => (
-              <TableCell key={index} align="right">
-                {rowData}
-              </TableCell>
+              <DataCell key={index} align="left">
+                {round(rowData, 0)}
+              </DataCell>
             ))}
           </TableRow>
         </TableBody>
@@ -95,5 +126,15 @@ const CarbonMapGraphTable = ({ datas, activeYear }: Props) => {
     </TableContainer>
   )
 }
+
+const FirstColumnCell = styled(TableCell)(({ theme }) => ({
+  ...theme.typography.body7,
+}))
+
+const DataCell = styled(TableCell)(({ theme }) => ({
+  ...theme.typography.h2,
+  fontSize: '1.25rem',
+  letterSpacing: '0.125rem',
+}))
 
 export default CarbonMapGraphTable
