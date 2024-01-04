aa1d4d69c1b0d9e731a362ff5b2b4bb328bb8552
xbexbex <petja.valkama@gmail.com>
2024-01-04 13:59:55 +0200

    Add mobile layout

--- src/app/(ui)/(applets)/hiilikartta/components/CarbonChangeLegend.tsx
+++ src/app/(ui)/(applets)/hiilikartta/components/CarbonChangeLegend.tsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Box, SxProps, Theme, Typography } from '@mui/material'
 import { T, useTranslate } from '@tolgee/react'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 import {
   CARBON_CHANGE_COLORS,
   CARBON_CHANGE_NO_DATA_COLOR,
 } from '../common/constants'
+import { ArrowDown, ArrowUp } from '#/components/icons'
 
 const NUMBER_OF_ITEMS = CARBON_CHANGE_COLORS.length + 1
 const FLEX_BASIS = 100 / NUMBER_OF_ITEMS + '%'
@@ -13,6 +16,17 @@ const FLEX_BASIS = 100 / NUMBER_OF_ITEMS + '%'
 type Props = { sx?: SxProps<Theme> }
 
 const CarbonChangeLegend = ({ sx }: Props) => {
+  const theme = useTheme()
+  const useNarrowLayout = useMediaQuery(theme.breakpoints.down('md'))
+
+  return useNarrowLayout ? (
+    <CarbonChangeLegendNarrow sx={sx}></CarbonChangeLegendNarrow>
+  ) : (
+    <CarbonChangeLegendWide sx={sx}></CarbonChangeLegendWide>
+  )
+}
+
+const CarbonChangeLegendWide = ({ sx }: Props) => {
   const { t } = useTranslate('hiilikartta')
   const noChangeTextRef = useRef<HTMLDivElement>(null)
 
@@ -157,6 +171,194 @@ const CarbonChangeLegend = ({ sx }: Props) => {
   )
 }
 
+const CarbonChangeLegendNarrow = ({ sx }: Props) => {
+  const { t } = useTranslate('hiilikartta')
+  const legendItemsRef = useRef<HTMLDivElement>(null)
+
+  const [noChangeTextTop, setNoChangeTextTop] = useState('0')
+  const [growTop, setGrowTop] = useState('0')
+  const [shrinkTop, setShrinkTop] = useState('0')
+
+  useEffect(() => {
+    if (legendItemsRef.current) {
+      const containerHeight = legendItemsRef.current.offsetHeight
+      // Assuming each item is of equal height and you want to align with the middle of the items
+      const itemHeight = containerHeight / NUMBER_OF_ITEMS
+      const newNoChangeTextTop = itemHeight * (NUMBER_OF_ITEMS / 2 - 0.5)
+      setNoChangeTextTop(`${newNoChangeTextTop}px`)
+      setGrowTop(`calc(${newNoChangeTextTop}px - 2rem)`)
+      setShrinkTop(`calc(${newNoChangeTextTop}px + 1.5rem)`)
+    }
+  }, [legendItemsRef.current?.offsetHeight])
+
+  return (
+    <Box
+      sx={[
+        { display: 'flex', flexDirection: 'column' },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
+    >
+      <Box
+        sx={{
+          typography: 'body7',
+          fontSize: '0.75rem',
+          lineHeight: 'normal',
+          letterSpacing: '0.075rem',
+          display: 'flex',
+          flexDirection: 'column',
+          pl: '5.5rem',
+        }}
+      >
+        <b>
+          <T
+            ns={'hiilikartta'}
+            keyName={'report.carbon_change_legend.title'}
+          ></T>
+        </b>
+        <Box sx={{ mt: 0.7 }}>
+          <T
+            ns={'hiilikartta'}
+            keyName={'report.carbon_change_legend.unit'}
+          ></T>
+        </Box>
+      </Box>
+      <Box
+        sx={[
+          {
+            display: 'flex',
+            flexDirection: 'row',
+            width: '100%',
+          },
+        ]}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            position: 'relative',
+            width: '5rem',
+            minWidth: '5rem',
+            maxWidth: '5rem',
+            mr: '0.5rem',
+            height: '100%',
+          }}
+        >
+          <Typography
+            sx={{
+              position: 'absolute',
+              top: noChangeTextTop,
+              typography: 'body1',
+              fontSize: '0.5rem',
+              lineHeight: 'normal',
+              letterSpacing: '0.05rem',
+              textAlign: 'right',
+              width: '100%',
+              pr: '1.15rem',
+            }}
+          >
+            <T
+              ns={'hiilikartta'}
+              keyName={'report.carbon_change_legend.no_change'}
+            ></T>
+          </Typography>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: growTop,
+              left: 'auto',
+              color: '#568175',
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'flex-end',
+              textAlign: 'right',
+              width: '100%',
+              justifyContent: 'flex-end',
+            }}
+          >
+            <Typography
+              sx={{
+                typography: 'body1',
+                fontSize: '0.5rem',
+                lineHeight: 'normal',
+                letterSpacing: '0.05rem',
+              }}
+            >
+              <T
+                ns={'hiilikartta'}
+                keyName={'report.carbon_change_legend.stores_expand'}
+              ></T>
+            </Typography>
+            <ArrowUp
+              sx={{
+                fontSize: '1rem',
+                height: 'auto',
+                width: '1rem',
+                ml: '0.4rem',
+              }}
+            />
+          </Box>
+          <Box
+            sx={{
+              position: 'absolute',
+              top: shrinkTop,
+              left: 'auto',
+              color: '#C54032',
+              display: 'flex',
+              flexDirection: 'row',
+              alignItems: 'flex-start',
+              textAlign: 'right',
+            }}
+          >
+            <Typography
+              sx={{
+                typography: 'body1',
+                fontSize: '0.5rem',
+                lineHeight: 'normal',
+                letterSpacing: '0.05rem',
+              }}
+            >
+              <T
+                ns={'hiilikartta'}
+                keyName={'report.carbon_change_legend.stores_shrink'}
+              ></T>
+            </Typography>
+            <ArrowDown
+              sx={{
+                fontSize: '1rem',
+                height: 'auto',
+                width: '1rem',
+                ml: '0.4rem',
+              }}
+            />
+          </Box>
+        </Box>
+        <Box
+          ref={legendItemsRef}
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '100%',
+          }}
+        >
+          {[...CARBON_CHANGE_COLORS].reverse().map((colorObj, index) => (
+            <LegendItemNarrow
+              color={colorObj.color}
+              key={index}
+              label={`${colorObj.min} – ${
+                colorObj.max < 0 ? '(' + colorObj.max + ')' : colorObj.max
+              }`}
+            ></LegendItemNarrow>
+          ))}
+          <LegendItemNarrow
+            color={CARBON_CHANGE_NO_DATA_COLOR}
+            label={t('report.carbon_change_legend.no_data')}
+          ></LegendItemNarrow>
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
 const LegendItem = ({ color, label }: { color: string; label: string }) => {
   return (
     <Box
@@ -187,6 +389,46 @@ const LegendItem = ({ color, label }: { color: string; label: string }) => {
           backgroundColor: color,
         }}
       ></Box>
+    </Box>
+  )
+}
+
+const LegendItemNarrow = ({
+  color,
+  label,
+}: {
+  color: string
+  label: string
+}) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        mt: '0.78rem',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          width: '2.34rem',
+          height: '0.46rem',
+          backgroundColor: color,
+          mr: '0.625rem',
+        }}
+      ></Box>
+      <Typography
+        sx={{
+          typography: 'body2',
+          fontSize: '0.625rem',
+          fontStyle: 'normal',
+          lineHeight: 'normal',
+          letterSpacing: '0.0625rem',
+          width: 'auto',
+        }}
+      >
+        {label}
+      </Typography>
     </Box>
   )
 }
