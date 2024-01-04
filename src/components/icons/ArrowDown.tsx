1bfd0918693a4dbcdbf482688b7ec2659d745659
xbexbex <petja.valkama@gmail.com>
2024-01-04 14:00:14 +0200

    Refactor ArrowDown to use box

--- src/components/icons/ArrowDown.tsx
+++ src/components/icons/ArrowDown.tsx
@@ -1,8 +1,22 @@
 import * as React from 'react'
-import type { SVGProps } from 'react'
-const SvgArrowDown = (props: SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width={17} height={11} fill="none" {...props}>
+import { Box, SxProps, Theme } from '@mui/material'
+
+type Props = {
+  sx?: SxProps<Theme>
+}
+
+const SvgArrowDown = (props: Props) => (
+  <Box
+    component="svg"
+    xmlns="http://www.w3.org/2000/svg"
+    width={17}
+    height={11}
+    viewBox="0 0 17 11"
+    fill="none"
+    {...props}
+  >
     <path stroke="currentColor" strokeWidth={2} d="M16 1 8.5 9 1 1" />
-  </svg>
+  </Box>
 )
+
 export default SvgArrowDown
