a1734090ba50ff55310b978fd6230fbf9013b8b8
xbexbex <petja.valkama@gmail.com>
2024-01-04 16:30:42 +0200

    Refactor DownIcon to use Box

--- src/components/icons/DownIcon.tsx
+++ src/components/icons/DownIcon.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import { Box, Icon, SvgIcon } from '@mui/material'
-import { SvgIconComponent } from '@mui/icons-material'
+import { Box, SxProps, Theme } from '@mui/material'
 
-// const Img = () => (
-//   <svg width="17" height="11" viewBox="0 0 17 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-//     <path d="M16 1L8.5 9L1 0.999999" stroke="black" stroke-width="2" />
-//   </svg>
-// )
-
-const DownIcon = (props: any) => (
-  <SvgIcon {...props}>
-    <svg width={24} height={24} viewBox="0 0 17 11" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-      <path d="M16 1 8.5 9 1 1" stroke="currentColor" strokeWidth={2} />
-    </svg>
-  </SvgIcon>
+type Props = {
+  sx?: SxProps<Theme>
+}
+const DownIcon = (props: Props) => (
+  <Box
+    component={'svg'}
+    width={24}
+    height={24}
+    viewBox="0 0 17 11"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M16 1 8.5 9 1 1" stroke="currentColor" strokeWidth={2} />
+  </Box>
 )
 
 export default DownIcon
