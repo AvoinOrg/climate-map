7e849b494aa7f3502296f656070f2c83148e241a
xbexbex <petja.valkama@gmail.com>
2024-01-05 23:02:14 +0200

    Add year select

--- src/app/(ui)/(applets)/hiilikartta/components/CarbonOverviewGraph/CarbonOverviewGraph.tsx
+++ src/app/(ui)/(applets)/hiilikartta/components/CarbonOverviewGraph/CarbonOverviewGraph.tsx
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
 import { styled } from '@mui/material/styles'
-import { Box, SxProps, Theme, Typography } from '@mui/material'
+import {
+  Box,
+  SxProps,
+  Theme,
+  Typography,
+  SelectChangeEvent,
+} from '@mui/material'
 import { T } from '@tolgee/react'
 
 import { pp } from '#/common/utils/general'
+import DropDownSelectMinimal from '#/components/common/DropDownSelectMinimal'
 
 import { PlanConfWithReportData } from 'applets/hiilikartta/common/types'
 import GeomGraphic from './GeomGraphic'
@@ -15,22 +22,53 @@ type Props = {
 }
 
 const CarbonOverviewGraph = ({ planConfs, featureYears, sx }: Props) => {
+  const [activeYear, setActiveYear] = useState(featureYears[1])
+
+  const handleYearChange = (event: SelectChangeEvent<string>) => {
+    setActiveYear(event.target.value)
+  }
+
   return (
     <Box sx={[...(Array.isArray(sx) ? sx : [sx])]}>
-      <Row
-        sx={(theme) => ({
-          mt: 6,
-        })}
-      >
-        <Typography
-          sx={(theme) => ({
-            typography: theme.typography.h1,
-            display: 'inline',
-          })}
-        >
-          <T keyName="report.impact_on_carbon_stock" ns={'hiilikartta'}></T>{' '}
-          {featureYears[1]}
-        </Typography>
+      <Row>
+        <Col>
+          <Typography
+            sx={(theme) => ({
+              typography: theme.typography.h1,
+            })}
+          >
+            <T
+              keyName="report.overview_graph.impact_on_carbon_stock"
+              ns={'hiilikartta'}
+            ></T>{' '}
+          </Typography>
+          <Row sx={{ justifyContent: 'flex-start', mt: 0.5 }}>
+            <Typography
+              sx={(theme) => ({
+                typography: theme.typography.h1,
+                display: 'inline',
+              })}
+            >
+              <T keyName="report.overview_graph.on_year" ns={'hiilikartta'}></T>{' '}
+            </Typography>
+            <DropDownSelectMinimal
+              options={featureYears.map((featureYear) => ({
+                label: featureYear,
+                value: featureYear,
+              }))}
+              value={activeYear}
+              onChange={handleYearChange}
+              optionSx={{
+                typography: 'h1',
+                display: 'inline',
+              }}
+              iconSx={{
+                mt: 0.2,
+                height: "0.75rem",
+              }}
+            ></DropDownSelectMinimal>
+          </Row>
+        </Col>
       </Row>
       <Row sx={{ mt: 10, flexWrap: 'wrap', justifyContent: 'normal' }}>
         {planConfs.map((planConf) => {
@@ -38,44 +76,47 @@ const CarbonOverviewGraph = ({ planConfs, featureYears, sx }: Props) => {
             <Row sx={{ mb: 10, flex: 0, mr: 'auto' }} key={planConf.id}>
               <Col>
                 <Typography typography={'h8'}>
-                  <T keyName="report.plan" ns="hiilikartta"></T>
+                  <T keyName="report.overview_graph.plan" ns="hiilikartta"></T>
                 </Typography>
                 <Typography typography={'h7'} sx={{ whiteSpace: 'nowrap' }}>
                   {planConf?.name}
                 </Typography>
                 <Typography typography={'h5'} sx={{ mt: 2 }}>
                   <T
-                    keyName="report.carbon_stock_decreases"
+                    keyName="report.overview_graph.carbon_stock_decreases"
                     ns="hiilikartta"
                   ></T>
                 </Typography>
                 <Typography mt={4} typography={'h5'}>
-                  <T keyName="report.carbon_eqv_unit" ns="hiilikartta"></T>
+                  <T
+                    keyName="report.overview_graph.carbon_eqv_unit"
+                    ns="hiilikartta"
+                  ></T>
                 </Typography>
                 <Typography mt={1} typography={'h1'}>
                   {pp(
                     planConf.reportData.agg.totals.bio_carbon_total_diff[
-                      featureYears[1]
+                      activeYear
                     ] +
                       planConf.reportData.agg.totals.ground_carbon_total_diff[
-                        featureYears[1]
+                        activeYear
                       ],
                     4
                   )}
                 </Typography>
                 <Typography mt={3} typography={'h5'}>
                   <T
-                    keyName="report.carbon_eqv_unit_hectare"
+                    keyName="report.overview_graph.carbon_eqv_unit_hectare"
                     ns="hiilikartta"
                   ></T>
                 </Typography>
                 <Typography mt={1} typography={'h1'}>
                   {pp(
                     planConf.reportData.agg.totals.bio_carbon_ha_diff[
-                      featureYears[1]
+                      activeYear
                     ] +
                       planConf.reportData.agg.totals.ground_carbon_ha_diff[
-                        featureYears[1]
+                        activeYear
                       ],
                     2
                   )}
@@ -84,7 +125,7 @@ const CarbonOverviewGraph = ({ planConfs, featureYears, sx }: Props) => {
               <Col sx={{ ml: 2 }}>
                 <GeomGraphic
                   feature={planConf.reportData.totals.features[0]}
-                  year={featureYears[1]}
+                  year={activeYear}
                   width={120}
                   height={200}
                   sx={{ mt: 3 }}
