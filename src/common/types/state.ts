46da44ba21d69fffc4908ba4819824aa1bf774f5
xbexbex <petja.valkama@gmail.com>
2024-01-16 17:08:14 +0200

    Reformat

--- src/common/types/state.ts
+++ src/common/types/state.ts
@@ -1,10 +1,26 @@
-export type VerificationStatus = 'unverified' | 'errored' | 'verified' | 'emailErrored' | 'emailSent'
+export type VerificationStatus =
+  | 'unverified'
+  | 'errored'
+  | 'verified'
+  | 'emailErrored'
+  | 'emailSent'
 
-export type ProfileState = 'none' | 'data' | 'dataIntegrate' | 'verification' | 'farmCarbon'
+export type ProfileState =
+  | 'none'
+  | 'data'
+  | 'dataIntegrate'
+  | 'verification'
+  | 'farmCarbon'
 
 export type ModalState = 'none' | 'signup' | 'login' | 'profile'
 
-export type RouteState = 'none' | 'allowed' | 'verify' | 'fetching' | 'denied' | 'login'
+export type RouteState =
+  | 'none'
+  | 'allowed'
+  | 'verify'
+  | 'fetching'
+  | 'denied'
+  | 'login'
 
 export type NotificationMessage = {
   message: string
