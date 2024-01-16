8af489201373f25a2b52ce729e8e8dddb57bb6a4
xbexbex <petja.valkama@gmail.com>
2024-01-16 16:35:53 +0200

    Update notification logic

--- src/common/store/UIStore.ts
+++ src/common/store/UIStore.ts
@@ -1,14 +1,19 @@
 import React from 'react'
 import { create } from 'zustand'
+import { immer } from 'zustand/middleware/immer'
 
-import { NotificationMessage } from '#/common/types/state'
+import {
+  InternalNotificationMessage,
+  NotificationMessage,
+} from '#/common/types/state'
+import { generateUUID } from '../utils/general'
 
 interface Vars {
   isSidebarOpen: boolean
   isSidebarDisabled: boolean
   mode: 'side' | 'full'
   isMapPopupOpen: boolean
-  notifications: NotificationMessage[]
+  notifications: Record<string, InternalNotificationMessage>
   isNavbarOpen: boolean
   isLoginModalOpen: boolean
   sidebarWidth: number | undefined
@@ -19,70 +24,90 @@ interface Actions {
   setMode: (value: 'side' | 'full') => void
   setIsSidebarDisabled: (value: boolean) => void
   setIsMapPopupOpen: (value: boolean) => void
-  notify: (message: NotificationMessage) => void
+  notify: (notification: NotificationMessage) => Promise<void>
+  updateNotification: (
+    notificationId: string,
+    notification: Partial<InternalNotificationMessage>
+  ) => Promise<void>
   setIsNavbarOpen: (value: boolean) => void
   setSidebarHeaderElement: undefined | ((value: React.JSX.Element) => void)
-  setSidebarHeaderElementSetter: (setter: (value: React.JSX.Element) => void) => void
+  setSidebarHeaderElementSetter: (
+    setter: (value: React.JSX.Element) => void
+  ) => void
   setIsLoginModalOpen: (isOpen: boolean) => void
   setSidebarWidth: (pixels: number) => void
 }
 
 type State = Vars & Actions
 
-export const useUIStore = create<State>((set, get) => {
-  const vars: Vars = {
-    isSidebarDisabled: false,
-    isSidebarOpen: true,
-    mode: 'side',
-    isMapPopupOpen: false,
-    isLoginModalOpen: false,
-    isNavbarOpen: true,
-    notifications: [],
-    sidebarWidth: undefined,
-  }
-  const actions: Actions = {
-    setIsSidebarOpen: (value) => set({ isSidebarOpen: value }),
-    setMode: (value) => set({ mode: value }),
-    setIsSidebarDisabled: (value) => set({ isSidebarDisabled: value }),
-    setIsMapPopupOpen: (value) => set({ isMapPopupOpen: value }),
-    setIsLoginModalOpen: (isOpen: boolean) => {
-      set({ isLoginModalOpen: isOpen })
-    },
-    // profileState: 'none',
-    // setProfileState: (value) => set({ profileState: value }),
-    // modalState: 'none',
-    // setModalState: (value) => set({ modalState: value }),
-    // signupFunnelStep: 0,
-    // setSignupFunnelStep: (value) => set({ signupFunnelStep: value }),
-    setIsNavbarOpen: (value) => set({ isNavbarOpen: value }),
-    // These two allow dynamic changing of the sidebar header from other components
-    // TODO: Figure out a better way to do this
-    setSidebarHeaderElement: undefined,
-    setSidebarHeaderElementSetter: (setter) => set({ setSidebarHeaderElement: setter }),
-    setSidebarWidth(pixels: number) {
-      set({ sidebarWidth: pixels })
-    },
-    notify: (message) => {
-      const newNotification: any = {}
-      const index = new Date().getTime()
+export const useUIStore = create<State>()(
+  immer((set, get) => {
+    const vars: Vars = {
+      isSidebarDisabled: false,
+      isSidebarOpen: true,
+      mode: 'side',
+      isMapPopupOpen: false,
+      isLoginModalOpen: false,
+      isNavbarOpen: true,
+      notifications: {},
+      sidebarWidth: undefined,
+    }
+    const actions: Actions = {
+      setIsSidebarOpen: (value) => set({ isSidebarOpen: value }),
+      setMode: (value) => set({ mode: value }),
+      setIsSidebarDisabled: (value) => set({ isSidebarDisabled: value }),
+      setIsMapPopupOpen: (value) => set({ isMapPopupOpen: value }),
+      setIsLoginModalOpen: (isOpen: boolean) => {
+        set({ isLoginModalOpen: isOpen })
+      },
+      // profileState: 'none',
+      // setProfileState: (value) => set({ profileState: value }),
+      // modalState: 'none',
+      // setModalState: (value) => set({ modalState: value }),
+      // signupFunnelStep: 0,
+      // setSignupFunnelStep: (value) => set({ signupFunnelStep: value }),
+      setIsNavbarOpen: (value) => set({ isNavbarOpen: value }),
+      // These two allow dynamic changing of the sidebar header from other components
+      // TODO: Figure out a better way to do this
+      setSidebarHeaderElement: undefined,
+      setSidebarHeaderElementSetter: (setter) =>
+        set({ setSidebarHeaderElement: setter }),
+      setSidebarWidth(pixels: number) {
+        set({ sidebarWidth: pixels })
+      },
+      notify: async (notification: NotificationMessage) => {
+        const newNotification: InternalNotificationMessage = {
+          id: generateUUID(),
+          message: notification.message,
+          severity: notification.severity,
+          duration: notification.duration || 6000,
+          triggeredTs: new Date().getTime(),
+          shown: false,
+        }
 
-      newNotification[index] = {
-        message,
-        severity: message.severity,
-        duration: message.duration || 6000,
-      }
+        await set((state) => {
+          state.notifications[newNotification.id] = newNotification
+        })
+      },
+      updateNotification: async (
+        notificationId: string,
+        notification: Partial<InternalNotificationMessage>
+      ) => {
+        const { notifications } = get()
 
-      set((state) => ({
-        notifications: { ...state.notifications, ...newNotification },
-      }))
+        const oldNotification = notifications[notificationId]
 
-      setTimeout(async () => {
-        const newNotifications: any = { ...get().notifications }
-        delete newNotifications[index]
-        set({ notifications: newNotifications })
-      }, message.duration || 6000)
-    },
-  }
+        if (oldNotification == null) {
+          console.error("Can't update a notification that does not exist")
+          return
+        }
+        const updatedNotification = { ...oldNotification, ...notification }
+        await set((state) => {
+          state.notifications[notificationId] = updatedNotification
+        })
+      },
+    }
 
-  return { ...vars, ...actions }
-})
+    return { ...vars, ...actions }
+  })
+)
