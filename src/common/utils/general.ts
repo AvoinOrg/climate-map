75efbd09ee0461943becaa65a26307fffe4c0817
xbexbex <petja.valkama@gmail.com>
2024-01-17 13:12:50 +0200

    Add mergeArraysAlternate

--- src/common/utils/general.ts
+++ src/common/utils/general.ts
@@ -35,3 +35,14 @@ export const generateShortId = () => {
 
   return id
 }
+
+export const mergeArraysAlternate = <T>(a: T[], b: T[]) => {
+  const result = []
+
+  for (let i = 0; i < Math.max(a.length, b.length); i++) {
+    if (i < a.length) result.push(a[i])
+    if (i < b.length) result.push(b[i])
+  }
+
+  return result
+}
