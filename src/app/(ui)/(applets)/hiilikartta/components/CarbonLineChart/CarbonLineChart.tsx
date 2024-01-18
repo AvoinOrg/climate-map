ab871c1357391582aebb0c525e6c31508de71841
xbexbex <petja.valkama@gmail.com>
2024-01-18 13:20:02 +0200

    Update styling and props

--- src/app/(ui)/(applets)/hiilikartta/components/CarbonLineChart/CarbonLineChart.tsx
+++ src/app/(ui)/(applets)/hiilikartta/components/CarbonLineChart/CarbonLineChart.tsx
@@ -1,25 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Box } from '@mui/material'
+import { Box, ToggleButton, Typography } from '@mui/material'
+import { styled } from '@mui/material/styles'
 
-import { CalcFeatureCollection } from '../../common/types'
+import { CalcFeatureCollection, UnitType } from '../../common/types'
 import CarbonLineChartInner from './CarbonLineChartInner'
+import { T } from '@tolgee/react'
 
 interface Props {
   data: CalcFeatureCollection[]
   featureYears: string[]
   planNames: string[]
-  useHaVals: boolean
 }
 
 const MIN_WIDTH = 700 // Define a minimum width for the chart
 
-const CarbonLineChart = ({
-  data,
-  featureYears,
-  planNames,
-  useHaVals = false,
-}: Props) => {
+const CarbonLineChart = ({ data, featureYears, planNames }: Props) => {
   const [width, setWidth] = useState(800) // Start with a default width
+  const [unitType, setUnitType] = React.useState<UnitType>('ha')
   const boxRef = useRef(null) // Ref for the container Box
 
   useEffect(() => {
@@ -42,18 +39,95 @@ const CarbonLineChart = ({
     }
   }, [boxRef])
 
+  const handleUnitTypeChange = (
+    _event: React.MouseEvent<HTMLElement>,
+    newCalcType: UnitType
+  ) => {
+    setUnitType(newCalcType)
+  }
+
   return (
-    <Box ref={boxRef} sx={{ overflowX: 'auto' }}>
-      <CarbonLineChartInner
-        data={data}
-        featureYears={featureYears}
-        planNames={planNames}
-        useHaVals={useHaVals}
-        width={width}
-        height={500}
-      />
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ mt: { xs: 0, md: 2.5 }, ml: { xs: 0, md: 2.5 } }}>
+        <Typography
+          sx={(theme) => ({
+            typography: theme.typography.h1,
+            display: 'inline',
+          })}
+        >
+          <T keyName="report.carbon_line_chart.title" ns={'hiilikartta'}></T>{' '}
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: 'inline-flex',
+          flexDirection: 'row',
+          mt: 10,
+          ml: 10,
+        }}
+      >
+        <StyledToggleButton
+          value="ha"
+          aria-label="ha"
+          sx={{ typography: 'body7', letterSpacing: 'normal' }}
+          selected={unitType === 'ha'}
+          onChange={handleUnitTypeChange}
+        >
+          <T
+            ns="hiilikartta"
+            keyName={'report.carbon_line_chart.unit_select_ha'}
+          ></T>
+        </StyledToggleButton>
+        <StyledToggleButton
+          value="total"
+          aria-label="total"
+          sx={{
+            typography: 'body7',
+            letterSpacing: 'normal',
+            ml: 0.5,
+          }}
+          selected={unitType === 'total'}
+          onChange={handleUnitTypeChange}
+        >
+          <T
+            ns="hiilikartta"
+            keyName={'report.carbon_line_chart.unit_select_total'}
+          ></T>
+        </StyledToggleButton>
+      </Box>
+      <Box ref={boxRef} sx={{ width: '100%', overflowX: 'auto' }}>
+        <CarbonLineChartInner
+          data={data}
+          featureYears={featureYears}
+          planNames={planNames}
+          unitType={unitType}
+          width={width}
+          height={500}
+        />
+      </Box>
     </Box>
   )
 }
+
+const StyledToggleButton = styled(ToggleButton)(({ theme }) => ({
+  borderRadius: '0.3125rem',
+  border: '1px solid',
+  borderColor: theme.palette.primary.dark,
+  color: theme.palette.neutral.darker,
+  width: '8.5rem',
+  whiteSpace: 'normal',
+  textTransform: 'none',
+  wordBreak: 'break-word',
+  marginBottom: '0.75rem',
+  paddingLeft: '1.25rem',
+  paddingRight: '1.25rem',
+  textAlign: 'left',
+  display: 'flex',
+  justifyContent: 'flex-start',
+  '&.Mui-selected': {
+    borderColor: theme.palette.secondary.dark,
+    backgroundColor: theme.palette.neutral.lighter,
+  },
+}))
 
 export default CarbonLineChart
