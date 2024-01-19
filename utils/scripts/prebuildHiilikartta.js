ac8ed1c028bf5afd706d8e2efcd06042547cd9d6
xbexbex <petja.valkama@gmail.com>
2024-01-19 16:24:43 +0200

    Update prebuild script for hiilikartta

--- utils/scripts/prebuildHiilikartta.js
+++ utils/scripts/prebuildHiilikartta.js
@@ -1,20 +1,25 @@
 const fs = require('fs')
 const path = require('path')
 
-const appletsPath = path.join(__dirname, 'src', 'app', 'ui', 'applets')
+const projectRoot = path.join(__dirname, '..', '..')
+const appletsPath = path.join(projectRoot, 'src', 'app', '(ui)', '(applets)')
 const hiilikarttaPath = path.join(appletsPath, 'hiilikartta')
-const pagePath = path.join(__dirname, 'src', 'app', 'ui', 'page.tsx')
+const pagePath = path.join(projectRoot, 'src', 'app', '(ui)', 'page.tsx')
 
 function renameAndCleanup() {
   // Rename hiilikartta folder
   if (fs.existsSync(hiilikarttaPath)) {
-    fs.renameSync(hiilikarttaPath, hiilikarttaPath.replace(/\(([^)]+)\)/, '$1'))
+    const newHiilikarttaPath = path.join(appletsPath, '(hiilikartta)')
+    fs.renameSync(hiilikarttaPath, newHiilikarttaPath)
   }
 
-  // Remove other folders in applets, except hiilikartta
+  // Remove other folders in (applets), except (hiilikartta)
   fs.readdirSync(appletsPath).forEach((file) => {
     const filePath = path.join(appletsPath, file)
-    if (fs.statSync(filePath).isDirectory() && filePath !== hiilikarttaPath) {
+    if (
+      fs.statSync(filePath).isDirectory() &&
+      !filePath.endsWith('(hiilikartta)')
+    ) {
       fs.rmdirSync(filePath, { recursive: true })
     }
   })
