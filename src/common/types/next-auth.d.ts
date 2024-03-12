79131182bd92967623dcdcdbc609150b129f71db
xbexbex <petja.valkama@gmail.com>
2024-03-12 18:06:07 +0200

    Add accessToken type declaration to next auth session

---
+++ src/common/types/next-auth.d.ts
@@ -0,0 +1,11 @@
+import 'next-auth'
+
+declare module 'next-auth' {
+  /**
+   * Extends the built-in session types with custom properties.
+   */
+  interface Session {
+    // Add the accessToken property to the Session type
+    accessToken?: string
+  }
+}
