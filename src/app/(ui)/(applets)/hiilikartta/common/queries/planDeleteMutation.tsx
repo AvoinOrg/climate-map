fbc4d2e6c2ea34664f63d537be4f0cd7854232ed
xbexbex <petja.valkama@gmail.com>
2024-03-15 16:37:05 +0200

    Add planDeleteMutation

---
+++ src/app/(ui)/(applets)/hiilikartta/common/queries/planDeleteMutation.tsx
@@ -0,0 +1,57 @@
+import { UseMutationOptions } from '@tanstack/react-query'
+import axios from 'axios'
+import { useSession } from 'next-auth/react'
+
+import { useMapStore } from '#/common/store/mapStore'
+
+import { useAppletStore } from 'applets/hiilikartta/state/appletStore'
+import { MutationState, PlanConf } from '../types'
+import { getPlanLayerGroupId } from '../utils'
+
+const API_URL = process.env.NEXT_PUBLIC_HIILIKARTTA_API_URL
+
+export const planDeleteMutation = (): UseMutationOptions<
+  void,
+  Error,
+  PlanConf
+> => {
+  const deletePlanConf = useAppletStore((state) => state.deletePlanConf)
+  const updatePlanConf = useAppletStore((state) => state.updatePlanConf)
+  const removeSerializableLayerGroup = useMapStore(
+    (state) => state.removeSerializableLayerGroup
+  )
+  const { data: session } = useSession()
+
+  return {
+    mutationFn: async (planConf: PlanConf) => {
+      await updatePlanConf(planConf.id, {
+        mutationState: MutationState.DELETING,
+      })
+
+      if (session) {
+        const delRes = await axios.delete(`${API_URL}/plan`, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+          params: { id: planConf.serverId },
+        })
+
+        if (delRes.status !== 200) {
+          throw new Error('Failed to delete the plan')
+        }
+      }
+
+      await removeSerializableLayerGroup(getPlanLayerGroupId(planConf.id))
+      await deletePlanConf(planConf.id)
+    },
+    onError: (error, planConf) => {
+      console.error(error)
+      updatePlanConf(planConf.id, {
+        mutationState: MutationState.IDLE,
+      })
+    },
+    retry: 3,
+    retryDelay: (attemptIndex) => Math.min(attemptIndex * 1000, 3000),
+  }
+}
